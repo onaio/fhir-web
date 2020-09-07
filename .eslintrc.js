8131af9cbe866682c57c88523e3b8f1375990d11
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-09-07 16:04:46 +0300

    update eslint configs
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,14 @@ module.exports = {
     browser: true,
     'jest/globals': true,
   },
-  extends: ['plugin:prettier/recommended'],
-  rules: {
+  extends: ['plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended','typestrict'],
+  rules:  {
     strict: 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-inferrable-types': 'warn',
   },
   plugins: ['jest'],
   parserOptions: {
