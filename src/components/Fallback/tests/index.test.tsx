36e7e54fe1486063c7b83f3ce4ec1ecf65c8a417
haseemulhaq <haseemulhaq@gmail.com>
2020-09-29 17:31:14 +0500

    add test for fallback component

---
+++ src/components/Fallback/tests/index.test.tsx
@@ -0,0 +1,21 @@
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import Fallback from '..';
+
+describe('/components/Fallback', () => {
+  it('renders without crasshing', () => {
+    shallow(<Fallback />);
+  });
+
+  it('render correctly', () => {
+    const wrapper = mount(<Fallback />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.find('Result')).toHaveLength(1);
+    expect(wrapper.find('Result').prop('status')).toBe('error');
+    expect(wrapper.find('Result').prop('title')).toBe('An Error Occurred');
+    expect(wrapper.find('Result').prop('subTitle')).toBe(
+      'There has been an error. It’s been reported to the site administrators via email and should be fixed shortly. Thanks for your patience.'
+    );
+  });
+});
