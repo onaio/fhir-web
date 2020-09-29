8928e07fc3cbe2699131b5ecdf57c6f6db6a4c76
Ashfahan <me@ashfahan.com>
2020-09-29 15:42:23 +0500

    :heavy_plus_sign: Add tests

---
+++ src/components/NotFound/tests/index.test.tsx
@@ -0,0 +1,42 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import NotFound from '..';
+
+const NotFoundLocation = 'pageurlwhichisnotavalible';
+const history = useHistory();
+
+const App = () => {
+  return (
+    <Switch>
+      {/* tslint:disable-next-line: jsx-no-lambda */}
+      <Route path="/" component={() => <div id="home" />} />
+      {/* tslint:disable-next-line: jsx-no-lambda */}
+      <Route path={`/${NotFoundLocation}`} component={() => <NotFound />} />
+    </Switch>
+  );
+};
+
+describe('src/components/NotFound', () => {
+  beforeEach(() => {
+    history.push(NotFoundLocation);
+  });
+
+  it('renders correctly', () => {
+    const wrapper = mount(<App />);
+    // should redirect to home
+    expect(wrapper.find('.ant-result-404')).toHaveLength(1);
+  });
+
+  it('redirects to home', () => {
+    const mockCallBack = jest.fn();
+
+    const wrapper = shallow(<App />);
+    wrapper.find('button').simulate('click');
+    expect(mockCallBack.mock.calls).toHaveLength(1);
+
+    expect(wrapper.find('.ant-result-404')).toHaveLength(0);
+    expect(wrapper.find('#home')).toHaveLength(1);
+  });
+});
