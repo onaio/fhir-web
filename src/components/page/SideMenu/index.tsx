416e0855a270587dd7caf6962ff8e78b9ab9078c
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-09-29 16:18:16 +0300

    Make sider component collapsible
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/components/page/SideMenu/index.tsx
+++ src/components/page/SideMenu/index.tsx
@@ -2,19 +2,25 @@ import * as React from 'react';
 import { Layout, Menu } from 'antd';
 import { UserOutlined, TeamOutlined, HomeOutlined } from '@ant-design/icons';
 import SubMenu from 'antd/lib/menu/SubMenu';
+import './SideMenu.css';
 
 const { Sider } = Layout;
 
 const SideMenu: React.FC = () => {
+  const [isCollapsed, setIsCollapsed] = React.useState<boolean>(false);
   return (
     <Sider
       style={{
         overflow: 'auto',
         left: 0,
+        height: '100vh',
       }}
       width={250}
+      collapsible
+      collapsed={isCollapsed}
+      onCollapse={setIsCollapsed}
     >
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+      <Menu defaultSelectedKeys={['1']} mode="inline">
         <Menu.Item key="1" icon={<HomeOutlined />}>
           Home
         </Menu.Item>
