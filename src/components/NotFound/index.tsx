e892d228fd76f9f10e854e1f2adc1c608168b0a6
Ashfahan <me@ashfahan.com>
2020-09-30 11:10:29 +0500

    :up: remove magic string

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,6 +1,7 @@
 import { Result, Button } from 'antd';
 import * as React from 'react';
 import { useHistory } from 'react-router';
+import { HOME_URL } from '../../constants';
 
 const NotFound: React.FC = () => {
   const history = useHistory();
@@ -11,7 +12,7 @@ const NotFound: React.FC = () => {
       title="404"
       subTitle="Sorry, the page you are trying to visit does not exist."
       extra={
-        <Button key="error" onClick={() => history.push('/')} type="primary">
+        <Button key="home" onClick={() => history.push(HOME_URL)} type="primary">
           Back Home
         </Button>
       }
