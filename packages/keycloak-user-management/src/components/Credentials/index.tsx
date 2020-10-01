d3d6ec1e2d63caef2ce9cf864f71b665d505e608
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-10-01 14:20:52 +0300

    Add keycloak-user-management as a package
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

---
+++ packages/keycloak-user-management/src/components/Credentials/index.tsx
@@ -0,0 +1,178 @@
+import React from 'react';
+import { Button, Form, Col, Card, Row, Input, Switch, notification } from 'antd';
+import { RouteComponentProps } from 'react-router';
+import { Store } from 'redux';
+import { connect } from 'react-redux';
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import { history } from '@onaio/connected-reducer-registry';
+import { HeaderBreadCrumb } from '../../../../components/page/HeaderBreadCrumb';
+import keycloakUsersReducer, {
+  fetchKeycloakUsers,
+  KeycloakUser,
+  makeKeycloakUsersSelector,
+  reducerName as keycloakUsersReducerName,
+} from '../../../../store/ducks/keycloak';
+import { KeycloakService } from '../../../../services';
+import '../CreateEditUser/CreateEditUser.css';
+
+reducerRegistry.register(keycloakUsersReducerName, keycloakUsersReducer);
+
+/** inteface for route params */
+
+export interface RouteParams {
+  userId: string;
+}
+
+/** props for editing a user view */
+export interface Props {
+  fetchKeycloakUsersCreator: typeof fetchKeycloakUsers;
+  keycloakUser: KeycloakUser | null;
+  serviceClass: typeof KeycloakService;
+}
+
+/** interface for data fields for team's form */
+export interface UserCredentialsFormFields {
+  password: string;
+  confirm: string;
+  temporary: boolean;
+}
+
+/** type intersection for all types that pertain to the props */
+export type PropsTypes = Props & RouteComponentProps<RouteParams>;
+
+/** default props for editing user component */
+export const defaultProps: Partial<PropsTypes> = {
+  fetchKeycloakUsersCreator: fetchKeycloakUsers,
+  keycloakUser: null,
+  serviceClass: KeycloakService,
+};
+
+/** Handle form submission */
+export const submitForm = (values: UserCredentialsFormFields, props: PropsTypes): void => {
+  const { serviceClass, match } = props;
+  const userId = match.params.userId;
+  const serve = new serviceClass(`/users/${userId}/reset-password`);
+  const { password, temporary } = values;
+  serve
+    .update({
+      temporary: temporary,
+      type: 'password',
+      value: password,
+    })
+    .then(() => {
+      history.push('/admin');
+      notification.success({
+        message: 'Credentials updated successfully',
+        description: '',
+      });
+    })
+    .catch((_: Error) => {
+      notification.error({
+        message: 'An error occurred',
+        description: '',
+      });
+    });
+};
+
+const UserCredentials: React.FC<PropsTypes> = (props: PropsTypes) => {
+  const userId = props.match.params.userId;
+  const isEditMode = !!userId;
+  const layout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 16 },
+  };
+
+  //   if (isLoading) {
+  //     return <Ripple />;
+  //   }
+
+  return (
+    <Col span={12}>
+      <Card title={`${isEditMode ? 'Edit User' : 'Create New User'}`} bordered={false}>
+        <HeaderBreadCrumb userId={userId} />
+        <div className="form-container">
+          <Form
+            {...layout}
+            onFinish={(values: UserCredentialsFormFields) => submitForm(values, props)}
+          >
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="confirm"
+              label="Confirm Password"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject('The two passwords that you entered do not match!');
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item name="temporary" label="Temporary" valuePropName="checked">
+              <Switch />
+            </Form.Item>
+            <Form.Item>
+              <Row justify="start">
+                <Col span={4}>
+                  <Button htmlType="submit" className="reset-password">
+                    Reset Password
+                  </Button>
+                </Col>
+              </Row>
+            </Form.Item>
+          </Form>
+        </div>
+      </Card>
+    </Col>
+  );
+};
+
+UserCredentials.defaultProps = defaultProps;
+
+export { UserCredentials };
+
+/** Interface for connected state to props */
+interface DispatchedProps {
+  keycloakUser: KeycloakUser | null;
+}
+
+// connect to store
+const mapStateToProps = (state: Partial<Store>, ownProps: PropsTypes): DispatchedProps => {
+  const userId = ownProps.match.params.userId;
+  const keycloakUsersSelector = makeKeycloakUsersSelector();
+  const keycloakUsers = keycloakUsersSelector(state, { id: [userId] });
+  const keycloakUser = keycloakUsers.length === 1 ? keycloakUsers[0] : null;
+  return { keycloakUser };
+};
+
+/** map props to action creators */
+const mapDispatchToProps = {
+  fetchKeycloakUsersCreator: fetchKeycloakUsers,
+};
+
+const ConnectedUserCredentials = connect(mapStateToProps, mapDispatchToProps)(UserCredentials);
+
+export default ConnectedUserCredentials;
---
+++ packages/keycloak-user-management/src/forms/tests/index.test.tsx
@@ -0,0 +1,201 @@
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import flushPromises from 'flush-promises';
+import React from 'react';
+import fetch from 'jest-fetch-mock';
+import { UserForm } from '..';
+import * as fixtures from '../../ducks/tests/fixtures';
+import { act } from 'react-dom/test-utils';
+
+describe('src/components/PractitionerForm', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+  it('renders without crashing', () => {
+    fetch.once(JSON.stringify([fixtures.keycloakUser]));
+    shallow(<UserForm />);
+  });
+
+  it('renders correctly', () => {
+    fetch.once(JSON.stringify([fixtures.keycloakUser]));
+    // looking for each fields
+    const wrapper = mount(<UserForm />);
+
+    // user's first name
+    const userInput = wrapper.find('input#firstName');
+    expect(userInput).toHaveLength(1);
+    expect(toJson(userInput)).toMatchSnapshot('first name input');
+    wrapper.unmount();
+  });
+
+  it('creates object to send correctly for creating new user', async () => {
+    fetch.once(JSON.stringify([]));
+    const wrapper = mount(<UserForm />);
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    // set users's first name
+    const nameInput = wrapper.find('input#firstName');
+    nameInput.simulate('change', { target: { name: 'firstName', value: 'Test' } });
+
+    // set users last name
+    const lastNameInput = wrapper.find('input#lastName');
+    lastNameInput.simulate('change', { target: { name: 'lastName', value: 'One' } });
+
+    // set username
+    const usernameInput = wrapper.find('input#username');
+    usernameInput.simulate('change', { target: { name: 'username', value: 'TestOne' } });
+
+    // set user email
+    const emailInput = wrapper.find('input#email');
+    emailInput.simulate('change', { target: { name: 'email', value: 'testone@gmail.com' } });
+
+    wrapper.find('form').simulate('submit');
+    await act(async () => {
+      wrapper.update();
+    });
+
+    await new Promise<unknown>((resolve) => setImmediate(resolve));
+    wrapper.update();
+
+    const payload = {
+      access: {
+        manageGroupMembership: false,
+        view: false,
+        mapRoles: false,
+        impersonate: false,
+        manage: false,
+      },
+      disableableCredentialTypes: [],
+      email: 'testone@gmail.com',
+      emailVerified: false,
+      enabled: true,
+      firstName: 'Test',
+      id: '',
+      lastName: 'One',
+      notBefore: 0,
+      requiredActions: [],
+      totp: false,
+      username: 'TestOne',
+    };
+
+    expect(fetch.mock.calls[0]).toEqual([
+      'https://keycloak-test.smartregister.org/auth/realms//users',
+      {
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache',
+        body: JSON.stringify(payload),
+        headers: {
+          accept: 'application/json',
+          authorization: 'Bearer null',
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        method: 'POST',
+      },
+    ]);
+    wrapper.unmount();
+  });
+
+  it('creates object to send correctly for editing user', async () => {
+    fetch.once(JSON.stringify([fixtures.keycloakUser]));
+    const props = {
+      initialValues: fixtures.keycloakUser,
+    };
+    const wrapper = mount(<UserForm {...props} />);
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    // practitioner's name
+    const nameInput = wrapper.find('input#firstName');
+    nameInput.simulate('change', { target: { name: 'firstName', value: 'Test2' } });
+
+    expect(toJson(wrapper.find('input#username'))).toMatchSnapshot('readonly username edit');
+
+    wrapper.find('form').simulate('submit');
+
+    await act(async () => {
+      wrapper.update();
+    });
+
+    await new Promise<unknown>((resolve) => setImmediate(resolve));
+
+    const payload = {
+      id: 'cab07278-c77b-4bc7-b154-bcbf01b7d35b',
+      createdTimestamp: 1600156317992,
+      username: 'opensrp',
+      enabled: true,
+      totp: false,
+      emailVerified: false,
+      firstName: 'Test2',
+      lastName: 'kenya',
+      email: 'test@onatest.com',
+      disableableCredentialTypes: [],
+      requiredActions: [],
+      notBefore: 0,
+      access: {
+        manageGroupMembership: true,
+        view: true,
+        mapRoles: true,
+        impersonate: false,
+        manage: true,
+      },
+    };
+
+    expect(fetch.mock.calls[0]).toEqual([
+      'https://keycloak-test.smartregister.org/auth/realms//users/cab07278-c77b-4bc7-b154-bcbf01b7d35b',
+      {
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache',
+        body: JSON.stringify(payload),
+        headers: {
+          accept: 'application/json',
+          authorization: 'Bearer null',
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        method: 'PUT',
+      },
+    ]);
+    wrapper.unmount();
+  });
+  it('user is not created if api is down', async () => {
+    fetch.mockReject(() => Promise.reject('API is down'));
+    const wrapper = mount(<UserForm />);
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    // set users's first name
+    const nameInput = wrapper.find('input#firstName');
+    nameInput.simulate('change', { target: { name: 'firstName', value: 'Test' } });
+
+    // set users last name
+    const lastNameInput = wrapper.find('input#lastName');
+    lastNameInput.simulate('change', { target: { name: 'lastName', value: 'One' } });
+
+    // set username
+    const usernameInput = wrapper.find('input#username');
+    usernameInput.simulate('change', { target: { name: 'username', value: 'TestOne' } });
+
+    // set user email
+    const emailInput = wrapper.find('input#email');
+    emailInput.simulate('change', { target: { name: 'email', value: 'testone@gmail.com' } });
+
+    wrapper.find('form').simulate('submit');
+    await act(async () => {
+      wrapper.update();
+    });
+
+    await new Promise<unknown>((resolve) => setImmediate(resolve));
+    wrapper.update();
+    expect(document.getElementsByClassName('ant-notification')).toHaveLength(1);
+    wrapper.unmount();
+  });
+});
