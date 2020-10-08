6d3b215bbd7cf074db9cbbbf920cdeada99ec1f1
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-10-08 16:00:24 +0300

    Add exclude propery in @opensrp/store tsconfig, transpile

--- packages/opensrp-store/dist/types/store/tests/ducks/random.d.ts
+++
@@ -1,4 +0,0 @@
-import { AnyAction } from 'redux';
-export declare const reducerName = "random";
-export declare const SET_RANDOM = "reveal-test/reducer/SET_RANDOM";
-export default function reducer(state: string | undefined, action: AnyAction): string;
