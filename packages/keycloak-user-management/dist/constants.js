c7fbb26fe711b7f9d75bc3265813864e85babae2
haseemulhaq <haseemulhaq@gmail.com>
2020-10-13 17:38:29 +0500

    merge latest master

---
+++ packages/keycloak-user-management/dist/constants.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+exports.CREATE_NEW_USER = exports.ADMIN_URL = exports.DELETE = exports.USER_EDIT_URL = exports.USER_DELETED_SUCCESSFULLY = exports.ERROR_OCCURED = void 0;
+var ERROR_OCCURED = 'An error occurred';
+exports.ERROR_OCCURED = ERROR_OCCURED;
+var USER_DELETED_SUCCESSFULLY = 'User deleted successfully';
+exports.USER_DELETED_SUCCESSFULLY = USER_DELETED_SUCCESSFULLY;
+var USER_EDIT_URL = '/user/edit';
+exports.USER_EDIT_URL = USER_EDIT_URL;
+var DELETE = 'Delete';
+exports.DELETE = DELETE;
+var ADMIN_URL = '/admin';
+exports.ADMIN_URL = ADMIN_URL;
+var CREATE_NEW_USER = '/user/new';
+exports.CREATE_NEW_USER = CREATE_NEW_USER;
---
+++ packages/keycloak-user-management/dist/types/constants.d.ts
@@ -0,0 +1,6 @@
+export declare const ERROR_OCCURED = 'An error occurred';
+export declare const USER_DELETED_SUCCESSFULLY = 'User deleted successfully';
+export declare const USER_EDIT_URL = '/user/edit';
+export declare const DELETE = 'Delete';
+export declare const ADMIN_URL = '/admin';
+export declare const CREATE_NEW_USER = '/user/new';
