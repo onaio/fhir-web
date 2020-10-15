e8229984f2a4672cd734b200f7b74753576cb9e3
haseemulhaq <haseemulhaq@gmail.com>
2020-10-15 20:00:36 +0500

    test loader props

--- client/src/components/page/Loading/tests/index.test.tsx
+++ client/src/components/page/Loading/tests/index.test.tsx
@@ -1,32 +1,30 @@
 // test ExportModal
 import { mount, shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import React from 'react';
 
 import Loading from '../index';
 
 describe('components/Loading', () => {
+  const props = {
+    borderColor: '#ff0000',
+    borderStyle: 'solid',
+    borderWidth: '6px',
+    height: '128px',
+    minHeight: '70vh',
+    width: '128px',
+  };
+
   it('renders without crashing', () => {
     shallow(<Loading />);
   });
-  it('renders the Loading component', () => {
-    const wrapper = mount(<Loading />);
-    expect(toJson(wrapper)).toMatchSnapshot();
-    wrapper.unmount();
+
+  it('should have props on calling loading component', () => {
+    const wrapper = mount(<Loading {...props} />);
+    expect(wrapper.props()).not.toBeNull();
   });
 
-  it('renders the Loading component when props are given', () => {
-    const props = {
-      borderColor: '#ff0000',
-      borderStyle: 'solid',
-      borderWidth: '6px',
-      height: '128px',
-      minHeight: '70vh',
-      width: '128px',
-    };
+  it('check props passed in', () => {
     const wrapper = mount(<Loading {...props} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
-    expect(wrapper.props()).toMatchSnapshot();
-    wrapper.unmount();
+    expect(wrapper.props()).toEqual(props);
   });
 });
