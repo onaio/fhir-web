07354e7fc68ec1e01f2b68f3f6d048febfae0fff
Ashfahan <me@ashfahan.com>
2020-10-19 00:51:42 +0500

    :up: Update form modularity

--- client/src/containers/pages/locations/LocationGroupAddition/index.tsx
+++ client/src/containers/pages/locations/LocationGroupAddition/index.tsx
@@ -6,8 +6,35 @@ interface Props {}
 
 const layout = { labelCol: { span: 8 }, wrapperCol: { span: 8 } };
 const offsetLayout = { wrapperCol: { offset: 8, span: 8 } };
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not validate email!',
+    number: '${label} is not a validate number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
+const location = [
+  { name: 'Option 1', value: 'Option1' },
+  { name: 'Option 2', value: 'Option2' },
+  { name: 'Option 3', value: 'Option3' },
+];
+
+const status = [
+  { label: 'Active', value: 'active' },
+  { label: 'Inactive', value: 'inactive' },
+];
+
+const initialvalue = { status: 'active' };
 
 export const LocationUnitGroupAdd: React.FC<Props> = () => {
+  function filter(input: string, option: any) {
+    return option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+  }
+
   return (
     <section>
       <Helmet>
@@ -15,41 +42,36 @@ export const LocationUnitGroupAdd: React.FC<Props> = () => {
       </Helmet>
       <h5 className="mb-3">Add Location Unit Group</h5>
       <div className="bg-white p-5">
-        <Form {...layout} requiredMark="optional" name="basic" initialValues={{ remember: true }}>
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: 'Please Select A location group!' }]}
-          >
+        <Form
+          {...layout}
+          requiredMark="optional"
+          name="location-add"
+          validateMessages={validateMessages}
+          initialValues={initialvalue}
+        >
+          <Form.Item label="Location Name" name="name" rules={[{ required: true }]}>
             <Select
               showSearch
               placeholder="Enter a location group name"
               optionFilterProp="children"
-              filterOption={(input: string, option: any) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
+              filterOption={filter}
             >
-              <Select.Option value="group1">Group 1</Select.Option>
-              <Select.Option value="group2">Group 2</Select.Option>
-              <Select.Option value="group3">Group 3</Select.Option>
+              {location.map((e) => (
+                <Select.Option value={e.value}>{e.name}</Select.Option>
+              ))}
             </Select>
           </Form.Item>
 
-          <Form.Item label="Status" name="status" valuePropName="checked">
-            <Radio.Group
-              options={[
-                { label: 'Active', value: 'active' },
-                { label: 'Inactive', value: 'inactive' },
-              ]}
-              defaultValue="active"
-            />
+          <Form.Item
+            label="Status"
+            name="status"
+            valuePropName="checked"
+            rules={[{ required: true }]}
+          >
+            <Radio.Group options={status} defaultValue={initialvalue.status} />
           </Form.Item>
 
-          <Form.Item
-            name="type"
-            label="Type"
-            rules={[{ required: true, message: 'Please Enter type description!' }]}
-          >
+          <Form.Item name="type" label="Type" rules={[{ required: true }]}>
             <Input.TextArea rows={4} placeholder="Description" />
           </Form.Item>
 
