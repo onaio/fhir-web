cf430441f32be190690086292f3a733efc5c1d7e
Kelvin Muchiri <kmubari@ona.io>
2020-10-21 12:35:59 +0300

    Transpile packages (#97)
    * Transpile keycloak-service
    
    * Transpile package keycloak-user-management
    
    * Transpile package opensrp-store
    
    * Revert lint-staged file filter

--- packages/keycloak-user-management/dist/types/components/CreateEditUser/index.d.ts
+++ packages/keycloak-user-management/dist/types/components/CreateEditUser/index.d.ts
@@ -6,14 +6,15 @@ import { KeycloakService } from '@opensrp/keycloak-service';
 import '../../index.css';
 /** inteface for route params */
 export interface RouteParams {
-    userId: string;
+  userId: string;
 }
 /** props for editing a user view */
 export interface EditUserProps {
-    accessToken: string;
-    fetchKeycloakUsersCreator: typeof fetchKeycloakUsers;
-    keycloakUser: KeycloakUser | null;
-    serviceClass: typeof KeycloakService;
+  accessToken: string;
+  fetchKeycloakUsersCreator: typeof fetchKeycloakUsers;
+  keycloakUser: KeycloakUser | null;
+  serviceClass: typeof KeycloakService;
+  keycloakBaseURL: string;
 }
 /** type intersection for all types that pertain to the props */
 export declare type PropsTypes = EditUserProps & RouteComponentProps<RouteParams>;
@@ -22,14 +23,28 @@ export declare const defaultInitialValues: KeycloakUser;
 /** default props for editing user component */
 export declare const defaultEditUserProps: EditUserProps;
 /** yup validations for practitioner data object from form */
-export declare const userSchema: Yup.ObjectSchema<Yup.Shape<object | undefined, {
-    lastName: string;
-    firstName: string;
-}>, object>;
+export declare const userSchema: Yup.ObjectSchema<
+  Yup.Shape<
+    object | undefined,
+    {
+      lastName: string;
+      firstName: string;
+    }
+  >,
+  object
+>;
 /**
  *
  * @param props - CreateEditUser component props
  */
 declare const CreateEditUsers: React.FC<PropsTypes>;
 export { CreateEditUsers };
-export declare const ConnectedCreateEditUsers: import("react-redux").ConnectedComponent<React.FC<PropsTypes>, Pick<PropsTypes, "location" | "serviceClass" | "history" | "match" | "staticContext"> & EditUserProps & RouteComponentProps<RouteParams, import("react-router").StaticContext, unknown>>;
+export declare const ConnectedCreateEditUsers: import('react-redux').ConnectedComponent<
+  React.FC<PropsTypes>,
+  Pick<
+    PropsTypes,
+    'location' | 'serviceClass' | 'keycloakBaseURL' | 'history' | 'match' | 'staticContext'
+  > &
+    EditUserProps &
+    RouteComponentProps<RouteParams, import('react-router').StaticContext, unknown>
+>;
