28f47427c21e2acb235c1c9011b4c806628dcfb3
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-10-21 10:29:40 +0300

    Transpile package keycloak-user-management

--- packages/keycloak-user-management/dist/types/components/Loading/index.d.ts
+++ packages/keycloak-user-management/dist/types/components/Loading/index.d.ts
@@ -2,14 +2,19 @@ import React from 'react';
 import '../../index.css';
 /** interface for Ripple props */
 export interface RippleProps {
-    borderColor?: string;
-    borderStyle?: string;
-    borderWidth?: string;
-    height?: string;
-    minHeight?: string;
-    width?: string;
+  borderColor?: string;
+  borderStyle?: string;
+  borderWidth?: string;
+  height?: string;
+  minHeight?: string;
+  width?: string;
 }
 export declare const defaultProps: Partial<RippleProps>;
-/** Loading component that displays a nice ripple */
+/**
+ * Loading component that displays a nice ripple
+ *
+ * @param {object} props - the ripple styles
+ * @returns {Element} - display the loader
+ */
 declare const Ripple: React.FC<RippleProps>;
 export default Ripple;
