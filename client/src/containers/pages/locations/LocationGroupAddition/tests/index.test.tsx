398cbcf3c597454f2979174029706e700081cfa4
Ashfahan <me@ashfahan.com>
2020-10-21 11:38:11 +0500

    :up: Match props in snapshot instead of whole component

--- client/src/containers/pages/locations/LocationGroupAddition/tests/index.test.tsx
+++ client/src/containers/pages/locations/LocationGroupAddition/tests/index.test.tsx
@@ -1,12 +1,11 @@
 import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import React from 'react';
 import LocationUnitGroupAdd from '..';
 
 describe('containers/pages/locations/LocationGroupAddition', () => {
   it('renders without crashing', () => {
     const wrapper = mount(<LocationUnitGroupAdd />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.find('section').props()).toMatchSnapshot();
   });
 
   it('tests filter function', () => {
