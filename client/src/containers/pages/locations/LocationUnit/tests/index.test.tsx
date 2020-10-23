ae915cf5cf71a57f817ed04c47a519202ab00de2
Ashfahan <me@ashfahan.com>
2020-10-23 05:00:26 +0500

    :up: increase coverage and make component modular

--- client/src/containers/pages/locations/LocationUnit/tests/index.test.tsx
+++ client/src/containers/pages/locations/LocationUnit/tests/index.test.tsx
@@ -2,16 +2,57 @@ import { mount, shallow } from 'enzyme';
 import flushPromises from 'flush-promises';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
-import { LocationUnit } from '..';
+import LocationUnit, { data } from '..';
+import { TREE } from '../../../../../utils/Tree';
 
 describe('containers/pages/locations/locationunit', () => {
+  const tableData: data[] = [];
+  for (let i = 1; i < 5; i++) {
+    tableData.push({
+      key: i.toString(),
+      name: `Edrward ${i}`,
+      level: i,
+      lastupdated: new Date(`Thu Oct ${i} 2020 14:15:56 GMT+0500 (Pakistan Standard Time)`),
+      status: 'Alive',
+      type: 'Feautire',
+      created: new Date(`Thu Oct ${i} 2020 14:15:56 GMT+0500 (Pakistan Standard Time)`),
+      externalid: `asdkjh123${i}`,
+      openmrsid: `asdasdasdkjh123${i}`,
+      username: `edward ${i}`,
+      version: `${i}`,
+      syncstatus: 'Synced',
+    });
+  }
+
+  const tree: TREE[] = [
+    {
+      title: 'Sierra Leone',
+      key: 'Sierra Leone',
+      children: [
+        { title: 'Bo', key: 'Bo', children: [{ title: '1', key: '1' }] },
+        { title: 'Bombali', key: 'Bombali', children: [{ title: '2', key: '2' }] },
+        {
+          title: 'Bonthe',
+          key: 'Bonthe',
+          children: [
+            {
+              title: 'Kissi Ten',
+              key: 'Kissi Ten',
+              children: [{ title: 'Bayama CHP', key: 'Bayama CHP' }],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
   it('renders without crashing', () => {
-    const wrapper = shallow(<LocationUnit />);
+    const wrapper = shallow(<LocationUnit tableData={tableData} tree={tree} />);
     expect(wrapper.props()).toMatchSnapshot();
   });
 
-  it('Render Set details', () => {
-    const wrapper = mount(<LocationUnit />);
+  it('Render view details', () => {
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
 
     // click on view detail
     const first_action = wrapper.find('.d-flex.justify-content-end.align-items-center').first();
@@ -28,11 +69,11 @@ describe('containers/pages/locations/locationunit', () => {
     expect(viewdetail.length).toBe(0);
   });
 
-  it('Render Set details', () => {
-    const wrapper = mount(<LocationUnit />);
+  it('Test view details close', () => {
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
 
     // click on view detail
-    const first_row = wrapper.find('tr[data-row-key="0"]');
+    const first_row = wrapper.find('tr[data-row-key="1"]');
     first_row.find('span.ant-dropdown-trigger').simulate('click'); // click on dropdown icon
     wrapper.find('.ant-dropdown-menu-item.ant-dropdown-menu-item-only-child').simulate('click'); // click on viewdetails
 
@@ -47,13 +88,13 @@ describe('containers/pages/locations/locationunit', () => {
   });
 
   it('Test save functionality', async () => {
-    const wrapper = mount(<LocationUnit />);
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
 
     // click edit button
-    let first_row = wrapper.find('tr[data-row-key="0"]');
+    let first_row = wrapper.find('tr[data-row-key="1"]');
     first_row.find('button').simulate('click');
 
-    first_row = wrapper.find('tr[data-row-key="0"]');
+    first_row = wrapper.find('tr[data-row-key="1"]');
     first_row
       .children()
       .first()
@@ -69,13 +110,13 @@ describe('containers/pages/locations/locationunit', () => {
   });
 
   it('Test cancel functionality', async () => {
-    const wrapper = mount(<LocationUnit />);
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
 
     // click edit button
-    let first_row = wrapper.find('tr[data-row-key="0"]');
+    let first_row = wrapper.find('tr[data-row-key="1"]');
     first_row.find('button').simulate('click');
 
-    first_row = wrapper.find('tr[data-row-key="0"]');
+    first_row = wrapper.find('tr[data-row-key="1"]');
     first_row
       .children()
       .first()
@@ -88,5 +129,44 @@ describe('containers/pages/locations/locationunit', () => {
       await flushPromises();
       wrapper.update();
     });
+  });
+
+  it('Test Name Sorting functionality', () => {
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
+
+    let heading = wrapper.find('thead');
+    expect(heading.find('th').length).toBe(4);
+    heading.find('th').at(0).children().simulate('click');
+    heading.find('th').at(0).children().simulate('click');
+
+    let body = wrapper.find('tbody');
+    expect(body.children().first().prop('rowKey')).toBe('4');
+    expect(body.children().last().prop('rowKey')).toBe('1');
+  });
+
+  it('Test Level Sorting functionality', () => {
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
+
+    let heading = wrapper.find('thead');
+    expect(heading.find('th').length).toBe(4);
+    heading.find('th').at(1).children().simulate('click');
+    heading.find('th').at(1).children().simulate('click');
+
+    let body = wrapper.find('tbody');
+    expect(body.children().first().prop('rowKey')).toBe('4');
+    expect(body.children().last().prop('rowKey')).toBe('1');
+  });
+
+  it('Test Last Updated Sorting functionality', () => {
+    const wrapper = mount(<LocationUnit tableData={tableData} tree={tree} />);
+
+    let heading = wrapper.find('thead');
+    expect(heading.find('th').length).toBe(4);
+    heading.find('th').at(2).children().simulate('click');
+    heading.find('th').at(2).children().simulate('click');
+
+    let body = wrapper.find('tbody');
+    expect(body.children().first().prop('rowKey')).toBe('4');
+    expect(body.children().last().prop('rowKey')).toBe('1');
   });
 });
