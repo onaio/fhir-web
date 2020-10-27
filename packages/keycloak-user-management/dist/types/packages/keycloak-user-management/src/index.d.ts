3b5a56e47c67c8efa0cfe67e3d00eab03333dc1f
p-netm <pmuriuki98@gmail.com>
2020-10-27 17:21:46 +0300

    Remove dist folders for all packages

--- packages/keycloak-user-management/dist/types/packages/keycloak-user-management/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import ConnectedAdminView from './components/';
-import ConnectedCreateEditUsers from './components/CreateEditUser';
-import ConnectedUserCredentials from './components/Credentials';
-export { ConnectedAdminView, ConnectedCreateEditUsers, ConnectedUserCredentials };
