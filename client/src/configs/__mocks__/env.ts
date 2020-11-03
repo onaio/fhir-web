a535bdda4094d8331775f37179f1cbe69dc684f5
Ashfahan <me@ashfahan.com>
2020-11-03 11:42:32 +0500

    :up: mock values

--- client/src/configs/__mocks__/env.ts
+++ client/src/configs/__mocks__/env.ts
@@ -1,1 +1,3 @@
 export const OPENSRP_API_BASE_URL = 'https://test.smartregister.org/opensrp/rest/';
+export const REACT_APP_LANGUAGE_SWITCHER = 'true';
+export const REACT_APP_DEFAULT_LANGUAGE = 'English';
