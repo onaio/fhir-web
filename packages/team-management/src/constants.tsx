862d78e2d72548f200908f489c5b48a359033bb7
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-11-04 10:26:35 +0300

    Add team url constants
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

---
+++ packages/team-management/src/constants.tsx
@@ -0,0 +1,5 @@
+export const CREATE_TEAM_URL = '/teams/new';
+export const EDIT_TEAM_URL = '/teams/edit';
+export const SINGLE_TEAM_URL = '/teams/view';
+export const TEAMS_LIST_URL = '/teams';
+export const ASSIGN_TEAMS_URL = '/plans/teamAssignment';
