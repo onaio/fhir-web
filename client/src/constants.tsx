1649c7e345679bd7a8341eb1cd875b85aaa40745
haseemVD <haseem.haq@venturedive.com>
2020-11-07 00:39:36 +0500

    'update'

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -13,3 +13,4 @@ export const URL_USER_EDIT = '/user/edit';
 export const URL_TEAMS = '/teams';
 export const URL_BACKEND_LOGIN = '/fe/login';
 export const URL_BACKEND_CALLBACK = '/fe/oauth/callback/opensrp';
+export const URL_LOCATION_UNIT_GROUP = '/location-unit-group';
