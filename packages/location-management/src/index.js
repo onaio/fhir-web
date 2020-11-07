3bf43ddbba6199908f20a3a635b870b0abcf42cb
Ashfahan <me@ashfahan.com>
2020-11-08 04:07:38 +0500

    :fire: unnecessary files

--- packages/location-management/src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var connectedlocationunitgroupadd = require("./components/LocationGroupAddition");
-exports.ConnectedLocationUnitGroupAdd = connectedlocationunitgroupadd["default"];
