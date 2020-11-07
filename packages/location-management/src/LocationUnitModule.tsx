c7461539dd9b45fdcc80148c4774d4502bddadbd
Ashfahan <me@ashfahan.com>
2020-11-07 20:36:59 +0500

    :heavy_plus_sign: Add Location unit Addition page

--- packages/location-management/src/LocationUnitModule.tsx
+++ packages/location-management/src/LocationUnitModule.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { URL_ADD_LOCATIONS_UNIT } from './constants';
 
 import LocationUnitView from './components/LocationUnitView';
+import LocationUnitAdd from './components/LocationUnitAdd';
 
 export default function LocationUnitModule() {
+  let modulebaseurl = '/location';
+
   return (
     <BrowserRouter>
       <Switch>
+        <Route path={modulebaseurl + URL_ADD_LOCATIONS_UNIT} component={LocationUnitAdd} />
         <Route exact component={LocationUnitView} />
       </Switch>
     </BrowserRouter>
