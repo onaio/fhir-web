108b3a739c502a250495221c1b13cec708992df6
haseemVD <haseem.haq@venturedive.com>
2020-11-09 13:46:00 +0500

    update test

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -13,4 +13,3 @@ export const URL_USER_EDIT = '/user/edit';
 export const URL_TEAMS = '/teams';
 export const URL_BACKEND_LOGIN = '/fe/login';
 export const URL_BACKEND_CALLBACK = '/fe/oauth/callback/opensrp';
-export const URL_LOCATION_UNIT_GROUP = '/location-unit-group';
--- packages/location-management/src/constants.tsx
+++
@@ -1,4 +0,0 @@
-// URLs
-export const URL_ALL_LOCATION_TAGS = '/location-tag';
-export const URL_DELETE_LOCATION_TAGS = '/location-tag/delete';
-export const KEYCLOAK_API_BASE_URL = 'https://opensrp-stage.smartregister.org/opensrp/rest';
