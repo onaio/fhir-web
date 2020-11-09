c274c863d4d97b45cd06939ee737285bbe0c905c
Ashfahan <me@ashfahan.com>
2020-11-10 02:19:53 +0500

    :up: move moduel base url into constants

--- packages/location-management/src/LocationUnitModule.tsx
+++ packages/location-management/src/LocationUnitModule.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import { URL_ADD_LOCATIONS_UNIT } from './constants';
+import { URL_ADD_LOCATIONS_UNIT, URL_LOCATIONS_UNIT } from './constants';
 
 import LocationUnitView from './components/LocationUnitView';
 import LocationUnitAdd from './components/LocationUnitAdd';
@@ -12,12 +12,13 @@ import LocationUnitAdd from './components/LocationUnitAdd';
  * @returns {Element} - Location Unit Module
  */
 export const LocationUnitModule: React.FC = () => {
-  const baseurl = '/location/unit';
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={baseurl + URL_ADD_LOCATIONS_UNIT + '/:id'} component={LocationUnitAdd} />
+        <Route
+          path={URL_LOCATIONS_UNIT + URL_ADD_LOCATIONS_UNIT + '/:id'}
+          component={LocationUnitAdd}
+        />
         <Route exact component={LocationUnitView} />
       </Switch>
     </BrowserRouter>
