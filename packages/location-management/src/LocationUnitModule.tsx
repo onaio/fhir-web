1c538c5395888f2670f1cac317f211887e4157e4
Ashfahan <me@ashfahan.com>
2020-11-10 02:58:12 +0500

    :heavy_plus_sign: combine URL route into one constant

--- packages/location-management/src/LocationUnitModule.tsx
+++ packages/location-management/src/LocationUnitModule.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import { URL_ADD_LOCATIONS_UNIT, URL_LOCATIONS_UNIT } from './constants';
+import { URL_LOCATION_UNIT_ADD } from './constants';
 
 import LocationUnitView from './components/LocationUnitView';
 import LocationUnitAdd from './components/LocationUnitAdd';
@@ -15,10 +15,8 @@ export const LocationUnitModule: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route
-          path={URL_LOCATIONS_UNIT + URL_ADD_LOCATIONS_UNIT + '/:id'}
-          component={LocationUnitAdd}
-        />
+        <Route path={URL_LOCATION_UNIT_ADD + '/:id'} component={LocationUnitAdd} />
+        <Route path={URL_LOCATION_UNIT_ADD} component={LocationUnitAdd} />
         <Route exact component={LocationUnitView} />
       </Switch>
     </BrowserRouter>
--- packages/location-management/src/constants.tsx
+++ packages/location-management/src/constants.tsx
@@ -6,8 +6,8 @@ export const API_BASE_URL = 'https://opensrp-stage.smartregister.org/opensrp/res
 export const LOCATION_UNIT_ALL = 'location/sync';
 export const LOCATION_UNIT_POST_PUT = 'location?is_jurisdiction=true';
 // Url
-export const URL_LOCATIONS_UNIT = '/location/unit';
-export const URL_ADD_LOCATIONS_UNIT = '/add';
+export const URL_LOCATION_UNIT = '/location/unit';
+export const URL_LOCATION_UNIT_ADD = URL_LOCATION_UNIT + '/add';
 
 //
 // Locatoin Tag
