81a8c8fdbdbdfcb0607cd7bdbbf7e2ba291986ac
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-11-10 16:24:51 +0300

    Add fetchSingleLocation action
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/location-management/src/ducks/location-hierarchy.ts
+++ packages/location-management/src/ducks/location-hierarchy.ts
@@ -17,6 +17,7 @@ export const reducerName = 'location-hierarchy';
 /** action to add a tree to store */
 export const TREE_FETCHED = 'location-hierarchy/TREE_FETCHED';
 export const FETCH_CURRENT_CHILDREN = 'location-hierarchy/FETCH_CURRENT_CHILDREN';
+export const FETCH_SINGLE_LOCATION = 'location-hierarchy/FETCH_SINGLE_LOCATION';
 
 /** describes action that adds a hierarchy tree to store */
 export interface FetchedTreeAction extends AnyAction {
@@ -30,8 +31,18 @@ export interface FetchedParentChildrenAction extends AnyAction {
   currentParentChildren: TreeNode[];
 }
 
+/** describes action that adds current parent children to store  */
+export interface FetchedSingleLocationAction extends AnyAction {
+  type: typeof FETCH_SINGLE_LOCATION;
+  locationObject: any;
+}
+
 /** combined full action types | its a union */
-export type TreeActionTypes = FetchedTreeAction | FetchedParentChildrenAction | AnyAction;
+export type TreeActionTypes =
+  | FetchedTreeAction
+  | FetchedParentChildrenAction
+  | FetchedSingleLocationAction
+  | AnyAction;
 
 // **************************** action creators ****************************
 
@@ -62,6 +73,19 @@ export function fetchCurrentChildren(children: TreeNode[]): FetchedParentChildre
   };
 }
 
+/**
+ * action creator when adding a single location to store
+ *
+ * @param {any} location - location object received from OpenSRP
+ * @returns {object} - action object
+ */
+export function fetchSingleLocation(location: any): FetchedSingleLocationAction {
+  return {
+    locationObject: location,
+    type: FETCH_SINGLE_LOCATION,
+  };
+}
+
 // **************************** medusa ****************************
 
 /** The store's slice state
@@ -70,6 +94,7 @@ export function fetchCurrentChildren(children: TreeNode[]): FetchedParentChildre
 export interface TreeState {
   hierarchyArray: any;
   currentParentChildren: any;
+  locationObject: any;
 }
 
 /** Create an immutable tree state */
@@ -79,6 +104,7 @@ export type ImmutableTreeState = TreeState & SeamlessImmutable.ImmutableObject<T
 export const initialState: ImmutableTreeState = SeamlessImmutable({
   hierarchyArray: [],
   currentParentChildren: [],
+  locationObject: {},
 });
 
 // the reducer function
@@ -99,6 +125,12 @@ export default function reducer(state: ImmutableTreeState = initialState, action
       return {
         ...state,
         currentParentChildren: action.currentParentChildren,
+      };
+
+    case FETCH_SINGLE_LOCATION:
+      return {
+        ...state,
+        locationObject: action.locationObject,
       };
 
     default:
