c4120947cc0af8216d3ad8465f096ca1440c5ab9
Ashfahan <me@ashfahan.com>
2020-11-16 12:14:50 +0500

    :fire: fix build issues

--- packages/location-management/src/constants.tsx
+++ packages/location-management/src/constants.tsx
@@ -1,18 +1,19 @@
 export const API_BASE_URL = 'https://opensrp-stage.smartregister.org/opensrp/rest/';
 
 //
-// Locatoin Unit
+// Location Unit
 //
 export const LOCATION_UNIT_ALL = 'location/sync';
 export const LOCATION_UNIT_GET = 'location';
 export const LOCATION_UNIT_POST_PUT = 'location?is_jurisdiction=true';
+export const LOCATION_FINDBYPROPERTIES = 'location/findByProperties';
 // Url
 export const URL_LOCATION_UNIT = '/location/unit';
 export const URL_LOCATION_UNIT_ADD = URL_LOCATION_UNIT + '/add';
 export const URL_LOCATION_UNIT_EDIT = URL_LOCATION_UNIT + '/edit';
 
 //
-// Locatoin Tag
+// Location Tag
 //
 export const LOCATION_TAG_ALL = 'location-tag';
 export const LOCATION_TAG_GET = 'location-tag/';
@@ -21,3 +22,8 @@ export const LOCATION_TAG_DELETE = 'location-tag/delete/';
 export const URL_LOCATION_TAG = '/location/group';
 export const URL_LOCATION_TAG_ADD = URL_LOCATION_TAG + '/add';
 export const URL_LOCATION_TAG_EDIT = URL_LOCATION_TAG + '/edit';
+
+//
+// Location Unit
+//
+export const LOCATION_HIERARCHY = 'location/hierarchy';
