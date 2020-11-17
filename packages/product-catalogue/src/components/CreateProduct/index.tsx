c98f583327ed06c0ab091f8a7df907815f864f87
p-netm <pmuriuki98@gmail.com>
2020-11-17 15:46:18 +0300

    Views to create and edit product in the catalogue (#148)
    * Add productForm and tests
    
    * Create product page
    
    * Add Edit product page
    
    * Add common props to create and edit pages
    
    * Update snapshots
    
    * Fix import path
    
    * Export create and edit Product page
    
    * Refactor class names
    
    * Correct misleading jsdoc
    
    * Correct jsdoc string in tests

---
+++ packages/product-catalogue/src/components/CreateProduct/index.tsx
@@ -0,0 +1,40 @@
+/** component renders view where users can create products
+ * in the catalogue
+ */
+import React from 'react';
+import { ProductForm } from '../ProductForm';
+import { Layout, PageHeader } from 'antd';
+import Helmet from 'react-helmet';
+import { CommonProps, defaultCommonProps } from '../../helpers/common';
+
+type CreateProductViewProps = CommonProps;
+
+const defaultProps = {
+  ...defaultCommonProps,
+};
+
+/**
+ * CreateProductView component
+ *
+ *  props - component props
+ */
+
+const CreateProductView = (props: CreateProductViewProps) => {
+  const { baseURL } = props;
+  const pageTitle = 'Add Product to Catalogue';
+
+  return (
+    <Layout className="content-section">
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+      <PageHeader title={pageTitle} className="pageHeader"></PageHeader>
+
+      <ProductForm baseURL={baseURL} />
+    </Layout>
+  );
+};
+
+CreateProductView.defaultProps = defaultProps;
+
+export { CreateProductView };
