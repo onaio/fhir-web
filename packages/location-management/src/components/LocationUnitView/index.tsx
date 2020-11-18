9a02182a02824406e8d37a54e84f3c56165bb262
Ashfahan <me@ashfahan.com>
2020-11-18 16:31:35 +0500

    show heirachy node even without any child

--- packages/location-management/src/components/LocationUnitView/index.tsx
+++ packages/location-management/src/components/LocationUnitView/index.tsx
@@ -75,8 +75,8 @@ const LocationUnitView: React.FC = () => {
                 .read(id)
                 .then((res: RawOpenSRPHierarchy) => {
                   const hierarchy = generateJurisdictionTree(res);
-                  if (hierarchy.model && hierarchy.model.children)
-                    dispatch(fetchAllHierarchies(hierarchy.model));
+                  // if (hierarchy.model && hierarchy.model.children)
+                  dispatch(fetchAllHierarchies(hierarchy.model));
                 })
                 .catch((e) => notification.error({ message: `${e}`, description: '' }));
             });
