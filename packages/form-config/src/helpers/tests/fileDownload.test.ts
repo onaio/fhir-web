001403facc5a749bff8c1cf4302efa7c4688a7f7
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-11-23 17:41:11 +0300

    Resolve failing eslint rules

--- packages/form-config/src/helpers/tests/fileDownload.test.ts
+++ packages/form-config/src/helpers/tests/fileDownload.test.ts
@@ -7,7 +7,9 @@ const testData = JSON.stringify({
 const content = JSON.parse(testData);
 const blob = new Blob([content], { type: 'application/json' });
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 (global as any).URL.createObjectURL = jest.fn();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 (global as any).URL.revokeObjectURL = jest.fn();
 
 describe('fileDownload', () => {
@@ -19,19 +21,23 @@ describe('fileDownload', () => {
   it('should handle download', async () => {
     await handleDownload(testData, 'test-data.json');
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((global as any).URL.createObjectURL).toHaveBeenCalledTimes(1);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((global as any).URL.createObjectURL).toHaveBeenCalledWith(blob);
   });
 
   it('should download file', () => {
     DownloadFile(blob, 'test-data.json');
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((global as any).URL.createObjectURL).toHaveBeenCalledTimes(1);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((global as any).URL.createObjectURL).toHaveBeenCalledWith(blob);
-
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((global as any).URL.revokeObjectURL).toHaveBeenCalledTimes(1);
 
-    expect(document.getElementsByTagName('a').length).toEqual(1);
+    expect(document.getElementsByTagName('a').length).toHaveLength(1);
 
     // find way to test actual download
   });
