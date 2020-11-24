3d49c48bdeb38801d9d242ca5575bd673fec29a1
p-netm <pmuriuki98@gmail.com>
2020-11-24 09:58:44 +0300

    Make routes such that they do not collide

--- packages/product-catalogue/src/constants.ts
+++ packages/product-catalogue/src/constants.ts
@@ -4,9 +4,9 @@ export const OPENSRP_PRODUCT_CATALOGUE = 'product-catalogue';
 export const PRODUCT_ID_ROUTE_PARAM = 'productId';
 
 // router routes
-export const CATALOGUE_LIST_VIEW_URL = '/product-catalogue';
+export const CATALOGUE_LIST_VIEW_URL = '/product-catalogue/list';
 export const CATALOGUE_EDIT_VIEW_URL = '/product-catalogue/edit';
-export const CATALOGUE_CREATE_VIEW_URL = 'product-catalogue/new';
+export const CATALOGUE_CREATE_VIEW_URL = '/product-catalogue/new';
 export const HOME_URL = '/';
 
 // magic strings
