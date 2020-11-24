8b405c23af9cf7a6763af1b8922e3a9bc0d4e826
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-11-24 09:01:04 +0300

    Pass opensrp base url as a prop to package components
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -18,6 +18,7 @@ import {
   BACKEND_ACTIVE,
   KEYCLOAK_API_BASE_URL,
   DISABLE_LOGIN_PROTECTION,
+  OPENSRP_API_BASE_URL,
 } from '../configs/env';
 import {
   REACT_CALLBACK_PATH,
@@ -78,7 +79,11 @@ export const PrivateComponent = ({ component: Component, ...rest }: ComponentPro
     <ConnectedPrivateRoute
       {...rest}
       component={(props: RouteComponentProps) => (
-        <Component {...props} keycloakBaseURL={KEYCLOAK_API_BASE_URL} />
+        <Component
+          {...props}
+          keycloakBaseURL={KEYCLOAK_API_BASE_URL}
+          opensrpBaseURL={OPENSRP_API_BASE_URL}
+        />
       )}
     />
   );
