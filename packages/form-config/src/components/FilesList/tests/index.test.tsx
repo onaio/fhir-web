06c6ba7619c7f11dcef414780c9d24ef8f0a8b51
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-11-25 13:54:26 +0300

    Add tests for component FilesList in package form-config

--- packages/form-config/src/components/FilesList/tests/index.test.tsx
+++ packages/form-config/src/components/FilesList/tests/index.test.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { createBrowserHistory } from 'history';
 import { ManifestFilesList, ConnectedManifestFilesList } from '../index';
-import { getFetchOptions, OpenSRPService } from '@opensrp/server-service';
+import { getFetchOptions } from '@opensrp/server-service';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import reducerRegistry, { store } from '@onaio/redux-reducer-registry';
 import flushPromises from 'flush-promises';
-import filesReducer, { fetchManifestFiles, filesReducerName } from '../../../ducks/manifestFiles';
+import fetch from 'jest-fetch-mock';
+import filesReducer, { filesReducerName, removeManifestFiles } from '../../../ducks/manifestFiles';
 import { fixManifestFiles, downloadFile } from '../../../ducks/tests/fixtures';
 import toJson from 'enzyme-to-json';
 import * as helpers from '../../../helpers/fileDownload';
 import _ from 'lodash';
+import { act } from 'react-dom/test-utils';
 
 /** register the reducers */
 reducerRegistry.register(filesReducerName, filesReducer);
@@ -29,6 +31,7 @@ const props = {
   isJsonValidator: true,
   LoadingComponent: <div></div>,
   uploadTypeUrl: 'manifest-file',
+  customAlert: jest.fn(),
 };
 
 const actualDebounce = _.debounce;
@@ -46,15 +49,18 @@ describe('components/manifestFiles', () => {
     _.debounce = actualDebounce;
   });
 
+  afterEach(() => {
+    store.dispatch(removeManifestFiles());
+    jest.clearAllMocks();
+    fetch.resetMocks();
+  });
+
   it('renders without crashing', () => {
     shallow(<ManifestFilesList {...props} />);
   });
 
   it('renders without crashing when connected to store', async () => {
-    store.dispatch(fetchManifestFiles(fixManifestFiles));
-    const mockList = jest.fn();
-    OpenSRPService.prototype.list = mockList;
-    mockList.mockReturnValueOnce(Promise.resolve(fixManifestFiles));
+    fetch.once(JSON.stringify(fixManifestFiles));
 
     const wrapper = mount(
       <Provider store={store}>
@@ -64,21 +70,21 @@ describe('components/manifestFiles', () => {
       </Provider>
     );
 
-    await flushPromises();
-    wrapper.update();
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
     expect(wrapper.find('DrillDownTable').props()).toMatchSnapshot('isJsonValidator true');
     expect(wrapper.find('SearchBar')).toHaveLength(1);
     expect(wrapper.find('Row Col').at(1).text()).toEqual('Upload New File');
+    wrapper.unmount();
   });
 
   it('download files correctly', async () => {
-    store.dispatch(fetchManifestFiles(fixManifestFiles));
+    fetch.once(JSON.stringify(fixManifestFiles));
+    fetch.once(JSON.stringify(downloadFile));
     const downloadSpy = jest.spyOn(helpers, 'handleDownload');
-    const mockList = jest.fn();
-    OpenSRPService.prototype.list = mockList;
-    mockList
-      .mockReturnValueOnce(Promise.resolve(fixManifestFiles))
-      .mockReturnValueOnce(Promise.resolve(downloadFile));
 
     const wrapper = mount(
       <Provider store={store}>
@@ -88,7 +94,10 @@ describe('components/manifestFiles', () => {
       </Provider>
     );
 
-    await flushPromises();
+    await act(async () => {
+      await flushPromises();
+    });
+
     wrapper.update();
 
     expect(wrapper.find('.tbody .tr')).toHaveLength(fixManifestFiles.length);
@@ -104,28 +113,77 @@ describe('components/manifestFiles', () => {
     expect(toJson(downloadFiledCell)).toMatchSnapshot();
     // click download button
     downloadFiledCell.simulate('click');
-    await flushPromises();
-    expect(downloadSpy).toHaveBeenCalledWith(downloadFile.clientForm.json, 'reveal-test-file.json');
-    // when isJsonValidator download to be called with is_json_validator param
-    expect(mockList.mock.calls[1][0]).toEqual({
-      /* eslint-disable @typescript-eslint/camelcase */
-      form_identifier: 'reveal-test-file.json',
-      form_version: '1.0.27',
-      is_json_validator: true,
+    await act(async () => {
+      await flushPromises();
     });
+
+    wrapper.update();
+
+    expect(downloadSpy).toHaveBeenCalledWith(downloadFile.clientForm.json, 'reveal-test-file.json');
+    expect(fetch.mock.calls[1]).toEqual([
+      'https://test-example.com/restform-download?form_identifier=reveal-test-file.json&form_version=1.0.27&is_json_validator=true',
+      {
+        headers: {
+          accept: 'application/json',
+          authorization: 'Bearer hunter2',
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        method: 'GET',
+      },
+    ]);
+    wrapper.unmount();
   });
 
   it('download with correct params form normal file', async () => {
-    store.dispatch(fetchManifestFiles(fixManifestFiles));
     const downloadSpy = jest.spyOn(helpers, 'handleDownload');
-    const mockList = jest.fn();
-    OpenSRPService.prototype.list = mockList;
-    mockList
-      .mockReturnValueOnce(Promise.resolve(fixManifestFiles))
-      .mockReturnValueOnce(Promise.resolve(downloadFile));
-
+    fetch.once(JSON.stringify(fixManifestFiles));
+    fetch.once(JSON.stringify(downloadFile));
     // file not json validator
-    props.isJsonValidator = false;
+    const normalFileProps = {
+      ...props,
+      isJsonValidator: false,
+    };
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedManifestFilesList {...normalFileProps} />
+        </Router>
+      </Provider>
+    );
+    await act(async () => {
+      await flushPromises();
+    });
+
+    wrapper.update();
+
+    expect(wrapper.find('DrillDownTable').props()).toMatchSnapshot('isJsonValidator false');
+    const downloadFiledCell = wrapper.find('.tbody .tr').at(0).find('.td').at(6).find('a');
+    expect(downloadFiledCell.text()).toEqual('Download');
+    // click download button
+    downloadFiledCell.simulate('click');
+    await act(async () => {
+      await flushPromises();
+    });
+    wrapper.update();
+    expect(downloadSpy).toHaveBeenCalledWith(downloadFile.clientForm.json, 'reveal-test-file.json');
+    expect(fetch.mock.calls[1]).toEqual([
+      'https://test-example.com/restform-download?form_identifier=reveal-test-file.json&form_version=1.0.27',
+      {
+        headers: {
+          accept: 'application/json',
+          authorization: 'Bearer hunter2',
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        method: 'GET',
+      },
+    ]);
+    wrapper.unmount();
+  });
+
+  it('handles failure if fetching mainfest files fails', async () => {
+    fetch.mockRejectOnce(() => Promise.reject('API is down'));
+    fetch.once(JSON.stringify(downloadFile));
 
     const wrapper = mount(
       <Provider store={store}>
@@ -134,21 +192,48 @@ describe('components/manifestFiles', () => {
         </Router>
       </Provider>
     );
-    await flushPromises();
+
+    await act(async () => {
+      await flushPromises();
+    });
     wrapper.update();
 
-    expect(wrapper.find('DrillDownTable').props()).toMatchSnapshot('isJsonValidator false');
-    const downloadFiledCell = wrapper.find('.tbody .tr').at(0).find('.td').at(6).find('a');
-    expect(downloadFiledCell.text()).toEqual('Download');
-    // click download button
-    downloadFiledCell.simulate('click');
-    await flushPromises();
-    expect(downloadSpy).toHaveBeenCalledWith(downloadFile.clientForm.json, 'reveal-test-file.json');
-    // when not json validator is_json_validator param is absent
-    expect(mockList.mock.calls[1][0]).toEqual({
-      /* eslint-disable @typescript-eslint/camelcase */
-      form_identifier: 'reveal-test-file.json',
-      form_version: '1.0.27',
+    expect(props.customAlert).toHaveBeenCalledWith('API is down', { type: 'error' });
+    expect(wrapper.find('.tbody .tr')).toHaveLength(0);
+
+    wrapper.unmount();
+  });
+
+  it('handles download file failure', async () => {
+    fetch.once(JSON.stringify(fixManifestFiles));
+    fetch.mockRejectOnce(() => Promise.reject('Cannot fetch file'));
+    const downloadSpy = jest.spyOn(helpers, 'handleDownload');
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedManifestFilesList {...props} />
+        </Router>
+      </Provider>
+    );
+
+    await act(async () => {
+      await flushPromises();
     });
+    wrapper.update();
+
+    expect(wrapper.find('.tbody .tr')).toHaveLength(fixManifestFiles.length);
+
+    const downloadFiledCell = wrapper.find('.tbody .tr').at(0).find('.td').at(5).find('a');
+    downloadFiledCell.simulate('click');
+
+    await act(async () => {
+      await flushPromises();
+    });
+
+    wrapper.update();
+    expect(props.customAlert).toHaveBeenCalledWith('Cannot fetch file', { type: 'error' });
+    expect(downloadSpy).not.toHaveBeenCalled();
+    wrapper.unmount();
   });
 });
