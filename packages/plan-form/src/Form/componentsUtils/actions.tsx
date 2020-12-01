49db74ed06b1fa0e81d164f302638c973d5b8fe4
p-netm <pmuriuki98@gmail.com>
2020-12-01 16:32:42 +0300

    Initial work for the eusm planform UI

---
+++ packages/plan-form/src/Form/componentsUtils/actions.tsx
@@ -0,0 +1,115 @@
+import { Dictionary } from '@onaio/utils';
+import React from 'react';
+import { DESCRIPTION_LABEL, EXPRESSION_LABEL, NAME } from '../../../lang';
+import { PlanActivityFormFields } from '../../../../opensrp-plans/src/components/JurisdictionSelect/PlanForm/types';
+import { Form, Input } from 'antd';
+
+const { Item: FormItem, List } = Form;
+const { TextArea } = Input;
+
+/**
+ * Returns form components to render triggers and conditions form fields
+ *
+ * @param planActivities - activities from the plan form field values
+ * @param isDisabled - whether to disable the fields or not
+ */
+export const getConditionAndTriggers = (
+  planActivities: PlanActivityFormFields[],
+  isDisabled = true
+) => {
+  const conditions: Dictionary = {};
+  const triggers: Dictionary = {};
+  for (let index = 0; index < planActivities.length; index++) {
+    const element = planActivities[index];
+
+    if (element.condition) {
+      conditions[element.actionCode] = element.condition.map((item, mapIndex) => {
+        return (
+          <FormItem
+            className="condition-group"
+            // row={true}
+            key={`${element.actionCode}-condition-${index}-${mapIndex}`}
+            // name={['activities', index]}
+          >
+            {item.expression && (
+              <FormItem
+                label={EXPRESSION_LABEL}
+                name={[index, 'condition', mapIndex, 'expression']}
+              >
+                <TextArea
+                  className="form-control col-sm-10"
+                  required={true}
+                  id={`activities[${index}].condition[${mapIndex}].expression`}
+                  disabled={isDisabled}
+                />
+              </FormItem>
+            )}
+            {item.description && (
+              <FormItem
+                label={DESCRIPTION_LABEL}
+                name={[index, 'condition', mapIndex, 'description']}
+              >
+                <TextArea
+                  className="form-control col-sm-10"
+                  required={true}
+                  id={`activities[${index}].condition[${mapIndex}].description`}
+                  disabled={isDisabled}
+                />
+              </FormItem>
+            )}
+          </FormItem>
+        );
+      });
+    }
+    if (element.trigger) {
+      triggers[element.actionCode] = element.trigger.map((item, mapIndex) => {
+        return (
+          <FormItem
+            className="trigger-group"
+            key={`${element.actionCode}-trigger-${index}-${mapIndex}`}
+            // name={''}
+          >
+            {item.name && (
+              <FormItem label={NAME} name={[index, 'trigger', mapIndex, 'name']}>
+                <Input
+                  className="form-control col-sm-10"
+                  required={true}
+                  type="text"
+                  id={`activities[${index}].trigger[${mapIndex}].name`}
+                  disabled={isDisabled}
+                />
+              </FormItem>
+            )}
+            {item.expression && (
+              <FormItem label={EXPRESSION_LABEL} name={[index, 'trigger', mapIndex, 'expression']}>
+                <TextArea
+                  className="form-control col-sm-10"
+                  required={true}
+                  id={`activities[${index}].trigger[${mapIndex}].expression`}
+                  disabled={isDisabled}
+                />
+              </FormItem>
+            )}
+            {item.description && (
+              <FormItem
+                label={DESCRIPTION_LABEL}
+                name={[index, 'trigger', mapIndex, 'description']}
+              >
+                <TextArea
+                  className="form-control col-sm-10"
+                  required={false}
+                  id={`activities[${index}].trigger[${mapIndex}].description`}
+                  disabled={isDisabled}
+                />
+              </FormItem>
+            )}
+          </FormItem>
+        );
+      });
+    }
+  }
+  return {
+    conditions,
+    triggers,
+  };
+};
---
+++ packages/plan-form/src/common.tsx
@@ -0,0 +1,7 @@
+export interface CommonProps {
+  baseURL: string;
+}
+
+export const defaultCommonProps = {
+  baseURL: 'https://mg-eusm-stage.smartregister.org/opensrp/',
+};
