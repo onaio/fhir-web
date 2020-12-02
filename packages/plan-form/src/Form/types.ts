a3558921597ff319ef91a6cf335d626e9eddc8f1
p-netm <pmuriuki98@gmail.com>
2020-12-02 17:55:39 +0300

    Add environment configs support

--- packages/plan-form/src/Form/types.ts
+++ packages/plan-form/src/Form/types.ts
@@ -1,8 +1,9 @@
-import { PlanDefinition } from '@opensrp/planform-core';
+import { PlanDefinition, EnvConfig } from '@opensrp/planform-core';
 
 /** called before submission starts, return true to proceed with submission */
 export type BeforeSubmit = (payload: PlanDefinition) => boolean;
 
-
 /** called after submission is successful */
 export type AfterSubmit = (payload: PlanDefinition) => void;
+
+export type PlanFormConfig = EnvConfig;
