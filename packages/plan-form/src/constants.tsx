42128e4968204293aa7d28e01f5656a91dab6129
p-netm <pmuriuki98@gmail.com>
2020-12-02 11:58:01 +0300

    Add constants and string constants

---
+++ packages/plan-form/src/constants.tsx
@@ -0,0 +1,33 @@
+export const A1 = 'A1';
+export const A2 = 'A2';
+export const B1 = 'B1';
+export const B2 = 'B2';
+export const LOW_PRIORITY = 'low-priority';
+export const MEDIUM_PRIORITY = 'medium-priority';
+export const HIGH_PRIORITIY = 'high-priority';
+export const TRUE = 'True';
+export const FALSE = 'False';
+export const DISABLED = 'Disabled';
+export const INTERNAL = 'internal';
+export const IGNORE = 'ignore';
+export const INTERVENTION_TYPE_CODE = 'interventionType';
+export const FI_STATUS_CODE = 'fiStatus';
+export const FI_REASON_CODE = 'fiReason';
+export const OPENSRP_EVENT_ID_CODE = 'opensrpEventId';
+export const CASE_NUMBER_CODE = 'caseNum';
+export const TASK_GENERATION_STATUS_CODE = 'taskGenerationStatus';
+export const TEAM_ASSIGNMENT_STATUS_CODE = 'teamAssignmentStatus';
+export const CASE_TRIGGERED = 'Case Triggered';
+export const ROUTINE = 'Routine';
+export const INVESTIGATION = 'Investigation';
+export const DAYS = 'days';
+export const CONDITION = 'condition';
+export const TRIGGER = 'trigger';
+
+// plan activity codes
+export const CASE_CONFIRMATION_ACTIVITY_CODE = 'caseConfirmation';
+export const FAMILY_REGISTRATION_ACTIVITY_CODE = 'familyRegistration';
+// opensrp api strings
+export const PLAN_LIST_URL = '/plans/list';
+export const OPENSRP_API_BASE_URL = '';
+export const OPENSRP_PLAN_ENDPOINT = 'plans';
