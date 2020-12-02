c1da15660bd8cd9ba0fb7accad057c3e4f7e7103
p-netm <pmuriuki98@gmail.com>
2020-12-02 17:57:19 +0300

    Export string constants

--- packages/planform-core/src/index.tsx
+++ packages/planform-core/src/index.tsx
@@ -3,3 +3,4 @@ export * from './formHelpers/types';
 export * from './formHelpers/settings';
 export * from './formHelpers/activitiesLookup';
 export * from './formHelpers/constants/enumsAndCodeConstants';
+export * from './formHelpers/constants/stringConstants';
