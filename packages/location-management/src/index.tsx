c0d609f0227fb09ceca16d7c7fd1360237d52090
Ashfahan <me@ashfahan.com>
2020-12-03 11:32:25 +0500

    fire remove nested routing

--- packages/location-management/src/index.tsx
+++ packages/location-management/src/index.tsx
@@ -1,17 +1,7 @@
-import LocationUnitModule from './LocationUnitModule';
-import LocationTagModule from './LocationTagModule';
 import LocationUnitAddEdit from './components/LocationUnitAddEdit';
 import LocationUnitView from './components/LocationUnitView';
 import LocationTagView from './components/LocationTagView';
 import LocationTagAddEdit from './components/LocationTagAddEdit';
 import Tree from './components/LocationTree';
 
-export {
-  LocationUnitModule,
-  LocationTagModule,
-  LocationUnitView,
-  LocationUnitAddEdit,
-  LocationTagView,
-  LocationTagAddEdit,
-  Tree,
-};
+export { LocationUnitView, LocationUnitAddEdit, LocationTagView, LocationTagAddEdit, Tree };
