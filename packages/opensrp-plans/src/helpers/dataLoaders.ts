065ec8ad770e3287a9dc4f095e139ed1bce15fb2
Kahummer <pkahama@ona.io>
2020-12-04 10:11:31 +0300

    Add get service hook

---
+++ packages/opensrp-plans/src/helpers/dataLoaders.ts
@@ -0,0 +1,45 @@
+/** get the full product Catalogue
+ */
+
+import { store, makeAPIStateSelector } from '@opensrp/store';
+import { OpenSRPService as GenericOpenSRPService } from '@opensrp/server-service';
+import { OPENSRP_API_BASE_URL, OPENSRP_PLANS } from '../constants';
+import { fetchPlanDefinitions } from '../ducks';
+import { PlanDefinition } from '../plan-global-types';
+
+const sessionSelector = makeAPIStateSelector();
+const accessToken = sessionSelector(store.getState(), { accessToken: true });
+
+/** OpenSRP service */
+export class OpenSRPService extends GenericOpenSRPService {
+  constructor(endpoint: string, baseURL: string = OPENSRP_API_BASE_URL) {
+    super(accessToken, baseURL, endpoint);
+  }
+}
+
+/**
+ * @param {string} baseURL -  base url of api
+ * @param {OpenSRPService} service - the opensrp service
+ * @param {fetchProducts}actionCreator - Action creator; creates actions thad adds products to the store
+ *
+ * @returns {Promise<void>}
+ */
+export async function loadPlans(
+  baseURL: string,
+  service: typeof OpenSRPService = OpenSRPService,
+  actionCreator: typeof fetchPlanDefinitions = fetchPlanDefinitions
+) {
+  const serve = new service(OPENSRP_PLANS, baseURL);
+  return serve
+    .list()
+    .then((response: PlanDefinition[] | null) => {
+      console.log(response);
+      if (response === null || response.length === 0) {
+        return Promise.reject(new Error('No data found'));
+      }
+      actionCreator(response);
+    })
+    .catch((err: Error) => {
+      throw err;
+    });
+}
