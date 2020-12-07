b209545e6e551dfd77710303f67ff0db3ebecda1
Kahummer <pkahama@ona.io>
2020-12-07 15:39:36 +0300

    Remove unnecessary else

--- packages/opensrp-plans/src/components/ListView/utils.tsx
+++ packages/opensrp-plans/src/components/ListView/utils.tsx
@@ -27,9 +27,8 @@ export const columns: ColumnsType<PlanDefinition> = [
         return -1;
       } else if (rec1.name < rec2.name) {
         return 1;
-      } else {
-        return 0;
       }
+      return 0;
     },
     width: '60%',
   },
