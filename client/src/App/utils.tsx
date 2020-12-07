7179a2e07691c72aeb6cedcb5194da3cc0e8d1b6
Kipchirchir Sigei <arapgodsmack@gmail.com>
2020-12-07 10:12:34 +0300

    Pass plan id as props to assignment component
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- client/src/App/utils.tsx
+++ client/src/App/utils.tsx
@@ -1,5 +1,9 @@
-import { OPENSRP_API_BASE_URL } from '../configs/env';
+import { DEFAULT_PLAN_ID, OPENSRP_API_BASE_URL } from '../configs/env';
 
 export const productCatalogueProps = {
   baseURL: OPENSRP_API_BASE_URL,
+};
+
+export const teamAssignmentProps = {
+  defaultPlanId: DEFAULT_PLAN_ID,
 };
