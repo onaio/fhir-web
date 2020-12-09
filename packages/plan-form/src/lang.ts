453048578d12fae906ac0d02812c5cf1b133bcbb
p-netm <pmuriuki98@gmail.com>
2020-12-09 14:24:16 +0300

    Add Planform (#209)
    * Add initial work on planform core
    
    * Make jurisdictions field optional in planform fields
    
    * Refactor some types
    
    * Make configuration env passeable as config object
    
    * Initial commit on the helpers tests
    
    * Update lang.ts constants file
    
    * Refactor date type
    
    * Remove non-planform related stuff from settings
    
    * Remove unused string constants
    
    * Update root yarn.lock file
    
    * Refactor verb in translation file
    
    * Fix isFiorDynamicFi
    
    * Move enums from types file
    
    * Mock date
    
    * Fix regression
    
    * Update yarn.lock
    
    * Initial work for the eusm planform UI
    
    * Add dependencies
    
    * Add types for planForm props
    
    * Refactor formHelpers
    
    * Add common props and variables
    
    * Refactor form index file
    
    Remove suprlus imports, Remove commented out code
    
    * Add constants and string constants
    
    * Link planForm package
    
    * Moce helpers from Forms to utils
    
    * Fix imports
    
    * Add environment configs support
    
    * Update yarn.lock
    
    * Update yarn.lock
    
    * Fix Cannot read property addEventListener of undefined
    
    I do not understand why the problem arose or why & how this fix fixes it,
    
    I found it here: https://stackoverflow.com/a/64872224/14564571
    
    * Add initial tests for eusm planform
    
    * Refactor hidden columns syntax
    
    * Add exported variables
    
    * Add subjectCodableConecpt field in triggers fieldsset
    
    * Update packages
    
    * Add dataloaders tests
    
    * Update import
    
    * Remove unused util functions
    
    * Continue adding tests
    
    * Update yarn.lock
    
    this has been very volatile lately
    
    * Refactor version and description in plan-form
    
    * Update Readme
    
    * Refactor fields for triggers and conditions
    
    * move types from Form to helpers
    
    * Update constants
    
    * Refactor utils
    
    * Refactor imports
    
    * Add tests for Form component
    
    * Remove surplus core folder
    
    * Add coverage for dataloaders
    
    * Test adding removing jurisdiction fields
    
    Ofcourse this interaction will never happen on the web and its aimed at increasing coverage
    
    * Add some more tests
    
    * Test before submit prop stops submission
    
    * Test error notification is sent
    
    * Add docstring on why configuring the initialValues is hard
    
    * Add test to type onchange handler
    
    * Add configs to dependency array
    
    * Bump version to prepatch
    
    * Fix config variable in dependency array
    
    * Refactor magic string and dependency array
    
    * Tweak form styles and default hidden fields
    
    * Update yarn.lock
    
    * Fix lint
    
    * Fix typo in readme
    
    * Add docstring to hiddenFields prop
    
    * Update Readme

---
+++ packages/plan-form/src/lang.ts
@@ -0,0 +1,145 @@
+export const A1_DESCRIPTION = 'Indigenous case recorded within the last year.';
+
+export const A1_NAME = 'Active';
+export const A2_DESCRIPTION =
+  'No indigenous case during the last year, but withing the last 3 years.';
+export const A2_NAME = 'Residual Non-Active';
+export const B1_DESCRIPTION = 'Receptive area but no indigenous cases within the last 3 years.';
+export const B1_NAME = 'Cleared Receptive';
+export const B2_DESCRIPTION = 'Non-receptive area.';
+export const B2_NAME = 'Cleared Non-Receptive';
+
+export const BCC_ACTIVITY = 'Behaviour Change Communication';
+export const BCC_ACTIVITY_DESCRIPTION = 'Conduct BCC activity';
+export const BCC_GOAL_DESCRIPTION = 'Complete at least 1 BCC activity for the operational area';
+export const BCC_GOAL_MEASURE = 'BCC Activities Complete';
+
+export const IRS_ACTIVITY = 'Spray Structures';
+export const IRS_ACTIVITY_DESCRIPTION =
+  'Visit each structure in the operational area and attempt to spray';
+export const IRS_GOAL_DESCRIPTION = 'Spray structures in the operational area';
+export const IRS_GOAL_MEASURE = 'Percent of structures sprayed';
+export const BEDNET_ACTIVITY = 'Bednet Distribution';
+export const BEDNET_ACTIVITY_DESCRIPTION =
+  'Visit 100% of residential structures in the operational area and provide nets';
+export const BEDNET_GOAL_MEASURE = 'Percent of residential structures received nets';
+export const BLOOD_SCREENING_ACTIVITY = 'Blood screening';
+export const BLOOD_SCREENING_ACTIVITY_DESCRIPTION =
+  'Visit all residential structures (100% within a 1 km radius of a confirmed index case and test each registered person';
+export const BLOOD_SCREENING_GOAL_MEASURE = 'Number of registered people tested';
+export const CASE_CONFIRMATION_ACTIVITY = 'Case Confirmation';
+export const CASE_CONFIRMATION_ACTIVITY_DESCRIPTION = 'Confirm the index case';
+export const CASE_CONFIRMATION_ACTIVITY_GOAL_MEASURE = 'Number of cases confirmed';
+
+export const REGISTER_FAMILY_ACTIVITY = 'Family Registration';
+export const REGISTER_FAMILY_ACTIVITY_DESCRIPTION =
+  'Register all families & family members in all residential structures enumerated (100% within the operational area';
+export const REGISTER_FAMILY_ACTIVITY_GOAL_MEASURE =
+  'Percent of residential structures with full family registration';
+export const LARVAL_DIPPING_ACTIVITY = 'Larval Dipping';
+export const LARVAL_DIPPING_ACTIVITY_DESCRIPTION =
+  'Perform a minimum of three larval dipping activities in the operational area';
+export const LARVAL_DIPPING_GOAL_MEASURE = 'Number of larval dipping activities completed';
+
+export const MOSQUITO_COLLECTION_ACTIVITY = 'Mosquito Collection';
+export const MOSQUITO_COLLECTION_ACTIVITY_DESCRIPTION =
+  'Set a minimum of three mosquito collection traps and complete the mosquito collection process';
+export const MOSQUITO_COLLECTION_GOAL_MEASURE =
+  'Number of mosquito collection activities completed';
+
+export const PRODUCT_CHECK_ACTIVITY = 'Product Check';
+export const PRODUCT_CHECK_ACTIVITY_DESCRIPTION =
+  'Check for all products (100% within the jurisdiction';
+export const PRODUCT_CHECK_GOAL_MEASURE = 'Percent of products checked';
+
+export const FIX_PRODUCT_PROBLEM_ACTIVITY = 'Fix Product Problem';
+export const FIX_PRODUCT_PROBLEM_ACTIVITY_DESCRIPTION =
+  'Fix problems for all products (100% within the jurisdiction';
+export const FIX_PRODUCT_PROBLEM_GOAL_MEASURE = 'Percent of products problems fixed';
+
+export const RECORD_GPS_ACTIVITY = 'Record GPS';
+export const RECORD_GPS_ACTIVITY_DESCRIPTION =
+  'Record GPS for all service points without GPS within the jurisdiction';
+export const RECORD_GPS_GOAL_MEASURE = 'Percent of GPS recorded';
+
+export const SERVICE_POINT_CHECK_ACTIVITY = 'Service Point';
+export const SERVICE_POINT_CHECK_ACTIVITY_DESCRIPTION =
+  'Conduct checks for all service point (100% within the Jurisdiction';
+export const SERVICE_POINT_CHECK_GOAL_MEASURE = 'Percent of service points checked';
+
+export const MDA_POINT_DISPENSE_ACTIVITY_DESCRIPTION =
+  'Dispense medication to each eligible person';
+export const MDA_POINT_DISPENSE_COLLECTION_GOAL = 'Percent of eligible people';
+export const MDA_POINT_ADVERSE_EFFECT_ACTIVITY_DESCRIPTION =
+  'Report any adverse events from medication';
+export const MDA_POINT_ADVERSE_EFFECT_COLLECTION_GOAL =
+  'Percent of people who reported adverse events';
+export const GOAL_UNIT_ACTIVITY = 'activit(y|ies';
+export const GOAL_UNIT_CASE = 'case(s';
+export const GOAL_UNIT_PERCENT = 'Percent';
+export const GOAL_UNIT_PERSON = 'Person(s';
+export const GOAL_UNIT_UNKNOWN = 'unknown';
+
+export const PLAN_STATUS_ACTIVE = 'active';
+export const PLAN_STATUS_COMPLETE = 'complete';
+export const PLAN_STATUS_DRAFT = 'draft';
+export const PLAN_STATUS_RETIRED = 'retired';
+
+export const DISTRICT = 'District';
+export const CANTON = 'Canton';
+export const VILLAGE = 'Village';
+export const LOW_PRIORITY_LABEL = 'Low Priority';
+export const MEDIUM_PRIORITY_LABEL = 'Medium Priority';
+export const HIGH_PRIORITIY_LABEL = 'High Priority';
+
+export const INVESTIGATION = 'Investigation';
+export const PROVINCE = 'Province';
+export const ROUTINE_TITLE = 'Routine';
+export const CASE_TRIGGERED_TITLE = 'Case Triggered';
+
+export const ACTION = 'Action';
+export const ACTIVITIES_LABEL = 'Activities';
+export const ADD = 'Add';
+export const ADD_ACTIVITY = 'Add Activity';
+export const ADD_CODED_ACTIVITY = 'Add %s Activity';
+export const AN_ERROR_OCCURRED = 'An Error Ocurred';
+export const AND = 'and';
+export const CASE_NUMBER = 'Case Number';
+export const CONDITIONS_LABEL = 'Conditions';
+export const DEFINITION_URI = 'Definition Uri';
+export const DESCRIPTION_LABEL = 'Description';
+export const DYNAMIC_FI_TITLE = 'Dynamic FI';
+export const DYNAMIC_IRS_TITLE = 'Dynamic IRS';
+export const DYNAMIC_MDA_TITLE = 'Dynamic MDA';
+export const SUPPLY_MANAGEMENT_TITLE = 'Supply Management';
+export const START_DATE = 'Start Date';
+export const END_DATE = 'End Date';
+export const FOCUS_AREA_HEADER = 'Focus Area';
+export const FOCUS_CLASSIFICATION_LABEL = 'Focus Classification';
+export const FOCUS_INVESTIGATION_STATUS_REASON = 'Focus Investigation Reason';
+export const GOAL_LABEL = 'Goal';
+export const ADD_FOCUS_INVESTIGATION = 'Add Focus Investigation';
+export const INTERVENTION_TYPE_LABEL = 'Intervention Type';
+export const IRS_TITLE = 'IRS';
+export const LOCATIONS = 'Locations';
+export const MDA_POINT_TITLE = 'MDA Point';
+export const PLAN_TITLE_LABEL = 'Plan Title';
+export const PLAN_START_DATE_LABEL = 'Plan Start Date';
+export const PLAN_END_DATE_LABEL = 'Plan End Date';
+export const QUANTITY_LABEL = 'Quantity';
+export const PRIORITY_LABEL = 'Priority';
+export const REASON_HEADER = 'Reason';
+export const SAVE_PLAN = 'Save Plan';
+export const SELECT_PLACHOLDER = 'Select %s';
+export const STATUS_HEADER = 'Status';
+export const TRIGGERS_LABEL = 'Triggers';
+export const FOCUS_INVESTIGATION = 'Focus Investigation';
+export const DATE_IS_REQUIRED = 'Date is Required';
+export const NAME_IS_REQUIRED = 'Name is Required';
+export const REQUIRED = 'Required';
+export const EXPRESSION_LABEL = 'Expression';
+export const NAME = 'Name';
+export const SUCCESSFULLY_UPDATED = 'Successfully Updated';
+export const SUCCESSFULLY_CREATED = 'Successfully Created';
+export const ACTIVE_DATE_RANGE_LABEL = 'Active date range';
+export const ADD_JURISDICTION = 'Add Jurisdiction';
