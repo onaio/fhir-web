69cbea05eece526dbcb4e841c861ff7aae7f42b6
haseemVD <haseem.haq@venturedive.com>
2020-12-11 12:17:36 +0500

    fix lint issue

--- packages/keycloak-user-management/src/components/Credentials/index.tsx
+++ packages/keycloak-user-management/src/components/Credentials/index.tsx
@@ -140,7 +140,7 @@ const UserCredentials: React.FC<CredentialsPropsTypes> = (props: CredentialsProp
   return (
     <Row className="layout-content">
       <h5 className="mb-3">
-        {CREDENTIALS} | {keycloakUser?.username || '-'}
+        {CREDENTIALS} | {keycloakUser ? keycloakUser.username : ''}
       </h5>
       <Col className="bg-white p-3" span={24}>
         <div className="form-container">
