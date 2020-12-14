8f9cbfc08215c48dc409f00c3ab771a74454314f
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-12-14 11:58:42 +0300

    Reduce snapshot in components/Antd/ReleaseList package form-config

--- packages/form-config/src/components/Antd/ReleaseList/tests/index.test.tsx
+++ packages/form-config/src/components/Antd/ReleaseList/tests/index.test.tsx
@@ -114,7 +114,10 @@ describe('components/Antd/ReleaseList', () => {
     ]);
     const content = wrapper.find('div.layout-content');
     expect(content.find('Title').props()).toMatchSnapshot('title');
-    expect(content.find('Card').props()).toMatchSnapshot('card');
+    expect(content.find('Card').prop('children')).toHaveLength(3);
+    expect(content.find('Card').prop('children')[0]).toMatchSnapshot('search');
+    expect(content.find('Card').prop('children')[1]).toMatchSnapshot('upload button');
+    expect(content.find('Card').prop('children')[2]).toMatchSnapshot('table');
     wrapper.unmount();
   });
 
