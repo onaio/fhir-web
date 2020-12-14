e88c21cb657d0fd2bcdb5155dd2a6f50df8404f7
p-netm <pmuriuki98@gmail.com>
2020-12-14 19:51:24 +0300

    Configure translation for package

---
+++ packages/plan-form/src/mls.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const coreEnJson = require('../locales/core/en.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const coreSwJson = require('../locales/core/sw.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const eusmEnJson = require('../locales/eusm/en.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const eusmSwJson = require('../locales/eusm/sw.json');
+
+// the format to load the resource files: <languageCode>_<projectCode>. in small
+const resources = {
+  en_core: {
+    translation: coreEnJson,
+  },
+  sw_core: {
+    translation: coreSwJson,
+  },
+  en_eusm: {
+    translation: eusmEnJson,
+  },
+  sw_eusm: {
+    translation: eusmSwJson,
+  },
+};
+
+// const configurable:
+const languageCode = 'en';
+const projectLanguageCode = 'eusm';
+
+i18n
+  .use(initReactI18next)
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources,
+    lng: `${languageCode}_${projectLanguageCode}`,
+    fallbackLng: `${languageCode}_${projectLanguageCode}`,
+    debug: true,
+    interpolation: { escapeValue: false },
+  })
+  .catch((err) => err);
+
+export default i18n;
