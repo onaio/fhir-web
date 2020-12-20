d356648c9c9264d9b2c548e2fd9f74b3952b37b1
haseemVD <haseem.haq@venturedive.com>
2020-12-21 00:11:04 +0500

    add table pagination test

--- packages/team-management/src/components/TeamsView/tests/table.test.tsx
+++ packages/team-management/src/components/TeamsView/tests/table.test.tsx
@@ -108,4 +108,19 @@ describe('components/TeamsView/table', () => {
     expect(body.children().first().prop('rowKey')).toBe('4');
     expect(body.children().last().prop('rowKey')).toBe('1');
   });
+
+  it('Should show table pagination options', () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <Table
+          setPractitionersList={() => jest.fn()}
+          setDetail={() => jest.fn()}
+          opensrpBaseURL=""
+          accessToken="hunter 2"
+          data={tableData}
+        />
+      </Router>
+    );
+    expect(wrapper.find('.ant-table-pagination')).toBeTruthy();
+  });
 });
