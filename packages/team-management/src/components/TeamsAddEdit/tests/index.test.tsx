d6d809d549f95facfdff90192ee72004b9c7fb31
Ashfahan <me@ashfahan.com>
2020-12-21 14:47:50 +0500

    :up: update and remove unnecessary tests

--- packages/team-management/src/components/TeamsAddEdit/tests/index.test.tsx
+++ packages/team-management/src/components/TeamsAddEdit/tests/index.test.tsx
@@ -6,15 +6,14 @@ import { store } from '@opensrp/store';
 import flushPromises from 'flush-promises';
 import { act } from 'react-dom/test-utils';
 import { MemoryRouter, Route, Router } from 'react-router';
-import { accessToken, id, intialValue, practitioner, practitioners, team } from './fixtures';
+import { accessToken, id, intialValue, practitioner, team } from './fixtures';
 import fetch from 'jest-fetch-mock';
 
 import TeamsAddEdit, { getPractitonerDetail, getTeamDetail } from '..';
 
 describe('Team-management/TeamsAddEdit/TeamsAddEdit', () => {
   afterEach(() => {
-    jest.resetAllMocks();
-    fetch.resetMocks();
+    fetch.mockClear();
   });
 
   it('renders without crashing', async () => {
@@ -28,46 +27,38 @@ describe('Team-management/TeamsAddEdit/TeamsAddEdit', () => {
       </Provider>
     );
 
+    expect(fetch.mock.calls).toMatchObject([
+      [
+        'https://opensrp-stage.smartregister.org/opensrp/rest/practitioner/',
+        {
+          headers: {
+            accept: 'application/json',
+            authorization: 'Bearer null',
+            'content-type': 'application/json;charset=UTF-8',
+          },
+          method: 'GET',
+        },
+      ],
+    ]);
+
     await act(async () => {
       await flushPromises();
     });
   });
 
   it('renders with id without crashing', async () => {
+    fetch.mockResponse(JSON.stringify(team));
     fetch.mockResponse(JSON.stringify(practitioner));
 
     mount(
       <Provider store={store}>
         <MemoryRouter initialEntries={[{ pathname: `/${id}`, hash: '', search: '', state: {} }]}>
-          <Route path="/:id" component={TeamsAddEdit} />
+          <Route path={'/:id'} component={TeamsAddEdit} />
         </MemoryRouter>
       </Provider>
     );
 
-    await act(async () => {
-      await flushPromises();
-    });
-  });
-
-  it('works correctly with store', async () => {
-    fetch
-      .once(JSON.stringify(practitioners))
-      .once(JSON.stringify(practitioner))
-      .once(JSON.stringify(team));
-
-    mount(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={[{ pathname: `/${id}`, hash: '', search: '', state: {} }]}>
-          <Route path="/:id" component={TeamsAddEdit} />
-        </MemoryRouter>
-      </Provider>
-    );
-
-    await act(async () => {
-      await flushPromises();
-    });
-
-    const fetchCalls = [
+    expect(fetch.mock.calls).toMatchObject([
       [
         'https://opensrp-stage.smartregister.org/opensrp/rest/organization/258b4dec-79d3-546d-9c5c-f172aa7e03b0',
         {
@@ -90,20 +81,11 @@ describe('Team-management/TeamsAddEdit/TeamsAddEdit', () => {
           method: 'GET',
         },
       ],
-      [
-        'https://opensrp-stage.smartregister.org/opensrp/rest/organization/practitioner/258b4dec-79d3-546d-9c5c-f172aa7e03b0',
-        {
-          headers: {
-            accept: 'application/json',
-            authorization: 'Bearer null',
-            'content-type': 'application/json;charset=UTF-8',
-          },
-          method: 'GET',
-        },
-      ],
-    ];
+    ]);
 
-    expect(fetch.mock.calls).toEqual(fetchCalls);
+    await act(async () => {
+      await flushPromises();
+    });
   });
 
   it('test getPractitonerDetail', async () => {
