32ec4420da415f77f469acf8269da0bfd87a2d1c
Ashfahan <me@ashfahan.com>
2020-12-21 17:59:13 +0500

    rewrite test for form to remove obsolute and unnessary tests

--- packages/team-management/src/components/TeamsAddEdit/tests/Form.test.tsx
+++ packages/team-management/src/components/TeamsAddEdit/tests/Form.test.tsx
@@ -9,10 +9,14 @@ import { Router } from 'react-router';
 import { notification } from 'antd';
 import fetch from 'jest-fetch-mock';
 
-import { accessToken, id, intialValue, practitioners, teamPost } from './fixtures';
-import Form, { onSubmit, setTeam } from '../Form';
+import { accessToken, id, intialValue, practitioners } from './fixtures';
+import Form from '../Form';
 
 describe('Team-management/TeamsAddEdit/Form', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
   it('renders without crashing', () => {
     const wrapper = mount(
       <Provider store={store}>
@@ -22,6 +26,7 @@ describe('Team-management/TeamsAddEdit/Form', () => {
       </Provider>
     );
 
+    expect(fetch.mock.calls).toMatchObject([]);
     expect(wrapper.find('form')).toHaveLength(1);
   });
 
@@ -40,112 +45,21 @@ describe('Team-management/TeamsAddEdit/Form', () => {
     );
 
     expect(wrapper.find('Form').prop('initialValue')).toMatchObject(intialValue);
+    expect(wrapper.find('form')).toHaveLength(1);
   });
 
   it('Cancel button', () => {
     const wrapper = mount(
       <Provider store={store}>
         <Router history={history}>
-          <Form
-            id={id}
-            initialValue={intialValue}
-            accessToken={accessToken}
-            practitioner={practitioners}
-          />
+          <Form accessToken={accessToken} practitioner={practitioners} />
         </Router>
       </Provider>
     );
 
+    expect(wrapper.find('form')).toHaveLength(1);
     wrapper.find('button#cancel').simulate('click');
-  });
-
-  it('Create TeamsAddEdit', async () => {
-    const wrapper = mount(
-      <Provider store={store}>
-        <Router history={history}>
-          <Form
-            id={id}
-            initialValue={intialValue}
-            accessToken={accessToken}
-            practitioner={practitioners}
-          />
-        </Router>
-      </Provider>
-    );
-
-    wrapper.find('form').simulate('submit');
-
-    await act(async () => {
-      await flushPromises();
-    });
-  });
-
-  it('Fail onSubmit', async () => {
-    fetch.mockRejectOnce(() => Promise.reject('An error occurred'));
-    const mockNotificationError = jest.spyOn(notification, 'error');
-    onSubmit(practitioners, accessToken, intialValue, id, jest.fn());
-    await act(async () => {
-      await flushPromises();
-    });
-
-    expect(mockNotificationError).toHaveBeenCalledWith({
-      description: undefined,
-      message: 'An error occurred',
-    });
-  });
-
-  it('Create setTeam', async () => {
-    fetch.mockResponse(JSON.stringify({}));
-
-    setTeam(accessToken, teamPost).then(jest.fn, jest.fn);
-
-    await act(async () => {
-      await flushPromises();
-    });
-
-    expect(fetch.mock.calls).toEqual([
-      [
-        'https://opensrp-stage.smartregister.org/opensrp/rest/organization/258b4dec-79d3-546d-9c5c-f172aa7e03b0',
-        {
-          'Cache-Control': 'no-cache',
-          Pragma: 'no-cache',
-          body: fetch.mock.calls[0][1].body,
-          headers: {
-            accept: 'application/json',
-            authorization: 'Bearer token',
-            'content-type': 'application/json;charset=UTF-8',
-          },
-          method: 'PUT',
-        },
-      ],
-    ]);
-  });
-
-  it('Edit setTeam', async () => {
-    fetch.mockResponse(JSON.stringify({}));
-
-    setTeam(accessToken, teamPost, id).then(jest.fn, jest.fn);
-
-    await act(async () => {
-      await flushPromises();
-    });
-
-    expect(fetch.mock.calls).toEqual([
-      [
-        'https://opensrp-stage.smartregister.org/opensrp/rest/organization/258b4dec-79d3-546d-9c5c-f172aa7e03b0',
-        {
-          'Cache-Control': 'no-cache',
-          Pragma: 'no-cache',
-          body: fetch.mock.calls[0][1].body,
-          headers: {
-            accept: 'application/json',
-            authorization: 'Bearer token',
-            'content-type': 'application/json;charset=UTF-8',
-          },
-          method: 'PUT',
-        },
-      ],
-    ]);
+    expect(history.goBack).toBeCalled();
   });
 
   it('Add Practinier field', async () => {
@@ -174,5 +88,30 @@ describe('Team-management/TeamsAddEdit/Form', () => {
     expect(wrapper.find('List[name="practitioners"] div.practitioners_Field')).toHaveLength(2);
     wrapper.find('button.removePractitioner').last().simulate('click');
     expect(wrapper.find('List[name="practitioners"] div.practitioners_Field')).toHaveLength(1);
+  });
+
+  it('fail and test call onsubmit', async () => {
+    const mockNotificationError = jest.spyOn(notification, 'error');
+    fetch.mockReject();
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <Form accessToken={accessToken} practitioner={practitioners} initialValue={intialValue} />
+        </Router>
+      </Provider>
+    );
+
+    expect(wrapper.find('form')).toHaveLength(1);
+    wrapper.find('form').simulate('submit');
+
+    await act(async () => {
+      await flushPromises();
+    });
+
+    expect(mockNotificationError).toHaveBeenCalledWith({
+      description: undefined,
+      message: 'An error occurred',
+    });
   });
 });
