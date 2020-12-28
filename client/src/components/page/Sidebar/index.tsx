ea7a8416a945f1078af6553578da255a5b8cb959
haseemVD <haseem.haq@venturedive.com>
2020-12-28 10:24:07 +0500

    expand menu item

--- client/src/components/page/Sidebar/index.tsx
+++ client/src/components/page/Sidebar/index.tsx
@@ -1,12 +1,12 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import './Sidebar.css';
 import { DashboardOutlined } from '@ant-design/icons';
 import { Dictionary } from '@onaio/utils';
 import { Layout, Menu } from 'antd';
 import Logo from '../../../assets/images/opensrp-logo-color.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import {
   LOCATIONS_UNIT,
   LOCATIONS_UNIT_GROUP,
@@ -63,6 +63,20 @@ const defaultSidebarProps: Partial<SidebarProps> = {
 export const SidebarComponent: React.FC<SidebarProps> = (props: SidebarProps) => {
   const { extraData } = props;
   const { roles } = extraData;
+  const [openKeys, setOpenKeys] = useState(['admin']);
+  let location = useLocation();
+
+  useEffect(() => {
+    setOpenKeys(['admin']);
+    if (location.pathname.indexOf('/admin') !== -1) {
+      setOpenKeys([...openKeys, 'users', location.pathname]);
+    } else if (location.pathname.indexOf('/location') !== -1) {
+      setOpenKeys([...openKeys, 'admin-locations', location.pathname]);
+    } else if (location.pathname !== '/') {
+      setOpenKeys([...openKeys, 'admin-form-config', location.pathname]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Layout.Sider width="275px" className="layout-sider">
@@ -71,25 +85,32 @@ export const SidebarComponent: React.FC<SidebarProps> = (props: SidebarProps) =>
           <img src={Logo} className="img-fluid" alt="" />
         </Link>
       </div>
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" className="menu-dark">
+      <Menu
+        theme="dark"
+        selectedKeys={[location.pathname]}
+        defaultOpenKeys={openKeys}
+        defaultSelectedKeys={[location.pathname]}
+        mode="inline"
+        className="menu-dark"
+      >
         {ENABLE_PLANS && (
           <Menu.SubMenu key="missions" icon={<DashboardOutlined />} title={MISSIONS}>
-            <Menu.Item key="plans-active">
+            <Menu.Item key={ACTIVE_PLANS_LIST_VIEW_URL}>
               <Link to={ACTIVE_PLANS_LIST_VIEW_URL} className="admin-link">
                 {ACTIVE}
               </Link>
             </Menu.Item>
-            <Menu.Item key="plans-draft">
+            <Menu.Item key={DRAFT_PLANS_LIST_VIEW_URL}>
               <Link to={DRAFT_PLANS_LIST_VIEW_URL} className="admin-link">
                 {DRAFT}
               </Link>
             </Menu.Item>
-            <Menu.Item key="plans-complete">
+            <Menu.Item key={COMPLETE_PLANS_LIST_VIEW_URL}>
               <Link to={COMPLETE_PLANS_LIST_VIEW_URL} className="admin-link">
                 {COMPLETE}
               </Link>
             </Menu.Item>
-            <Menu.Item key="plans-trash">
+            <Menu.Item key={TRASH_PLANS_LIST_VIEW_URL}>
               <Link to={TRASH_PLANS_LIST_VIEW_URL} className="admin-link">
                 {TRASH}
               </Link>
@@ -99,20 +120,20 @@ export const SidebarComponent: React.FC<SidebarProps> = (props: SidebarProps) =>
         <Menu.SubMenu key="admin" icon={<DashboardOutlined />} title={ADMIN}>
           {roles && roles.includes('ROLE_EDIT_KEYCLOAK_USERS') && (
             <Menu.SubMenu key="users" title={USERS}>
-              <Menu.Item key="users">
+              <Menu.Item key={URL_ADMIN}>
                 <Link to={URL_ADMIN} className="admin-link">
                   {USER_MANAGEMENT}
                 </Link>
               </Menu.Item>
             </Menu.SubMenu>
           )}
-          <Menu.Item key="teams">
+          <Menu.Item key={URL_TEAMS}>
             <Link to={URL_TEAMS} className="admin-link">
               {TEAMS}
             </Link>
           </Menu.Item>
           {ENABLE_PRODUCT_CATALOGUE && (
-            <Menu.Item key="product-catalogue">
+            <Menu.Item key={CATALOGUE_LIST_VIEW_URL}>
               <Link to={CATALOGUE_LIST_VIEW_URL} className="admin-link">
                 {PRODUCT_CATALOGUE}
               </Link>
@@ -120,12 +141,12 @@ export const SidebarComponent: React.FC<SidebarProps> = (props: SidebarProps) =>
           )}
           {ENABLE_LOCATIONS && (
             <Menu.SubMenu key="admin-locations" title="Locations">
-              <Menu.Item key="locations-unit">
+              <Menu.Item key={URL_LOCATION_UNIT}>
                 <Link to={URL_LOCATION_UNIT} className="admin-link">
                   {LOCATIONS_UNIT}
                 </Link>
               </Menu.Item>
-              <Menu.Item key="locations-unit-group">
+              <Menu.Item key={URL_LOCATION_UNIT_GROUP}>
                 <Link to={URL_LOCATION_UNIT_GROUP} className="admin-link">
                   {LOCATIONS_UNIT_GROUP}
                 </Link>
@@ -134,17 +155,17 @@ export const SidebarComponent: React.FC<SidebarProps> = (props: SidebarProps) =>
           )}
           {ENABLE_FORM_CONFIGURATION && (
             <Menu.SubMenu key="admin-form-config" title={FORM_CONFIGURATIONS}>
-              <Menu.Item key="admin-form-config-releases">
+              <Menu.Item key={URL_MANIFEST_RELEASE_LIST}>
                 <Link to={URL_MANIFEST_RELEASE_LIST} className="admin-link">
                   {MANIFEST_RELEASES}
                 </Link>
               </Menu.Item>
-              <Menu.Item key="admin-form-config-drafts">
+              <Menu.Item key={URL_DRAFT_FILE_LIST}>
                 <Link to={URL_DRAFT_FILE_LIST} className="admin-link">
                   {DRAFT_FILES}
                 </Link>
               </Menu.Item>
-              <Menu.Item key="admin-form-config-json-validators">
+              <Menu.Item key={URL_JSON_VALIDATOR_LIST}>
                 <Link to={URL_JSON_VALIDATOR_LIST} className="admin-link">
                   {JSON_VALIDATORS}
                 </Link>
