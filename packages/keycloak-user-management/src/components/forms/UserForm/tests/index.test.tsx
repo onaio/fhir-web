aed917c83bc06f462157a3b500220e8432793df5
haseemVD <68631902+haseemVD@users.noreply.github.com>
2021-01-11 13:32:22 +0500

    Add name in heading on Edit (#291)
    * add edit unit name
    
    * add user name in header on edit
    
    * add test in location and user mgmt
    
    * revert title from helmet

--- packages/keycloak-user-management/src/components/forms/UserForm/tests/index.test.tsx
+++ packages/keycloak-user-management/src/components/forms/UserForm/tests/index.test.tsx
@@ -319,6 +319,24 @@ describe('forms/UserForm', () => {
     wrapper.unmount();
   });
 
+  it('render correct user name in header', async () => {
+    fetch.mockReject(() => Promise.reject('API is down'));
+    const propEdit = {
+      ...props,
+      initialValues: fixtures.keycloakUser,
+    };
+    const wrapper = mount(<UserForm {...propEdit} />);
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    expect(wrapper.find('.mb-3.header-title').text()).toEqual(
+      `Edit User | ${fixtures.keycloakUser.username}`
+    );
+  });
+
   it('show practitioner toggle if user id and practitioner is passed', async () => {
     const props = {
       initialValues: { id: '123' },
