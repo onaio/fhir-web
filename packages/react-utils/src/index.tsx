3fb48f18fcd2d86d4f82c2662d6a5be44e00fcd4
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-01-12 09:29:42 +0300

    update module exports
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/react-utils/src/index.tsx
+++ packages/react-utils/src/index.tsx
@@ -1,3 +1,5 @@
 export * from './components/BrokenPage';
 export * from './components/Resource404';
 export * from './components/UtilPageExtra';
+export * from './components/SearchBar';
+export * from './ducks';
