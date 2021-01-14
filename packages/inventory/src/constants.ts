7406d8ae19fca6e61d814394235c946a3d7f772e
p-netm <pmuriuki98@gmail.com>
2021-01-14 12:23:44 +0300

    Add string constants for inventory

---
+++ packages/inventory/src/constants.ts
@@ -0,0 +1,10 @@
+// opensrp api strings
+export const OPENSRP_API_BASE_URL = 'https://mg-eusm-staging.smartregister.org/opensrp/rest/';
+
+// router routes
+export const INVENTORY_SERVICE_POINT_LIST_VIEW = '/inventory/list';
+export const INVENTORY_SERVICE_POINT_PROFILE_VIEW = '/inventory/list';
+
+// other constants
+/** namespace for the keys attached to the columns */
+export const TableColumnsNamespace = 'inventory';
