81cae8aec539f69e318e476d5ac063cce3bb0dc0
p-netm <pmuriuki98@gmail.com>
2021-01-14 18:11:06 +0300

    Remove useLoader hook
    Its a bit over-engineered

--- packages/react-utils/src/index.tsx
+++ packages/react-utils/src/index.tsx
@@ -1,5 +1,4 @@
 export * from './components/BrokenPage';
 export * from './components/Resource404';
 export * from './components/UtilPageExtra';
-export * from './hooks/loadingReducer';
 export * from './helpers/dataLoaders';
