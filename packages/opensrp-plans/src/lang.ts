7e893f55a008b31a173dd8936d09d05494046e9e
Kahummer <pkahama@ona.io>
2021-01-15 21:59:54 +0300

    Fix conflicts

--- packages/opensrp-plans/src/lang.ts
+++ packages/opensrp-plans/src/lang.ts
@@ -15,8 +15,21 @@ export const EDIT_PLAN = `Edit mission`;
 export const CREATE_PLAN = `Create new mission`;
 export const NO_STATUS_FOUND = `No Status Found`;
 export const EDIT = 'Edit';
-export const START_DATE = 'Start date';
-export const END_DATE = 'End date';
+export const START_DATE = 'Start Date';
+export const END_DATE = 'End Date';
+
+export const CANCEL = 'Cancel';
+export const SAVE = 'Save';
+export const EDIT_TEAMS = 'Edit teams';
+export const EDIT_AREAS = 'Edit areas';
+export const SELECT = 'Select';
+export const SELECT_TEAMS = 'Select teams';
+export const SELECT_AREAS = 'Select areas';
+export const ACTIVATE_MISSION = 'Activate mission';
+export const SUCCESSFULLY_ACTIVATED_MISSION = 'Successfully activated mission';
+export const FAILED_TO_ACTIVATE_MISSION = 'Activating mission failed';
+export const COULD_NOT_LOAD_ASSIGNMENTS = 'Could not load Assignments';
+
 export const MISSION_DATA = 'Mission data';
 export const DOWNLOAD_MISSION_DATA = 'Download mission data';
 export const SERVICE_POINTS_VISITED = 'Service points visited';
