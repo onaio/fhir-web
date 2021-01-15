fae6f5dc561de93c6c47489e8f0ad707a0812514
p-netm <pmuriuki98@gmail.com>
2021-01-15 17:11:31 +0300

    Add language configuration envs and docs

--- app/src/configs/__mocks__/env.ts
+++ app/src/configs/__mocks__/env.ts
@@ -1,2 +1,4 @@
 export const OPENSRP_API_BASE_URL = 'https://test.smartregister.org/opensrp/rest/';
 export const ENABLE_PRODUCT_CATALOGUE = true;
+export const LANGUAGE_CODE = 'en';
+export const PROJECT_LANGUAGE_CODE = 'eusm';
