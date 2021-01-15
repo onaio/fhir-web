0ad7b00072e32ca3bc41666469a7ba3717ca32af
p-netm <pmuriuki98@gmail.com>
2021-01-15 17:10:50 +0300

    Export type that describes a package's configuration options

--- packages/plan-form/src/index.tsx
+++ packages/plan-form/src/index.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import i18n from './mls';
+export type { PlanFormPackageConfigs } from './helpers/configs';
 export * from './Form/index';
 export * from './helpers/types';
 export * from './helpers/utils';
