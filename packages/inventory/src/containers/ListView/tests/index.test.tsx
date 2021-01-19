369ed78933aa5ced7e56427d253f94e2718b8253
p-netm <pmuriuki98@gmail.com>
2021-01-19 12:53:07 +0300

    Update snapshots

--- packages/inventory/src/containers/ListView/tests/index.test.tsx
+++ packages/inventory/src/containers/ListView/tests/index.test.tsx
@@ -76,9 +76,7 @@ describe('List view Page', () => {
     );
 
     /** loading view */
-    expect(wrapper.text()).toMatchInlineSnapshot(
-      `"Loading ...Fetching locationsPlease wait, while locations are being fetched"`
-    );
+    expect(wrapper.text()).toMatchInlineSnapshot(`""`);
 
     await act(async () => {
       await new Promise((resolve) => setImmediate(resolve));
@@ -118,9 +116,7 @@ describe('List view Page', () => {
     );
 
     /** loading view */
-    expect(wrapper.text()).toMatchInlineSnapshot(
-      `"Loading ...Fetching locationsPlease wait, while locations are being fetched"`
-    );
+    expect(wrapper.text()).toMatchInlineSnapshot(`""`);
 
     await act(async () => {
       await new Promise((resolve) => setImmediate(resolve));
@@ -199,9 +195,7 @@ describe('List view Page', () => {
     );
 
     /** loading view */
-    expect(wrapper.text()).toMatchInlineSnapshot(
-      `"Loading ...Fetching locationsPlease wait, while locations are being fetched"`
-    );
+    expect(wrapper.text()).toMatchInlineSnapshot(`""`);
 
     await act(async () => {
       await new Promise((resolve) => setImmediate(resolve));
