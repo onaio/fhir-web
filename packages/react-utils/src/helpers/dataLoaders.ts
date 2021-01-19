f54b804a4fb08bc4a20b6712d27c75120835d425
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-01-19 18:35:55 +0300

    minor react-utils code cleanup
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/react-utils/src/helpers/dataLoaders.ts
+++ packages/react-utils/src/helpers/dataLoaders.ts
@@ -8,6 +8,11 @@ import { history } from '@onaio/connected-reducer-registry';
 import { refreshToken } from '@onaio/gatekeeper';
 import { getAccessToken, isTokenExpired } from '@onaio/session-reducer';
 import { Dictionary } from '@onaio/utils';
+import {
+  EXPRESS_TOKEN_REFRESH_URL,
+  LOGIN_URL,
+  SESSION_EXPIRED_TEXT,
+} from '../components/constants';
 
 /** OpenSRP service Generic class */
 export class OpenSRPService<T extends object = Dictionary> extends GenericOpenSRPService<T> {
@@ -31,10 +36,10 @@ export const handleSessionOrTokenExpiry = async () => {
   if (isTokenExpired(store.getState())) {
     try {
       // refresh token
-      return await refreshToken('/refresh/token', store.dispatch, {});
+      return await refreshToken(`${EXPRESS_TOKEN_REFRESH_URL}`, store.dispatch, {});
     } catch (e) {
-      history.push('/login');
-      throw new Error('Session Expired');
+      history.push(`${LOGIN_URL}`);
+      throw new Error(`${SESSION_EXPIRED_TEXT}`);
     }
   } else {
     return getAccessToken(store.getState());
