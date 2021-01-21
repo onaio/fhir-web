344628f2b79675719a43a5c448d295dfcd46f4ab
p-netm <pmuriuki98@gmail.com>
2021-01-21 16:47:32 +0300

    Remove surplus ducks module

--- packages/react-utils/src/index.tsx
+++ packages/react-utils/src/index.tsx
@@ -3,4 +3,3 @@ export * from './components/Resource404';
 export * from './components/UtilPageExtra';
 export * from './components/Search';
 export * from './components/Search/utils';
-export * from './ducks';
