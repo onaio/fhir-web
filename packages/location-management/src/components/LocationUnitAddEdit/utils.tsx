9d086dada331c8a781d074d8e0db4411184eb335
p-netm <pmuriuki98@gmail.com>
2021-01-22 09:27:43 +0300

    Add util that generates formValues from locationUnit response

--- packages/location-management/src/components/LocationUnitAddEdit/utils.tsx
+++ packages/location-management/src/components/LocationUnitAddEdit/utils.tsx
@@ -1,5 +1,31 @@
-import { Rule } from 'rc-field-form/lib/interface';
+import { LocationUnit } from 'location-management/src/ducks/location-units';
+import { FormFields } from './Form';
 
-export const validationRules = {
-  isJurisdiction: [{ required: true, type: 'boolean' }] as Rule[],
+export const getLocationFormFields = (
+  location: LocationUnit,
+  instance: FormInstances
+): FormFields => {
+  const formFields = {
+    id: location.id,
+    instance,
+    ...location.properties,
+    locationTags: location.locationTags?.map((loc) => loc.id),
+    geometry: JSON.stringify(location.geometry),
+    type: location.type,
+  };
+
+  // remove the existing formFields
+  delete formFields.serviceTypes;
+
+  const finalFormFields = {
+    ...formFields,
+    serviceTypes: location.properties.serviceTypes?.map((type) => type.name) ?? [],
+  };
+
+  return finalFormFields;
 };
+
+export enum FormInstances {
+  CORE = 'core',
+  EUSM = 'eusm',
+}
