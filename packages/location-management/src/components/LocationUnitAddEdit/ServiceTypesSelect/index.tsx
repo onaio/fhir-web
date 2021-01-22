0004349ef062c3a89f66f062a68db6d9a0b3d5db
p-netm <pmuriuki98@gmail.com>
2021-01-22 09:23:56 +0300

    Change LocationUni.id prop type

---
+++ packages/location-management/src/components/LocationUnitAddEdit/ServiceTypesSelect/index.tsx
@@ -0,0 +1,78 @@
+import { OpenSRPService } from '@opensrp/react-utils';
+import { Select, SelectProps } from 'formik-antd';
+import { loadSettings } from '../../../helpers/dataLoaders';
+import { useEffect, useState } from 'react';
+import {
+  baseURL,
+  SERVICE_TYPES_SETTINGS_ID,
+  SETTINGS_CONFIGURATION_TYPE,
+} from '../../../constants';
+import { sendErrorNotification } from '@opensrp/notifications';
+import React from 'react';
+import { Dictionary } from '@onaio/utils/dist/types/types';
+
+/** props for service types select component */
+export interface ServiceTypeSelectProps extends SelectProps {
+  service: typeof OpenSRPService;
+  baseURL: string;
+}
+
+const defaultServiceTypeProps = {
+  service: OpenSRPService,
+  baseURL: baseURL,
+};
+
+/** describes a single settings object as received from settings api */
+export interface ServiceTypesSetting {
+  key: string;
+  label: string;
+  description: string;
+  uuid: string;
+  settingsId: string;
+  settingIdentifier: typeof SERVICE_TYPES_SETTINGS_ID;
+  settingMetadataId: string;
+  v1Settings: false;
+  resolveSettings: false;
+  documentId: string;
+  serverVersion: number;
+  type: typeof SETTINGS_CONFIGURATION_TYPE;
+}
+
+/** Service type select components, gets options from the api
+ *
+ * @param props - the component props
+ */
+const ServiceTypeSelect = (props: ServiceTypeSelectProps) => {
+  const [loading, setLoading] = useState(true);
+  const [settingsData, setSettingsData] = useState<ServiceTypesSetting[]>([]);
+
+  useEffect(() => {
+    // fetch service type settings
+    loadSettings(SERVICE_TYPES_SETTINGS_ID, props.baseURL, props.service, setSettingsData)
+      .catch((err) => sendErrorNotification(err.message))
+      .finally(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const selectOptions = settingsData.map((setting) => ({
+    value: setting.label,
+  }));
+
+  const selectProps: ServiceTypeSelectProps = {
+    ...props,
+    options: selectOptions,
+    loading,
+  };
+
+  // remove custom props -> will show warning if passed to component
+  delete (selectProps as Dictionary).baseURL;
+  delete (selectProps as Dictionary).service;
+
+  return <Select {...selectProps}></Select>;
+};
+
+ServiceTypeSelect.defaultProps = defaultServiceTypeProps;
+
+export { ServiceTypeSelect };
