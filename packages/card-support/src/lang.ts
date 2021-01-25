4b60f1ccb4a51d43dcc5c31307a16698cd413142
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2021-01-25 11:06:18 +0300

    Add tests for opensrp/pkg-config, remove unnecessary tests for mls.tsx files

--- packages/card-support/src/lang.ts
+++ packages/card-support/src/lang.ts
@@ -2,12 +2,12 @@ import i18n from './mls';
 
 export const ERROR_OCCURRED = i18n.t('An error occurred');
 export const DOWNLOAD_CLIENT_DATA = i18n.t('Download Client Data');
-export const CLIENT_LOCATION = i18n.t('Client location');
-export const ALL_LOCATIONS = i18n.t('All locations');
+export const CLIENT_LOCATION = i18n.t('Client Location');
+export const ALL_LOCATIONS = i18n.t('All Locations');
 export const CARD_STATUS = i18n.t('Card Status');
 export const NEEDS_CARD = i18n.t('Needs Card');
 export const CARD_NOT_NEEDED = i18n.t('Card not needed');
-export const BOTH_NEEDS_CARD_CARD_NOT_NEEDED = i18n.t('Both "Needs card" and "Card not needed');
+export const BOTH_NEEDS_CARD_CARD_NOT_NEEDED = i18n.t('Both "Needs card" and "Card not needed"');
 export const CARD_ORDER_DATE = i18n.t('Card Order Date');
 export const CARD_ORDER_DATE_REQUIRED = i18n.t('Please enter start date and end date');
 export const SELECT_CARD_ORDER_DATE = i18n.t('Select Card Order Date');
