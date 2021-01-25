eec37555ba28d69ab1260dcc8176361d6d8a3705
Ashfahan <me@ashfahan.com>
2021-01-25 11:46:08 +0500

    revert geo location fix in this pr

--- packages/location-management/src/components/LocationUnitAddEdit/Form.tsx
+++ packages/location-management/src/components/LocationUnitAddEdit/Form.tsx
@@ -91,14 +91,14 @@ export function removeEmptykeys(obj: any) {
  * @param {string} id id of the node
  * @returns {number | null} return geolocation if found else return null
  */
-export function findParentGeoLocation(tree: ParsedHierarchyNode[], id: string): number | undefined {
-  const map: (number | undefined)[] = tree.flatMap((node) => {
+export function findParentGeoLocation(tree: ParsedHierarchyNode[], id: string): number | null {
+  const map: (number | null)[] = tree.flatMap((node) => {
     if (node.id === id) return node.node.attributes.geographicLevel;
     else if (node.children) return findParentGeoLocation(node.children, id);
-    else return undefined;
+    else return null;
   });
 
-  return map.find((e) => e !== undefined);
+  return map.find((e) => e !== null);
 }
 
 /** Handle form submission
