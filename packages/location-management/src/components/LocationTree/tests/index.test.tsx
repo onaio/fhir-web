581780d86d998ae5df342f690ced14e9e862824b
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2021-01-26 09:32:06 +0300

    Resolve failing tests when lang file constants are used in LocationTree

--- packages/location-management/src/components/LocationTree/tests/index.test.tsx
+++ packages/location-management/src/components/LocationTree/tests/index.test.tsx
@@ -2,7 +2,7 @@ import { mount } from 'enzyme';
 import React from 'react';
 import Tree from '../';
 import { treedata } from '../../../ducks/locationHierarchy/tests/hierarchyFixtures';
-import { store } from '@onaio/redux-reducer-registry';
+import { store } from '@opensrp/store';
 import { Provider } from 'react-redux';
 import { act } from 'react-dom/test-utils';
 import flushPromises from 'flush-promises';
