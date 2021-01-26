c465de976cf0026ebf9dfe3e24cb5ef8463fadc9
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2021-01-26 14:37:39 +0300

    Refactor code

--- packages/team-management/src/mls.tsx
+++ packages/team-management/src/mls.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import { getConfigs } from '@opensrp/pkg-config';
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import { initializei18n, LanguageResources } from '@opensrp/pkg-config';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const coreFrJson = require('../locales/core/fr.json');
@@ -9,7 +8,7 @@ const coreFrJson = require('../locales/core/fr.json');
 const coreArJson = require('../locales/core/ar.json');
 
 // the format to load the resource files: <languageCode>_<projectCode>. in small
-const resources = {
+const resources: LanguageResources = {
   fr_core: {
     translation: coreFrJson,
   },
@@ -18,20 +17,6 @@ const resources = {
   },
 };
 
-// const configurable:
-const languageCode = getConfigs().languageCode ?? 'en';
-const projectLanguageCode = getConfigs().projectLanguageCode ?? 'core';
-
-i18n
-  .use(initReactI18next)
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    resources,
-    lng: `${languageCode}_${projectLanguageCode}`,
-    fallbackLng: `${languageCode}_${projectLanguageCode}`,
-    interpolation: { escapeValue: false },
-    returnEmptyString: false,
-  })
-  .catch((err) => err);
+initializei18n(resources);
 
 export default i18n;
