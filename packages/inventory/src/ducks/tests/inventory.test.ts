9367584df7fcae64b69e64e342753b9d23dc1e7b
haseemVD <haseem.haq@venturedive.com>
2021-01-28 17:53:46 +0500

    add inventory ducks

---
+++ packages/inventory/src/ducks/tests/inventory.test.ts
@@ -0,0 +1,54 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import { store } from '@opensrp/store';
+import reducer, {
+  reducerName,
+  getInventoriesById,
+  getInventoryById,
+  getInventoriesArray,
+  getTotalInventories,
+  setTotalInventories,
+  removeInventories,
+  fetchInventories,
+  Inventory,
+} from '../inventory';
+import { inventory1, inventory2 } from './fixtures';
+
+reducerRegistry.register(reducerName, reducer);
+
+describe('src/ducks/inventory', () => {
+  beforeEach(() => {
+    store.dispatch(removeInventories());
+  });
+
+  it('should have initial state', () => {
+    expect(getInventoriesById(store.getState())).toEqual({});
+    expect(getInventoryById(store.getState(), 'someId')).toEqual(null);
+    expect(getInventoriesArray(store.getState())).toEqual([]);
+    expect(getTotalInventories(store.getState())).toEqual(0);
+  });
+
+  it('sets total records correctly', () => {
+    store.dispatch(setTotalInventories(5));
+    expect(getTotalInventories(store.getState())).toEqual(5);
+    store.dispatch(setTotalInventories(10));
+    expect(getTotalInventories(store.getState())).toEqual(10);
+  });
+
+  it('fetches inventory correctly', () => {
+    store.dispatch(fetchInventories([inventory1, inventory2] as Inventory[]));
+    expect(getInventoriesById(store.getState())).toEqual({
+      [inventory1.servicePointId]: inventory1,
+      [inventory2.servicePointId]: inventory2,
+    });
+    expect(getInventoryById(store.getState(), inventory2.servicePointId)).toEqual(inventory2);
+    expect(getInventoriesArray(store.getState())).toEqual([inventory1, inventory2]);
+  });
+
+  it('removes inventory correctly', () => {
+    store.dispatch(fetchInventories([inventory1, inventory2] as Inventory[]));
+    expect(getInventoriesArray(store.getState())).toHaveLength(2);
+
+    store.dispatch(removeInventories());
+    expect(getInventoriesArray(store.getState())).toHaveLength(0);
+  });
+});
