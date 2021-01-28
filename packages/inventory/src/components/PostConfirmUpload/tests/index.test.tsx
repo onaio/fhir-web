97d40c79821d92048e1427b3821c0a4b1306d82b
p-netm <pmuriuki98@gmail.com>
2021-01-28 12:14:18 +0300

    Update post confirm component tests

--- packages/inventory/src/components/PostConfirmUpload/tests/index.test.tsx
+++ packages/inventory/src/components/PostConfirmUpload/tests/index.test.tsx
@@ -6,6 +6,8 @@ describe('post confirmation upload card', () => {
   it('works  correctly', () => {
     const wrapper = mount(<PostConfirmationUpload />);
 
-    expect(wrapper.text()).toMatchInlineSnapshot();
+    expect(wrapper.text()).toMatchInlineSnapshot(
+      `"Inventory is being added to service points…Inventory may take a few minutes to appear."`
+    );
   });
 });
