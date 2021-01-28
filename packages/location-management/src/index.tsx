2a889a4acb68d47e27d8321468f06a67772920dd
p-netm <pmuriuki98@gmail.com>
2021-01-28 20:22:32 +0300

    Re-export some more variables

--- packages/location-management/src/index.tsx
+++ packages/location-management/src/index.tsx
@@ -1,18 +1,20 @@
-import { LocationUnitAddEdit } from './components/LocationUnitAddEdit';
+import { LocationUnitAddEdit, LocationUnitAddEditProps } from './components/LocationUnitAddEdit';
 import LocationUnitView from './components/LocationUnitView';
 import LocationUnitGroupView from './components/LocationUnitGroupView';
 import LocationUnitGroupAddEdit from './components/LocationUnitGroupAddEdit';
 import Tree from './components/LocationTree';
+import { FormInstances } from './components/LocationUnitAddEdit/utils';
 
 export * as locationHierachyDucks from './ducks/location-hierarchy';
 export * from './ducks/types';
-
+export type { LocationUnitAddEditProps };
 export {
   LocationUnitView,
   LocationUnitAddEdit,
   LocationUnitGroupView,
   LocationUnitGroupAddEdit,
   Tree,
+  FormInstances,
 };
 
 export * from './ducks/locationHierarchy';
