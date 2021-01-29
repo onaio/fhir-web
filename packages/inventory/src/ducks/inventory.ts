85bf0d2281610cdb94a5f62a22808a73ad882f9b
haseemVD <haseem.haq@venturedive.com>
2021-01-29 16:45:53 +0500

    add doc string | add selector for service point Id

--- packages/inventory/src/ducks/inventory.ts
+++ packages/inventory/src/ducks/inventory.ts
@@ -33,10 +33,15 @@ export const inventoryReducer = reducerFactory<Inventory>(inventoryReducerName);
 
 // action
 /** actionCreator returns action to to add Item records to store */
-export const fetchInventories = fetchActionCreatorFactory<Inventory>(
+export const fetchInventoriesByStockId = fetchActionCreatorFactory<Inventory>(
   inventoryReducerName,
   'stockId'
 );
+export const fetchInventoriesByServicePointId = fetchActionCreatorFactory<Inventory>(
+  inventoryReducerName,
+  'servicePointId'
+);
+
 export const removeInventories = removeActionCreatorFactory(inventoryReducerName);
 export const setTotalInventories = setTotalRecordsFactory(inventoryReducerName);
 
@@ -44,23 +49,38 @@ export const setTotalInventories = setTotalRecordsFactory(inventoryReducerName);
 export const getInventoriesArray = getItemsArrayFactory<Inventory>(inventoryReducerName);
 export const getTotalInventories = getTotalRecordsFactory(inventoryReducerName);
 
-export const getStore = (store: Partial<Store>) => {
+/** gets all trees key'd by the rootNodes id
+ *
+ * @param store - the store
+ */
+export const getInventoriesByIds = (store: Partial<Store>) => {
   return (store as Dictionary)[inventoryReducerName].objectsById as Dictionary<Inventory>;
 };
 
+/** prop filters to customize selector queries */
 interface Filters {
   stockIds?: string[];
 }
 
+/** gets all trees key'd by the rootNodes id
+ *
+ * @param store - the store
+ * @param props - the filterProps
+ */
 const getIds = (store: Partial<Store>, props: Filters) => props.stockIds;
 
-export const getInventoriesByIds = createSelector(getStore, getIds, (inventoriesByIds, ids) => {
-  if (ids) {
-    const inventoriesOfInterest: Inventory[] = [];
-    ids.forEach((id) => {
-      inventoriesOfInterest.push(inventoriesByIds[id]);
-    });
-    return inventoriesOfInterest;
+/** factory that returns a selector to retrieve the inventories using stockId or servicePointId ids */
+export const getInventoriesByIdsFactory = createSelector(
+  getInventoriesByIds,
+  getIds,
+  (inventoriesByIds, ids) => {
+    if (ids) {
+      const inventoriesOfInterest: Inventory[] = [];
+      ids.forEach((id) => {
+        inventoriesOfInterest.push(inventoriesByIds[id]);
+      });
+      return inventoriesOfInterest;
+    }
+    return values(inventoriesByIds);
   }
-  return values(inventoriesByIds);
-});
+);
