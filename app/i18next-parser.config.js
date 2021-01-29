7559bf6e703b4c0be1d78ed08c08227a8b8b4b55
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2021-01-29 14:45:44 +0300

    Translate package inventory

--- app/i18next-parser.config.js
+++ app/i18next-parser.config.js
@@ -5,5 +5,5 @@ const configs = require('../i18next-parser.config');
 
 module.exports = {
   ...configs,
-  output: 'src/locales/eusm/$LOCALE.json',
+  output: 'src/locales/core/$LOCALE.json',
 };
--- packages/inventory/src/lang.ts
+++ packages/inventory/src/lang.ts
@@ -1,11 +1,15 @@
-export const ADD_SERVICE_POINT = '+ Add service point';
-export const SERVICE_POINT_INVENTORY = 'Service point inventory';
-export const SERVICE_POINT_TH = 'Service point';
-export const TYPE_TH = 'type';
-export const LOCATION_TH = 'Location';
-export const SERVICE_POINT_ID_TH = 'Service point ID';
-export const ACTIONS_TH = 'Actions';
-export const VIEW = 'view';
-export const FETCHING_LOCATIONS = 'Fetching locations';
-export const FETCHING_LOCATIONS_DESCRIPTION = 'Please wait, while locations are being fetched';
-export const LOADING_ELLIPSIS = 'Loading ...';
+import i18n from './mls';
+
+export const ADD_SERVICE_POINT = i18n.t('+ Add service point');
+export const SERVICE_POINT_INVENTORY = i18n.t('Service point inventory');
+export const SERVICE_POINT_TH = i18n.t('Service point');
+export const TYPE_TH = i18n.t('type');
+export const LOCATION_TH = i18n.t('Location');
+export const SERVICE_POINT_ID_TH = i18n.t('Service point ID');
+export const ACTIONS_TH = i18n.t('Actions');
+export const VIEW = i18n.t('view');
+export const FETCHING_LOCATIONS = i18n.t('Fetching locations');
+export const FETCHING_LOCATIONS_DESCRIPTION = i18n.t(
+  'Please wait, while locations are being fetched'
+);
+export const LOADING_ELLIPSIS = i18n.t('Loading ...');
