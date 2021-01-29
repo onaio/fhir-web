f46ec3144d8f26a92b4145ed28161ff326de2a8e
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2021-01-29 15:24:31 +0300

    Configure MLS for product-catalogue

--- packages/product-catalogue/src/lang.ts
+++ packages/product-catalogue/src/lang.ts
@@ -1,36 +1,42 @@
-export const ADD_PRODUCT = 'Add product to catalogue';
-export const EDIT = 'Edit';
-export const PRODUCT_CATALOGUE = 'Product Catalogue';
-export const ADD_PRODUCT_TO_CATALOGUE = ' + Add product to catalogue';
-export const PRODUCT_NAME_TH = 'Product Name';
-export const ID_TH = 'ID';
-export const ACTIONS_TH = 'Actions';
-export const LOADING_ELLIPSIS = 'Loading...';
-export const FETCHING_PRODUCT = 'Fetching product Catalogue';
-export const FETCHING_PRODUCT_DESCRIPTION = 'Please wait, as we fetch the product Catalogue.';
-export const REQUIRED = 'Required';
-export const YES = 'yes';
-export const NO = 'no';
-export const SUCCESSFULLY_UPDATED = 'Successfully Updated';
-export const SUCCESSFULLY_ADDED = 'Successfully Added';
+import i18n from './mls';
+
+export const ADD_PRODUCT = i18n.t('Add product to catalogue');
+export const EDIT = i18n.t('Edit');
+export const PRODUCT_CATALOGUE = i18n.t('Product Catalogue');
+export const ADD_PRODUCT_TO_CATALOGUE = i18n.t(' + Add product to catalogue');
+export const PRODUCT_NAME_TH = i18n.t('Product Name');
+export const ID_TH = i18n.t('ID');
+export const ACTIONS_TH = i18n.t('Actions');
+export const LOADING_ELLIPSIS = i18n.t('Loading...');
+export const FETCHING_PRODUCT = i18n.t('Fetching product Catalogue');
+export const FETCHING_PRODUCT_DESCRIPTION = i18n.t(
+  'Please wait, as we fetch the product Catalogue.'
+);
+export const REQUIRED = i18n.t('Required');
+export const YES = i18n.t('yes');
+export const NO = i18n.t('no');
+export const SUCCESSFULLY_UPDATED = i18n.t('Successfully Updated');
+export const SUCCESSFULLY_ADDED = i18n.t('Successfully Added');
 export const ENTER_PRODUCTS_NAME = "Enter the product's name e.g Midwifery Kit";
-export const PRODUCT_NAME = 'Product name';
-export const UNIQUE_ID = 'Unique ID';
-export const MATERIAL_NUMBER = 'Material number';
-export const CREATED = 'Created';
-export const LAST_UPDATED = 'Last Updated';
-export const SERVER_VERSION = 'Server version';
-export const CANCEL = 'Cancel';
-export const PHOTO_OF_THE_PRODUCT = 'Photo of the product (optional)';
-export const ACCOUNTABILITY_PERIOD = 'Accountability period (in months)';
+export const PRODUCT_NAME = i18n.t('Product name');
+export const UNIQUE_ID = i18n.t('Unique ID');
+export const MATERIAL_NUMBER = i18n.t('Material number');
+export const CREATED = i18n.t('Created');
+export const LAST_UPDATED = i18n.t('Last Updated');
+export const SERVER_VERSION = i18n.t('Server version');
+export const CANCEL = i18n.t('Cancel');
+export const PHOTO_OF_THE_PRODUCT = i18n.t('Photo of the product (optional)');
+export const ACCOUNTABILITY_PERIOD = i18n.t('Accountability period (in months)');
 export const DESCRIBE_THE_PRODUCTS_USE = "Describe the product's intended use at the service point";
-export const USED_APPROPRIATELY = 'Is it being used appropriately? (optional)';
-export const CONDITION_PLACEHOLDER =
-  'Describe how a supply monitor would assess whether the product is in good condition';
-export const CONDITION_LABEL = 'Is it in good condition? (optional)';
-export const AVAILABILITY_PLACEHOLDER =
-  'Describe where a supply monitor can locate this product at the service point.';
-export const AVAILABILITY_LABEL = 'Is it there?';
-export const ATTRACTIVE_ITEM_LABEL = 'Attractive item?';
+export const USED_APPROPRIATELY = i18n.t('Is it being used appropriately? (optional)');
+export const CONDITION_PLACEHOLDER = i18n.t(
+  'Describe how a supply monitor would assess whether the product is in good condition'
+);
+export const CONDITION_LABEL = i18n.t('Is it in good condition? (optional)');
+export const AVAILABILITY_PLACEHOLDER = i18n.t(
+  'Describe where a supply monitor can locate this product at the service point.'
+);
+export const AVAILABILITY_LABEL = i18n.t('Is it there?');
+export const ATTRACTIVE_ITEM_LABEL = i18n.t('Attractive item?');
 export const MATERIAL_NUMBER_PLACEHOLDER = "Enter the product's material number";
-export const SUBMIT = 'Submit';
+export const SUBMIT = i18n.t('Submit');
