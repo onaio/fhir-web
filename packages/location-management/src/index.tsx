02196f767cd3d0845c5720faf689241b0b947d42
p-netm <pmuriuki98@gmail.com>
2021-02-01 12:18:02 +0300

    Update exported components

--- packages/location-management/src/index.tsx
+++ packages/location-management/src/index.tsx
@@ -1,22 +1,16 @@
-import { LocationUnitAddEdit, LocationUnitAddEditProps } from './components/LocationUnitAddEdit';
 import LocationUnitView from './components/LocationUnitView';
 import LocationUnitGroupView from './components/LocationUnitGroupView';
 import LocationUnitGroupAddEdit from './components/LocationUnitGroupAddEdit';
 import Tree from './components/LocationTree';
-import { FormInstances } from './components/LocationUnitAddEdit/utils';
+import { FormInstances } from './components/LocationForm/utils';
 
 export * as locationHierachyDucks from './ducks/location-hierarchy';
 export * from './ducks/types';
-export type { LocationUnitAddEditProps };
-export {
-  LocationUnitView,
-  LocationUnitAddEdit,
-  LocationUnitGroupView,
-  LocationUnitGroupAddEdit,
-  Tree,
-  FormInstances,
-};
+export { LocationUnitView, LocationUnitGroupView, LocationUnitGroupAddEdit, Tree, FormInstances };
 
+export * from './components/EditLocationUnit';
+export * from './components/NewLocationUnit';
+export * from './components/LocationForm';
 export * from './ducks/locationHierarchy';
 export * from './ducks/locationHierarchy/utils';
 export * from './helpers/dataLoaders';
