ec0ae90c5fab3b3e9507ea4c665c280387046591
p-netm <pmuriuki98@gmail.com>
2021-02-01 12:08:57 +0300

    Add a lang file

---
+++ packages/location-management/src/lang.ts
@@ -0,0 +1,3 @@
+export const SUCCESSFULLY_CREATED_LOCATION = 'Location was successfully created';
+export const SUCCESSFULLY_UPDATED_LOCATION = 'Location was successfully updated';
+export const CANCEL = 'Cancel';
