f1d532f7970ffc5555c0c644a1c4d70401f870b4
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-02-02 07:55:46 +0300

    move menu schema to utils
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- app/src/components/page/Sidebar/index.tsx
+++ app/src/components/page/Sidebar/index.tsx
@@ -2,59 +2,13 @@
 import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import './Sidebar.css';
-import { DashboardOutlined, IdcardOutlined } from '@ant-design/icons';
 import { Dictionary } from '@onaio/utils';
 import { Layout, Menu } from 'antd';
 import Logo from '../../../assets/images/opensrp-logo-color.png';
 import { Link, useLocation } from 'react-router-dom';
-import {
-  LOCATIONS_UNIT,
-  LOCATIONS_UNIT_GROUP,
-  PRODUCT_CATALOGUE,
-  TEAMS,
-  URL_USER,
-  URL_HOME,
-  URL_LOCATION_UNIT_GROUP,
-  URL_TEAMS,
-  URL_DOWNLOAD_CLIENT_DATA,
-  URL_LOCATION_UNIT,
-  USER_MANAGEMENT,
-  URL_JSON_VALIDATOR_LIST,
-  URL_DRAFT_FILE_LIST,
-  URL_MANIFEST_RELEASE_LIST,
-  FORM_CONFIGURATION,
-  MANIFEST_RELEASES,
-  DRAFT_FILES,
-  JSON_VALIDATORS,
-  USERS,
-  ADMIN,
-  ACTIVE,
-  DRAFT,
-  COMPLETE,
-  TRASH,
-  MISSIONS,
-  SERVICE_POINT_INVENTORY,
-  INVENTORY,
-  URL_ADMIN,
-} from '../../../constants';
-import { CATALOGUE_LIST_VIEW_URL } from '@opensrp/product-catalogue';
-import {
-  ENABLE_FORM_CONFIGURATION,
-  ENABLE_PLANS,
-  ENABLE_TEAMS,
-  ENABLE_LOCATIONS,
-  ENABLE_PRODUCT_CATALOGUE,
-  ENABLE_CARD_SUPPORT,
-  ENABLE_INVENTORY,
-} from '../../../configs/env';
-import {
-  ACTIVE_PLANS_LIST_VIEW_URL,
-  DRAFT_PLANS_LIST_VIEW_URL,
-  COMPLETE_PLANS_LIST_VIEW_URL,
-  TRASH_PLANS_LIST_VIEW_URL,
-} from '@opensrp/plans';
-import { INVENTORY_SERVICE_POINT_LIST_VIEW } from '@opensrp/inventory';
-import { getActiveKey } from './utils';
+import { URL_HOME } from '../../../constants';
+
+import { getActiveKey, menusSchema } from './utils';
 
 /** interface for SidebarProps */
 export interface SidebarProps extends RouteComponentProps {
@@ -89,147 +43,9 @@ export const SidebarComponent: React.FC<SidebarProps> = (props: SidebarProps) =>
   let loc = location.pathname.split('/');
   const [openKeys, setOpenKeys] = React.useState<React.ReactText[]>([]);
 
-  // menu items schema
-  const menus: MenuItems[] = [
-    {
-      otherProps: { icon: <DashboardOutlined />, title: `${MISSIONS}` },
-      key: 'missions',
-      enabled: ENABLE_PLANS,
-      children: [
-        {
-          otherProps: { title: `${ACTIVE}` },
-          url: `${ACTIVE_PLANS_LIST_VIEW_URL}`,
-          key: 'missions-active',
-          children: [],
-        },
-        {
-          otherProps: { title: `${DRAFT}` },
-          url: `${DRAFT_PLANS_LIST_VIEW_URL}`,
-          key: 'missions-draft',
-          children: [],
-        },
-        {
-          otherProps: { title: `${COMPLETE}` },
-          url: `${COMPLETE_PLANS_LIST_VIEW_URL}`,
-          key: 'missions-complete',
-          children: [],
-        },
-        {
-          otherProps: { title: `${TRASH}` },
-          url: `${TRASH_PLANS_LIST_VIEW_URL}`,
-          key: 'missions-trash',
-          children: [],
-        },
-      ],
-    },
-    {
-      otherProps: { icon: <DashboardOutlined />, title: `${INVENTORY}` },
-      key: 'inventory',
-      enabled: ENABLE_INVENTORY,
-      children: [
-        {
-          otherProps: { title: `${SERVICE_POINT_INVENTORY}` },
-          url: `${INVENTORY_SERVICE_POINT_LIST_VIEW}`,
-          key: 'inventory-list',
-          children: [],
-        },
-      ],
-    },
-    {
-      otherProps: { icon: <DashboardOutlined />, title: `${ADMIN}` },
-      key: 'admin',
-      enabled: true,
-      url: '/admin',
-      children: [
-        {
-          otherProps: { title: `${USERS}` },
-          key: 'users',
-          enabled: roles && roles.includes('ROLE_EDIT_KEYCLOAK_USERS'),
-          children: [
-            {
-              otherProps: { icon: '', title: `${USER_MANAGEMENT}` },
-              key: 'user-management',
-              url: `${URL_USER}`,
-              children: [],
-            },
-          ],
-        },
-        {
-          otherProps: { icon: '', title: 'Locations' },
-          key: 'location',
-          enabled: ENABLE_LOCATIONS,
-          children: [
-            {
-              otherProps: { icon: '', title: `${LOCATIONS_UNIT}` },
-              url: `${URL_LOCATION_UNIT}`,
-              key: 'location-unit',
-              children: [],
-            },
-            {
-              otherProps: { icon: '', title: `${LOCATIONS_UNIT_GROUP}` },
-              url: `${URL_LOCATION_UNIT_GROUP}`,
-              key: 'location-group',
-              children: [],
-            },
-          ],
-        },
-        {
-          otherProps: { icon: '', title: `${PRODUCT_CATALOGUE}` },
-          key: 'product-catalogue',
-          enabled: ENABLE_PRODUCT_CATALOGUE,
-          url: `${URL_ADMIN}${CATALOGUE_LIST_VIEW_URL}`,
-          children: [],
-        },
-        {
-          otherProps: { icon: '', title: `${TEAMS}` },
-          key: 'teams',
-          enabled: ENABLE_TEAMS,
-          url: `${URL_TEAMS}`,
-          children: [],
-        },
-        {
-          otherProps: { icon: '', title: `${FORM_CONFIGURATION}` },
-          key: 'form-config',
-          enabled: ENABLE_FORM_CONFIGURATION,
-          children: [
-            {
-              otherProps: { icon: '', title: `${MANIFEST_RELEASES}` },
-              key: 'form-config-releases',
-              url: `${URL_MANIFEST_RELEASE_LIST}`,
-              children: [],
-            },
-            {
-              otherProps: { icon: '', title: `${DRAFT_FILES}` },
-              key: 'form-config-draft',
-              url: `${URL_DRAFT_FILE_LIST}`,
-              children: [],
-            },
-            {
-              otherProps: { icon: '', title: `${JSON_VALIDATORS}` },
-              key: 'form-config-validators',
-              url: `${URL_JSON_VALIDATOR_LIST}`,
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      otherProps: { icon: <IdcardOutlined />, title: 'Card Support' },
-      key: 'card-support',
-      enabled: ENABLE_CARD_SUPPORT,
-      children: [
-        {
-          otherProps: { title: 'Download Client Data' },
-          url: `${URL_DOWNLOAD_CLIENT_DATA}`,
-          key: 'download-client-data',
-          children: [],
-        },
-      ],
-    },
-  ];
-
   const mainMenu: JSX.Element[] = [];
+
+  const menus = menusSchema(roles);
 
   const mapChildren = React.useCallback((child: MenuItems) => {
     if (child.children.length) {
