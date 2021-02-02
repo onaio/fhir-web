e95605e09272f27808ec284c4136ab0463ae0ef4
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-02-02 08:58:42 +0300

    Fix failing tests
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/keycloak-user-management/src/components/UserList/TableActions/utils.tsx
+++ packages/keycloak-user-management/src/components/UserList/TableActions/utils.tsx
@@ -7,7 +7,6 @@ import { KEYCLOAK_URL_USERS, USER_DELETED_SUCCESSFULLY, ERROR_OCCURED } from '..
  * Delete keycloak user
  *
  * @param {Function} removeKeycloakUsersCreator - remove users action creator
- * @param {string} accessToken - access token
  * @param {string} keycloakBaseURL - keycloak api base URL
  * @param {string} userId - id of user to be deleted
  * @param {Function} isLoadingCallback - callback function that sets loading state
@@ -15,7 +14,6 @@ import { KEYCLOAK_URL_USERS, USER_DELETED_SUCCESSFULLY, ERROR_OCCURED } from '..
  */
 export const deleteUser = (
   removeKeycloakUsersCreator: typeof removeKeycloakUsers,
-  accessToken: string,
   keycloakBaseURL: string,
   userId: string,
   isLoadingCallback: (loading: boolean) => void
