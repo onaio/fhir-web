8fe41108207ddac1e0a0817a0323dc861101f596
p-netm <pmuriuki98@gmail.com>
2021-02-02 14:24:49 +0300

    Make loadJurisdictions data helper endpoint configurable

--- packages/location-management/src/helpers/dataLoaders.ts
+++ packages/location-management/src/helpers/dataLoaders.ts
@@ -77,25 +77,32 @@ export const defaultParamFilters: ParamFilters = {
   geographicLevel: 0,
 };
 
-/** loader function to get jurisdictions by geographic level
+/**
+ * loader function to get jurisdictions by geographic level
  *
  * @param dispatcher - called with response, adds data to store
  * @param openSRPBaseURL - the openSRP api base url
  * @param urlParams - search params to be added to request
  * @param filterParams - filterParams for property_filter search_param
  * @param service - openSRP service class
+ * @param endpoint - the openSRP endpoint
  */
 export async function loadJurisdictions(
   dispatcher?: typeof fetchLocationUnits,
   openSRPBaseURL: string = baseURL,
   urlParams: GetLocationParams = defaultGetLocationParams,
   filterParams: ParamFilters = defaultParamFilters,
-  service: typeof OpenSRPService = OpenSRPService
+  service: typeof OpenSRPService = OpenSRPService,
+  endpoint: string = LOCATION_UNIT_FIND_BY_PROPERTIES
 ) {
-  const serve = new service(LOCATION_UNIT_FIND_BY_PROPERTIES, openSRPBaseURL);
+  const serve = new service(endpoint, openSRPBaseURL);
+  const filterParamsObject =
+    Object.values(filterParams).length > 0
+      ? { properties_filter: service.getFilterParams(filterParams as Dictionary) }
+      : {};
   const params = {
     ...urlParams,
-    properties_filter: service.getFilterParams(filterParams as Dictionary),
+    ...filterParamsObject,
   } as Dictionary;
   return serve
     .list(params)
