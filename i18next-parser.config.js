85db1a6b03fb9527a9c6c8b5c81d955b06c14e82
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2021-02-04 14:36:22 +0300

    Use key as default value plan-form translation

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // Namespace separator used in your translation keys
   // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-  output: 'locales/core/$LOCALE.json',
+  output: 'locales/eusm/$LOCALE.json',
   // Supports $LOCALE and $NAMESPACE injection
   // Supports JSON (.json) and YAML (.yml) file formats
   // Where to write the locale files relative to process.cwd()
@@ -68,7 +68,7 @@ module.exports = {
   sort: false,
   // Whether or not to sort the catalog
 
-  useKeysAsDefaultValue: false,
+  useKeysAsDefaultValue: true,
   // Whether to use the keys as the default value; ex. "Hello": "Hello", "World": "World"
   // The option `defaultValue` will not work if this is set to true
 
