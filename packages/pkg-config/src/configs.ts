0550981893cdf3583414d6eeab6009a76073ec36
p-netm <pmuriuki98@gmail.com>
2021-02-16 09:24:31 +0300

    Change default namespace separator

--- packages/pkg-config/src/configs.ts
+++ packages/pkg-config/src/configs.ts
@@ -51,6 +51,7 @@ export const initializei18n = (i18next: typeof i18n, opensrpResources: LanguageR
       fallbackLng: `${languageCode}_${projectLanguageCode}`,
       interpolation: { escapeValue: false },
       returnEmptyString: false,
+      nsSeparator: '::'
     })
     .catch((err) => err);
 };
