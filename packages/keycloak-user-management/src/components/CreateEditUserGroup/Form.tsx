15a3208b2c5ee90a8f32ace0cfaa67ba2982ddf0
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-02-22 17:37:17 +0300

    Add data loaders to fetch assigned/available roles
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/keycloak-user-management/src/components/CreateEditUserGroup/Form.tsx
+++ packages/keycloak-user-management/src/components/CreateEditUserGroup/Form.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
-import { Button, Col, Row, Form, Input } from 'antd';
+import { Button, Col, Row, Form, Input, Transfer } from 'antd';
 import { KeycloakService } from '@opensrp/keycloak-service';
 import { sendErrorNotification, sendSuccessNotification } from '@opensrp/notifications';
 import { KEYCLOAK_URL_USER_GROUPS, URL_USER_GROUPS } from '../../constants';
@@ -16,6 +16,7 @@ import {
   ADD_USER_GROUP,
 } from '../../lang';
 import { KeycloakUserGroup } from '../../ducks/userGroups';
+import { fetchAssignedRoles, fetchAvailableRoles } from './utils';
 /** Interface for practitioner json object */
 export interface Practitioner {
   active: boolean;
@@ -52,6 +53,8 @@ export const defaultProps: Partial<UserGroupFormProps> = {
 const UserGroupForm: React.FC<UserGroupFormProps> = (props: UserGroupFormProps) => {
   const { initialValues, keycloakBaseURL } = props;
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [availableRoles, setAvailableRoles] = useState(null);
+  const [assignedRoles, setAssignedRoles] = useState(null);
   const history = useHistory();
   const [form] = Form.useForm();
   const layout = {
@@ -80,6 +83,14 @@ const UserGroupForm: React.FC<UserGroupFormProps> = (props: UserGroupFormProps) 
       ...initialValues,
     });
   }, [form, initialValues]);
+
+  React.useEffect(() => {
+    fetchAvailableRoles(initialValues.id, keycloakBaseURL, setAvailableRoles);
+  }, [initialValues.id, keycloakBaseURL]);
+
+  React.useEffect(() => {
+    fetchAssignedRoles(initialValues.id, keycloakBaseURL, setAssignedRoles);
+  }, [initialValues.id, keycloakBaseURL]);
 
   return (
     <Row className="layout-content">
@@ -123,6 +134,27 @@ const UserGroupForm: React.FC<UserGroupFormProps> = (props: UserGroupFormProps) 
             rules={[{ required: true, message: NAME_REQUIRED }]}
           >
             <Input />
+          </Form.Item>
+          <Form.Item
+            name="roles"
+            id="roles"
+            label="Realm Roles"
+            rules={[{ required: true, message: 'Role Required' }]}
+          >
+            <Transfer
+              dataSource={[]}
+              titles={['Source', 'Target']}
+              targetKeys={[]}
+              selectedKeys={[]}
+              render={() => <div>Test</div>}
+              disabled={false}
+              locale={{
+                itemUnit: 'article',
+                itemsUnit: 'articles',
+                notFoundContent: 'The list is empty',
+                searchPlaceholder: 'Search here',
+              }}
+            />
           </Form.Item>
           <Form.Item {...tailLayout}>
             <Button type="primary" htmlType="submit" className="create-group">
