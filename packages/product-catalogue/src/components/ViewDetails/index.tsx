bad0e25a9ee3364597fe7dae4166f104abe37f57
haseemVD <haseem.haq@venturedive.com>
2021-03-02 18:40:45 +0500

    fix ui for detail panel

--- packages/product-catalogue/src/components/ViewDetails/index.tsx
+++ packages/product-catalogue/src/components/ViewDetails/index.tsx
@@ -1,6 +1,6 @@
 import { ProductCatalogue } from '../../ducks/productCatalogue';
 import React from 'react';
-import { Col, Typography, Space } from 'antd';
+import { Col } from 'antd';
 import { Resource404 } from '@opensrp/react-utils';
 import { Dictionary } from '@onaio/utils';
 import { PRODUCT_NAME, UNIQUE_ID, MATERIAL_NUMBER, SERVER_VERSION } from '../../lang';
@@ -8,8 +8,6 @@ import { CloseOutlined } from '@ant-design/icons';
 import { useHistory } from 'react-router';
 import { Button } from 'antd';
 import { CATALOGUE_LIST_VIEW_URL } from '../../constants';
-
-const { Text } = Typography;
 
 /**
  * helper function that creates a list of key,value pairs.
@@ -64,20 +62,16 @@ const ViewDetails = (props: ViewDetailsProps) => {
       {objectId && !object ? (
         <Resource404></Resource404>
       ) : (
-        <Space direction="vertical">
+        <div className="p-4">
           {extractViewDetails(object as ProductCatalogue).map(([key, val]) => {
             return (
-              <span key={`${key}-${val}`}>
-                <Text strong={true} className="display-block">
-                  {key}
-                </Text>
-                <Text type="secondary" className="display-block">
-                  {val}
-                </Text>
-              </span>
+              <div key={`${key}-${val}`} className="mb-4 small">
+                <p className="mb-0 font-weight-bold">{key}</p>
+                <p className="mb-0">{`${val}`}</p>
+              </div>
             );
           })}
-        </Space>
+        </div>
       )}
     </Col>
   );
