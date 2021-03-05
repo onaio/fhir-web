81070dd3d90b47443aa11384b4fcad4766db9f5e
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-03-05 08:54:56 +0300

    Add teams assignment mls support
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

---
+++ packages/team-assignment/src/lang.ts
@@ -0,0 +1,21 @@
+import i18n from './mls';
+
+// Errors
+export const ERROR_OCCURED = i18n.t('An error occurred');
+export const ENTER_TEAM_NAME = i18n.t('Enter a Team name');
+export const TEAMS = i18n.t('Teams');
+export const SUCCESSFULLY_ASSIGNED_TEAMS = 'Successfully Assigned Teams';
+
+// other constants
+/** namespace for the keys attached to the columns */
+export const TableColumnsNamespace = i18n.t('team-assignment');
+
+// Table view strings
+
+export const TEAM_ASSIGNMENT_PAGE_TITLE = i18n.t('Team Assignment');
+export const TEAM_ASSIGNMENT_MODAL_TITLE = i18n.t('Assign/Unassign Teams');
+export const NAME = i18n.t('Name');
+export const ACTIONS = i18n.t('Actions');
+export const ASSIGN_TEAMS = i18n.t('Assigned Teams');
+export const SAVE = i18n.t('Save');
+export const CANCEL = i18n.t('Cancel');
