c1a0c87635a2469acda73254814bb9c2ae24dd9b
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-03-09 14:46:38 +0300

    Env updates
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- docker/app.sh
+++ docker/app.sh
@@ -5,5 +5,5 @@ set -e
 
 confd -onetime -backend env
 sed -i "s/CONFIG_VERSION/`date '+%s'`/" /usr/src/web/index.html
-sed -i "s/Redirected path should match configured path/Redirected path ' +  Url.parse(options.redirectUri).pathname + 'should match configured path/" /usr/src/app/node_modules/client-oauth2/src/client-oauth2.js
+sed -i "s/Redirected path should match configured path/Redirected path ' +  Url.parse(options.redirectUri).pathname + 'should match configured path/" /usr/src/web/app/node_modules/client-oauth2/src/client-oauth2.js
 
