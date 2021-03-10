9b60ed0548d4c9b62d98aefb00111ed7354bd6c2
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-03-10 11:04:05 +0300

    Update default evns in docker config template
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- docker/app.sh
+++ docker/app.sh
@@ -5,5 +5,4 @@ set -e
 
 confd -onetime -backend env
 sed -i "s/CONFIG_VERSION/`date '+%s'`/" /usr/src/web/index.html
-sed -i "s/Redirected path should match configured path/Redirected path ' +  Url.parse(options.redirectUri).pathname + 'should match configured path/" /usr/src/web/app/node_modules/client-oauth2/src/client-oauth2.js
-
+sed -i "s/Redirected path should match configured path/Redirected path ' +  Url.parse(options.redirectUri).pathname + 'should match configured path/" /usr/src/web/node_modules/client-oauth2/src/client-oauth2.js
