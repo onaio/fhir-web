04195ba39856d04929975c8c32931c98ac6336c1
p-netm <pmuriuki98@gmail.com>
2021-03-12 00:40:09 +0300

    Add type to i18n from config

--- packages/form-config/src/mls.tsx
+++ packages/form-config/src/mls.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { getConfig } from '@opensrp/pkg-config';
 import { loadLanguageResources } from '@opensrp/react-utils';
+import type { i18nInstance } from 'i18next';
 
-const i18n = getConfig('i18n');
+const i18n = getConfig('i18n') as i18nInstance;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const coreFrJson = require('../locales/core/fr.json');
