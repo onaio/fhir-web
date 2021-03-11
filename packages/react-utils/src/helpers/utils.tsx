56f2db3f58f724dd09da530bf44aed994aae3ab5
p-netm <pmuriuki98@gmail.com>
2021-03-12 00:39:25 +0300

    Change loadLanguageResource util function

--- packages/react-utils/src/helpers/utils.tsx
+++ packages/react-utils/src/helpers/utils.tsx
@@ -1,13 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dictionary } from '@onaio/utils/dist/types/types';
+import type { i18nInstance } from 'i18next';
+
 /**
  * Abstraction to add language resources to the i18n instance
  *
  * @param i18n the i18n instance
  * @param resources - an object that contains the resources
  */
-export const loadLanguageResources = (i18n: any, resources: any) => {
+export const loadLanguageResources = (i18n: i18nInstance | undefined, resources: Dictionary) => {
   Object.entries(resources).forEach(([language, nsObject]) => {
-    Object.entries(nsObject as any).forEach(([ns, resource]) => {
+    Object.entries(nsObject).forEach(([ns, resource]) => {
       i18n?.addResourceBundle(language, ns, resource);
     });
   });
--- packages/react-utils/src/lang.ts
+++ packages/react-utils/src/lang.ts
@@ -1,4 +1,4 @@
-import i18n from './mls';
+import i18nInstance from './mls';
 import { Dictionary } from '@onaio/utils';
 
 export type Lang = Dictionary<string>;
@@ -7,23 +7,25 @@ const lang: Lang = {};
 
 /** recompute values */
 function fill() {
-  lang.SEARCH = i18n.t('Search');
-  lang.FORBIDDEN_PAGE_STATUS = i18n.t('403');
-  lang.SOMETHING_WENT_WRONG = i18n.t('Something went wrong');
-  lang.YOU_ARE_UNAUTHORIZED = i18n.t('Sorry, you are not authorized to access this page');
-  lang.ERROR = i18n.t('Error');
-  lang.TITLE_404 = i18n.t('404');
-  lang.RESOURCE_DOES_NOT_EXIST = i18n.t('Sorry, the resource you requested for, does not exist');
-  lang.GO_HOME = i18n.t('Go home');
-  lang.GO_BACK = i18n.t('Go back');
-  lang.SESSION_EXPIRED_TEXT = i18n.t('Session Expired');
+  lang.SEARCH = i18nInstance.t('Search');
+  lang.FORBIDDEN_PAGE_STATUS = i18nInstance.t('403');
+  lang.SOMETHING_WENT_WRONG = i18nInstance.t('Something went wrong');
+  lang.YOU_ARE_UNAUTHORIZED = i18nInstance.t('Sorry, you are not authorized to access this page');
+  lang.ERROR = i18nInstance.t('Error');
+  lang.TITLE_404 = i18nInstance.t('404');
+  lang.RESOURCE_DOES_NOT_EXIST = i18nInstance.t(
+    'Sorry, the resource you requested for, does not exist'
+  );
+  lang.GO_HOME = i18nInstance.t('Go home');
+  lang.GO_BACK = i18nInstance.t('Go back');
+  lang.SESSION_EXPIRED_TEXT = i18nInstance.t('Session Expired');
 }
 
 // run it initial
 fill();
 
 // bind some events and fill values again (doing the magic you expect to happen magically)
-i18n.on('languageChanged', () => {
+i18nInstance.on('languageChanged', () => {
   fill();
 });
 
