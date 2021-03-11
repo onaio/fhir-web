1cca15fbade2a7ef7fe1a3e26e0f6ab636efbfff
p-netm <pmuriuki98@gmail.com>
2021-03-11 17:13:06 +0300

    Initialize i18n instance in app

--- app/src/mls.tsx
+++ app/src/mls.tsx
@@ -1,22 +1,44 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import i18n from 'i18next';
-import { initializei18n, LanguageResources } from '@opensrp/pkg-config';
+import { initReactI18next } from 'react-i18next';
+import { LANGUAGE_CODE, PROJECT_LANGUAGE_CODE } from './configs/env';
+
+const fallbackLng = `en_core`;
+const configuredLng = `${LANGUAGE_CODE}_${PROJECT_LANGUAGE_CODE}`;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const coreFrJson = require('./locales/core/fr.json');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const coreArJson = require('./locales/core/ar.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const eusmFrJson = require('./locales/eusm/fr.json');
 
 // the format to load the resource files: <languageCode>_<projectCode>. in small
-const resources: LanguageResources = {
-  fr_core: {
-    translation: coreFrJson,
-  },
+const resources = {
   ar_core: {
     translation: coreArJson,
   },
+  fr_core: {
+    translation: coreFrJson,
+  },
+  fr_eusm: {
+    translation: eusmFrJson,
+  },
 };
 
-initializei18n(i18n, resources);
+i18n
+  .use(initReactI18next)
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources,
+    lng: configuredLng,
+    fallbackLng: fallbackLng,
+    interpolation: { escapeValue: false },
+    returnEmptyString: false,
+    nsSeparator: '::',
+    debug: true,
+    keySeparator: false,
+  })
+  .catch((err) => err);
 
 export default i18n;
