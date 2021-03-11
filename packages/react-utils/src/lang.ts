570cc94c40fa1aaba4cd3068d18ffdc951e3a57c
p-netm <pmuriuki98@gmail.com>
2021-03-11 15:09:30 +0300

    Refactor lang constants to be accessed by reference

--- packages/react-utils/src/lang.ts
+++ packages/react-utils/src/lang.ts
@@ -1,14 +1,31 @@
 import i18n from './mls';
+import { Dictionary } from '@onaio/utils';
 
-export const SEARCH = i18n.t('Search');
-export const FORBIDDEN_PAGE_STATUS = i18n.t('403');
-export const SOMETHING_WENT_WRONG = i18n.t('Something went wrong');
-export const YOU_ARE_UNAUTHORIZED = i18n.t('Sorry, you are not authorized to access this page');
-export const ERROR = i18n.t('Error');
-export const TITLE_404 = i18n.t('404');
-export const RESOURCE_DOES_NOT_EXIST = i18n.t(
-  'Sorry, the resource you requested for, does not exist'
-);
-export const GO_HOME = i18n.t('Go home');
-export const GO_BACK = i18n.t('Go back');
-export const SESSION_EXPIRED_TEXT = i18n.t('Session Expired');
+export type Lang = Dictionary<string>;
+
+const lang: Lang = {};
+
+/** recompute values */
+function fill() {
+  lang.SEARCH = i18n.t('Search');
+  lang.FORBIDDEN_PAGE_STATUS = i18n.t('403');
+  lang.SOMETHING_WENT_WRONG = i18n.t('Something went wrong');
+  lang.YOU_ARE_UNAUTHORIZED = i18n.t('Sorry, you are not authorized to access this page');
+  lang.ERROR = i18n.t('Error');
+  lang.TITLE_404 = i18n.t('404');
+  lang.RESOURCE_DOES_NOT_EXIST = i18n.t('Sorry, the resource you requested for, does not exist');
+  lang.GO_HOME = i18n.t('Go home');
+  lang.GO_BACK = i18n.t('Go back');
+  lang.SESSION_EXPIRED_TEXT = i18n.t('Session Expired');
+}
+
+// run it initial
+fill();
+
+// bind some events and fill values again (doing the magic you expect to happen magically)
+i18n.on('languageChanged', () => {
+  fill();
+});
+
+// export the const
+export default lang;
