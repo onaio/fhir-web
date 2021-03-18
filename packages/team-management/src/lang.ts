cbed6c9548ad8827ada1b6dca5dc63b7b7639753
p-netm <pmuriuki98@gmail.com>
2021-03-19 00:20:53 +0300

    Refactor mls workflow in team management

--- packages/team-management/src/lang.ts
+++ packages/team-management/src/lang.ts
@@ -1,33 +1,54 @@
-import i18n from './mls';
+import i18n, { namespace } from './mls';
+import { Dictionary } from '@onaio/utils';
 
-// Tooltips
-export const TIP_REQUIRED_FIELD = 'This is a required field';
+export type Lang = Dictionary<string>;
 
-// Errors
-export const ERROR_OCCURRED = i18n.t('An error occurred');
+const lang: Lang = {};
 
-// Messages
-export const MSG_ASSIGN_PRACTITIONERS = i18n.t('Assigning Practitioners');
-export const MSG_ASSIGN_PRACTITONERS_SUCCESS = i18n.t('Successfully Assigned Practitioners');
-export const MSG_TEAMS_UPDATE_SUCCESS = i18n.t('Successfully Updated Teams');
-export const MSG_TEAMS_ADD_SUCCESS = i18n.t('Successfully Added Teams');
+/** recompute values */
+function fill() {
+  // Tooltips
+  lang.TIP_REQUIRED_FIELD = i18n.t(`${namespace}::This is a required field`);
 
-// Rendered text
-export const ENTER_TEAM_NAME = i18n.t('Enter a team name');
-export const TEAM_NAME = i18n.t('Team Name');
-export const STATUS = i18n.t('Status');
-export const TEAM_MEMBERS = i18n.t('Team Members');
-export const SAVING = i18n.t('Saving');
-export const SAVE = i18n.t('Save');
-export const ACTIVE = i18n.t('Active');
-export const INACTIVE = i18n.t('Inactive');
-export const SELECT_PRACTITIONER = i18n.t('Select user (practitioners only)');
-export const CANCEL = i18n.t('Cancel');
-export const EDIT_TEAM = i18n.t('Edit Team');
-export const CREATE_TEAM = i18n.t('Create Team');
-export const EDIT = i18n.t('Edit');
-export const CREATE = i18n.t('Create');
-export const IDENTIFIER = i18n.t('Identifier');
-export const NO_TEAM_MEMBERS = i18n.t('No team members');
-export const TEAMS = i18n.t('Teams');
-export const SEARCH = i18n.t('Search');
+  // Errors
+  lang.ERROR_OCCURRED = i18n.t(`${namespace}::An error occurred`);
+
+  // Messages
+  lang.MSG_ASSIGN_PRACTITIONERS = i18n.t(`${namespace}::Assigning Practitioners`);
+  lang.MSG_ASSIGN_PRACTITONERS_SUCCESS = i18n.t(
+    `${namespace}::Successfully Assigned Practitioners`
+  );
+  lang.MSG_TEAMS_UPDATE_SUCCESS = i18n.t(`${namespace}::Successfully Updated Teams`);
+  lang.MSG_TEAMS_ADD_SUCCESS = i18n.t(`${namespace}::Successfully Added Teams`);
+
+  // Rendered text
+  lang.ENTER_TEAM_NAME = i18n.t(`${namespace}::Enter a team name`);
+  lang.TEAM_NAME = i18n.t(`${namespace}::Team Name`);
+  lang.STATUS = i18n.t(`${namespace}::Status`);
+  lang.TEAM_MEMBERS = i18n.t(`${namespace}::Team Members`);
+  lang.SAVING = i18n.t(`${namespace}::Saving`);
+  lang.SAVE = i18n.t(`${namespace}::Save`);
+  lang.ACTIVE = i18n.t(`${namespace}::Active`);
+  lang.INACTIVE = i18n.t(`${namespace}::Inactive`);
+  lang.SELECT_PRACTITIONER = i18n.t(`${namespace}::Select user (practitioners only)`);
+  lang.CANCEL = i18n.t(`${namespace}::Cancel`);
+  lang.EDIT_TEAM = i18n.t(`${namespace}::Edit Team`);
+  lang.CREATE_TEAM = i18n.t(`${namespace}::Create Team`);
+  lang.EDIT = i18n.t(`${namespace}::Edit`);
+  lang.CREATE = i18n.t(`${namespace}::Create`);
+  lang.IDENTIFIER = i18n.t(`${namespace}::Identifier`);
+  lang.NO_TEAM_MEMBERS = i18n.t(`${namespace}::No team members`);
+  lang.TEAMS = i18n.t(`${namespace}::Teams`);
+  lang.SEARCH = i18n.t(`${namespace}::Search`);
+}
+
+// run it initial
+fill();
+
+// bind some events and fill values again (doing the magic you expect to happen magically)
+i18n.on(`languageChanged`, () => {
+  fill();
+});
+
+// export the const
+export default lang;
