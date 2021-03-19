0dd1d1aa02f32b47170ed9f70eaaa35f522a8560
fmutua <fmutua@ona.io>
2021-03-19 10:25:31 +0300

    Oauth in cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,6 +25,7 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 const referer = Cypress.config('baseUrl')
 const currentUser = Cypress.env('currentUser')
+const username = Cypress.env('username')
 
 Cypress.Commands.add('loginRequest', () => {
     let body = {
@@ -42,6 +43,55 @@ Cypress.Commands.add('loginRequest', () => {
     })
 })
 
+Cypress.Commands.add("loginByAuth0Api", (username: username, password: "") => {
+    cy.log(`Logging in as ${username}`);
+      const client_id = Cypress.env("auth0_client_id");
+      const client_secret = Cypress.env("auth0_client_secret");
+      const audience = Cypress.env("auth0_audience");
+      const scope = Cypress.env("auth0_scope");
+    
+      cy.request({
+        method: "POST",
+        url: `https://${Cypress.env("auth0_domain")}/oauth/token`,
+        body: {
+          grant_type: "password",
+          username,
+          password,
+          audience,
+          scope,
+          client_id,
+          client_secret,
+        },
+      }).then(({ body }) => {
+        const claims = jwt.decode(body.id_token);
+        const { nickname, name, picture, updated_at, email, email_verified, sub, exp } = claims;
+    
+        const item = {
+          body: {
+            ...body,
+            decodedToken: {
+              claims,
+              user: {
+                nickname,
+                name,
+                picture,
+                updated_at,
+                email,
+                email_verified,
+                sub,
+              },
+              audience,
+              client_id,
+            },
+          },
+          expiresAt: exp,
+        };
+    
+        window.localStorage.setItem('auth0Cypress', JSON.stringify(item));
+    
+        cy.visit("/");
+      });
+    });
 
 
     
