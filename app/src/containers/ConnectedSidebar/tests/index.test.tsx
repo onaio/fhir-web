ba8e9822da75593778f2ce5c305499706ac61dd5
Ashfahan <me@ashfahan.com>
2021-04-07 11:45:48 +0500

    reduce the snapshot by decreasing the amount of tests

--- app/src/containers/ConnectedSidebar/tests/index.test.tsx
+++ app/src/containers/ConnectedSidebar/tests/index.test.tsx
@@ -51,7 +51,7 @@ describe('components/ConnectedSidebar', () => {
     wrapper.unmount();
   });
 
-  it('displays menu links for enabled modules', () => {
+  it('displays menu links for All Enabled modules', () => {
     const envModule = require('../../../configs/env');
     envModule.ENABLE_LOCATIONS = true;
     envModule.ENABLE_TEAMS = true;
@@ -94,38 +94,6 @@ describe('components/ConnectedSidebar', () => {
     wrapper.unmount();
   });
 
-  it('displays menu links for enabled Location module', () => {
-    const envModule = require('../../../configs/env');
-    envModule.ENABLE_LOCATIONS = true;
-    envModule.OPENSRP_ROLES = {
-      LOCATIONS: 'ROLE_VIEW_KEYCLOAK_USERS',
-    };
-    store.dispatch(
-      authenticateUser(
-        true,
-        {
-          email: 'bob@example.com',
-          name: 'Bobbie',
-          username: 'RobertBaratheon',
-        },
-        {
-          roles: ['ROLE_VIEW_KEYCLOAK_USERS'],
-          username: 'superset-user',
-          user_id: 'cab07278-c77b-4bc7-b154-bcbf01b7d35b',
-        }
-      )
-    );
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <ConnectedSidebar />
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(wrapper.find('Menu').at(0).prop('children')).toMatchSnapshot();
-    wrapper.unmount();
-  });
-
   it('correctly expand users menu', () => {
     const envModule = require('../../../configs/env');
     envModule.OPENSRP_ROLES = {
@@ -157,70 +125,6 @@ describe('components/ConnectedSidebar', () => {
     wrapper.unmount();
   });
 
-  it('correctly expand location menu', () => {
-    const envModule = require('../../../configs/env');
-    envModule.ENABLE_LOCATIONS = true;
-    envModule.OPENSRP_ROLES = {
-      LOCATIONS: 'ROLE_VIEW_KEYCLOAK_USERS',
-    };
-    store.dispatch(
-      authenticateUser(
-        true,
-        {
-          email: 'bob@example.com',
-          name: 'Bobbie',
-          username: 'RobertBaratheon',
-        },
-        {
-          roles: ['ROLE_VIEW_KEYCLOAK_USERS'],
-          username: 'superset-user',
-          user_id: 'cab07278-c77b-4bc7-b154-bcbf01b7d35b',
-        }
-      )
-    );
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={[{ pathname: `/location`, hash: '', search: '', state: {} }]}>
-          <ConnectedSidebar />
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(wrapper.find('Menu').first().prop('children')).toMatchSnapshot();
-    wrapper.unmount();
-  });
-
-  it('correctly expand admin-form-config menu', () => {
-    const envModule = require('../../../configs/env');
-    envModule.ENABLE_FORM_CONFIGURATION = true;
-    envModule.OPENSRP_ROLES = {
-      FORM_CONFIGURATION: 'ROLE_VIEW_KEYCLOAK_USERS',
-    };
-    store.dispatch(
-      authenticateUser(
-        true,
-        {
-          email: 'bob@example.com',
-          name: 'Bobbie',
-          username: 'RobertBaratheon',
-        },
-        {
-          roles: ['ROLE_VIEW_KEYCLOAK_USERS'],
-          username: 'superset-user',
-          user_id: 'cab07278-c77b-4bc7-b154-bcbf01b7d35b',
-        }
-      )
-    );
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={[{ pathname: `/draft`, hash: '', search: '', state: {} }]}>
-          <ConnectedSidebar />
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(wrapper.find('Menu').at(0).prop('children')).toMatchSnapshot();
-    wrapper.unmount();
-  });
-
   it('shows the correct logo', () => {
     const wrapper = mount(
       <Provider store={store}>
@@ -230,41 +134,6 @@ describe('components/ConnectedSidebar', () => {
       </Provider>
     );
     expect(toJson(wrapper.find('.logo'))).toMatchSnapshot('Logo');
-  });
-
-  it('correctly expand teams menu', () => {
-    const envModule = require('../../../configs/env');
-    envModule.ENABLE_TEAMS = true;
-    envModule.ENABLE_TEAMS_ASSIGNMENT_MODULE = true;
-    envModule.OPENSRP_ROLES = {
-      TEAMS: 'ROLE_VIEW_KEYCLOAK_USERS',
-    };
-    store.dispatch(
-      authenticateUser(
-        true,
-        {
-          email: 'bob@example.com',
-          name: 'Bobbie',
-          username: 'RobertBaratheon',
-        },
-        {
-          roles: ['ROLE_VIEW_KEYCLOAK_USERS'],
-          username: 'superset-user',
-          user_id: 'cab07278-c77b-4bc7-b154-bcbf01b7d35b',
-        }
-      )
-    );
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[{ pathname: `/admin/teams`, hash: '', search: '', state: {} }]}
-        >
-          <ConnectedSidebar />
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(wrapper.find('Menu').at(0).prop('children')).toMatchSnapshot();
-    wrapper.unmount();
   });
 
   it('correctly sets open keys', () => {
