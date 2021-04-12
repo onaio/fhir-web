aa1d54f563cd73ae7e3c9b59e5a80c52c1e2563f
Ashfahan <me@ashfahan.com>
2021-04-12 11:01:48 +0500

    Revert implementation to default table props

--- packages/react-utils/src/components/TableLayout/index.tsx
+++ packages/react-utils/src/components/TableLayout/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Table as AntTable } from 'antd';
-import { ColumnType, TableProps } from 'antd/lib/table';
-
-export interface Column<T> extends ColumnType<T>, Record<string, any> {
-  dataIndex?: React.Key | React.Key[];
-  key?: React.Key;
-}
+import { TableProps } from 'antd/lib/table';
 
 const defaultoptions: TableProps<any> = {
   pagination: {
@@ -16,17 +11,12 @@ const defaultoptions: TableProps<any> = {
   },
 };
 
-export interface Props<T> extends Omit<TableProps<T>, 'columns' | 'dataSource'> {
-  datasource: T[];
-  columns?: Column<T>[];
-}
-
-export function TableLayout<T extends object = Record<string, any>>(props: Props<T>) {
-  const { columns, datasource, children } = props;
+export function TableLayout<T extends object = Record<string, any>>(props: TableProps<T>) {
+  const { columns, dataSource, children } = props;
   const options = { ...defaultoptions, ...props };
 
   return (
-    <AntTable<T> dataSource={datasource} columns={columns} {...options}>
+    <AntTable<T> dataSource={dataSource} columns={columns} {...options}>
       {children}
     </AntTable>
   );
