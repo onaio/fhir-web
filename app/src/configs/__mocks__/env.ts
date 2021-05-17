d0fd74a59a423426ebf6d47fa325223b384f902a
p-netm <pmuriuki98@gmail.com>
2021-05-17 22:13:18 +0300

    Update tests

--- app/src/configs/__mocks__/env.ts
+++ app/src/configs/__mocks__/env.ts
@@ -21,3 +21,28 @@ export const OPENSRP_ROLES = {
 export const MAIN_LOGO_SRC =
   'https://github.com/OpenSRP/web/raw/master/app/src/assets/images/opensrp-logo-color.png';
 export const PLAN_ASSIGNMENT_AT_GEO_LEVEL = 1;
+
+export const OPENSRP_LOGOUT_URL = 'https://opensrp-stage.smartregister.org/opensrp/logout.do';
+
+export const OPENSRP_OAUTH_STATE = 'REACT_APP_OPENSRP_OAUTH_STATE';
+
+// notice the ending is NOT / here
+export const OPENSRP_ACCESS_TOKEN_URL =
+  'https://opensrp-stage.smartregister.org/opensrp/oauth/token';
+
+// notice the ending is NOT / here
+export const OPENSRP_AUTHORIZATION_URL =
+  'https://opensrp-stage.smartregister.org/opensrp/oauth/authorize';
+
+export const NAVBAR_BRAND_IMG_SRC =
+  'https://github.com/OpenSRP/opensrp-web/raw/master/clients/core/src/assets/images/logo.png';
+
+export const KEYCLOAK_LOGOUT_URL =
+  'https://keycloak-stage.smartregister.org/auth/realms/opensrp-web-stage/protocol/openid-connect/logout';
+
+/** Express server settings */
+export const EXPRESS_OAUTH_GET_STATE_URL = 'http://localhost:3000/oauth/state';
+
+export const EXPRESS_OAUTH_LOGOUT_URL = 'http://localhost:3000/logout';
+
+export const DOMAIN_NAME = 'http://localhost:3000';
