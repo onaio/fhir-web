f7a9927d4f772d6f6ee873a82b43023e9de115ae
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-05-19 08:14:16 +0300

    Remove description sorting
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/keycloak-user-management/src/components/UserRolesList/index.tsx
+++ packages/keycloak-user-management/src/components/UserRolesList/index.tsx
@@ -98,12 +98,6 @@ export const UserRolesList: React.FC<Props & RouteComponentProps> = (
       title: lang.DESCRIPTION,
       dataIndex: 'description',
       editable: true,
-      sorter: (a: TableData, b: TableData) => {
-        if (a.description && b.description) {
-          return a.description.localeCompare(b.description);
-        }
-        return 0;
-      },
     },
   ];
 
