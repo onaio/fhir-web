8bb733c17f64f28915890b84d10b56712ef7bdb0
Ashfahan <me@ashfahan.com>
2021-05-22 05:14:37 +0500

    Extract Translations

--- packages/error-boundary-fallback/src/lang.ts
+++ packages/error-boundary-fallback/src/lang.ts
@@ -7,7 +7,7 @@ const lang: Dictionary<string> = {};
 function fill() {
   lang.PAGE_TITLE = i18n.t(`An Error Occurred`, { ns: namespace });
   lang.PAGE_SUB_TITLE = i18n.t(
-    `There has been an error. It’s been reported to the site administrators via email and should be fixed shortly. Thanks for your patience.'`,
+    `There has been an error. It’s been reported to the site administrators via email and should be fixed shortly. Thanks for your patience.`,
     { ns: namespace }
   );
   lang.BUTTON_TITLE = i18n.t(`Back Home`, { ns: namespace });
