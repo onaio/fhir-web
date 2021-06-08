230dcb07edcbcd27e9a8bc2c53186eeb8de890df
Ashfahan <me@ashfahan.com>
2021-06-08 23:42:06 +0500

    use reducer instead of foreach to improve code

--- app/src/fhir-team-management/src/utils.ts
+++ app/src/fhir-team-management/src/utils.ts
@@ -4,14 +4,15 @@ export function ProcessFHIRResponse<T>(res: FHIRResponse<T>) {
   return ProcessFHIRObjects(res.entry.map((e) => e.resource));
 }
 
-export function ProcessFHIRObjects<T>(
-  object: FhirObject<T>[]
-): (Omit<T, 'identifier'> & { identifier: IdentifierObject })[] {
+export function ProcessFHIRObjects<T>(object: FhirObject<T>[]) {
   return object.map((e) => ProcessFHIRObject(e));
 }
 
-export function ProcessFHIRObject<T>(object: FhirObject<T>) {
-  let identifier: IdentifierObject = {} as IdentifierObject; // only keep those value that have offical identifier
-  (object.identifier as Identifier[]).forEach((e) => (identifier[e.use] = e));
-  return { ...object, identifier: identifier };
+export function ProcessFHIRObject<T>(object: FhirObject<T>): T {
+  const identifier: IdentifierObject = object.identifier.reduce((prev, id) => {
+    const typesid = id as Identifier;
+    return { ...prev, [typesid.use]: typesid.value };
+  }, {}) as IdentifierObject;
+
+  return ({ ...object, identifier: identifier } as unknown) as T;
 }
