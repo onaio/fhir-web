5092a151a8269262f4180556311eaeb34a554796
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-06-21 10:09:48 +0300

    Abstract query hooks and add broken page handler
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- packages/fhir-care-team/src/components/ListView/index.tsx
+++ packages/fhir-care-team/src/components/ListView/index.tsx
@@ -6,9 +6,9 @@ import { MoreOutlined, PlusOutlined } from '@ant-design/icons';
 import { RouteComponentProps, useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import FHIR from 'fhirclient';
-import { sendErrorNotification } from '@opensrp/notifications';
+import { IfhirR4 } from '@smile-cdr/fhirts';
 import { useQuery } from 'react-query';
-import { createChangeHandler, getQueryParams, SearchForm } from '@opensrp/react-utils';
+import { createChangeHandler, getQueryParams, SearchForm, BrokenPage } from '@opensrp/react-utils';
 import lang from '../../lang';
 import {
   SEARCH_QUERY_PARAM,
@@ -19,6 +19,7 @@ import {
   URL_CREATE_CARE_TEAM,
 } from '../../constants';
 import { ViewDetails } from '../ViewDetails';
+import { Dictionary } from '@onaio/utils';
 
 // route params for user group pages
 interface RouteParams {
@@ -47,31 +48,37 @@ export type CareTeamListPropTypes = Props & RouteComponentProps<RouteParams>;
  * @param {Object} props - UserRolesList component props
  * @returns {Function} returns User Roles list display
  */
+
+export const fetcCareTeams = async (fhirBaseURL: string): Promise<IfhirR4.IBundle> => {
+  return await FHIR.client(fhirBaseURL).request(FHIR_CARE_TEAM);
+};
+
+export const useCareTeamsHook = (fhirBaseURL: string) => {
+  return useQuery(FHIR_CARE_TEAM, () => fetcCareTeams(fhirBaseURL), {
+    select: (res: IfhirR4.IBundle) => res,
+    refetchOnWindowFocus: false,
+    retryOnMount: true,
+  });
+};
 export const CareTeamList: React.FC<CareTeamListPropTypes> = (props: CareTeamListPropTypes) => {
   const { fhirBaseURL } = props;
   const history = useHistory();
   const careTeamId = props.match.params[ROUTE_PARAM_CARE_TEAM_ID] ?? '';
 
-  const { data, isLoading, isFetching } = useQuery(
-    FHIR_CARE_TEAM,
-    () => FHIR.client(fhirBaseURL).request(FHIR_CARE_TEAM),
-    {
-      onError: () => sendErrorNotification(lang.ERROR_OCCURED),
-      select: (res: any) => res,
-      refetchOnWindowFocus: false,
-      retryOnMount: true,
-      suspense: !!careTeamId,
-    }
-  );
+  const { data, isLoading, isFetching, error } = useCareTeamsHook(fhirBaseURL);
 
-  if (isLoading || !data || isFetching) return <Spin size="large" />;
+  if (isLoading || isFetching) return <Spin size="large" />;
+
+  if (error) {
+    return <BrokenPage errorMessage={`${error}`} />;
+  }
 
   const searchFormProps = {
     defaultValue: getQueryParams(props.location)[SEARCH_QUERY_PARAM],
     onChangeHandler: createChangeHandler(SEARCH_QUERY_PARAM, props),
   };
 
-  const tableData: TableData[] = data.entry?.map((datum: any, index: number) => {
+  const tableData = data?.entry?.map((datum: Dictionary, index: number) => {
     return {
       key: `${index}`,
       id: datum.resource.id,
