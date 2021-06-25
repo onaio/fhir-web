ecdb6387d3c7049e517570901bb231e9d8e5aaf1
Macharia Muguku <machariamuguku@gmail.com>
2021-06-25 11:53:39 +0300

    add test for component internal state change on re-render for component s that render differently based on url

--- packages/react-utils/src/helpers/tests/componentUtils.test.tsx
+++ packages/react-utils/src/helpers/tests/componentUtils.test.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/camelcase */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { act } from 'react-dom/test-utils';
 import { history } from '@onaio/connected-reducer-registry';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Router } from 'react-router';
 import { store } from '@opensrp/store';
 import * as componentUtils from '../componentUtils';
 import { UserList } from '@opensrp/user-management';
@@ -14,6 +14,7 @@ import { Resource404 } from '../../components/Resource404';
 import { KEYCLOAK_API_BASE_URL } from '@opensrp/keycloak-service';
 import { authenticateUser } from '@onaio/session-reducer';
 import flushPromises from 'flush-promises';
+import fetch from 'jest-fetch-mock';
 
 const { PublicComponent, PrivateComponent, isAuthorized } = componentUtils;
 
@@ -164,5 +165,120 @@ describe('componentUtils', () => {
       ['unauthorized']
     );
     expect(isUserAuthorized).toBeFalsy();
+  });
+
+  it('Updates state on route/pathname changes for same component', async () => {
+    store.dispatch(
+      authenticateUser(
+        true,
+        {
+          email: 'test@gmail.com',
+          name: 'This Name',
+          username: 'tHat Part',
+        },
+        {
+          roles: ['ROLE_VIEW_KEYCLOAK_USERS'],
+          username: 'superset-user',
+          user_id: 'cab07278-c77b-4bc7-b154-bcbf01b7d35b',
+        }
+      )
+    );
+
+    // mock component with internal state change
+    const MockComponent = (props: {
+      match: {
+        params: {
+          id: string;
+        };
+      };
+    }) => {
+      // get user id from url
+      const {
+        match: {
+          params: { id },
+        },
+      } = props;
+      const [someState, setSomeState] = useState<{
+        firstName: string;
+        lastName: string;
+      }>({
+        firstName: '',
+        lastName: '',
+      });
+
+      // update state on fetch - internal data change on mount
+      useEffect(() => {
+        fetch(`http://example.com/users/${id}`)
+          .then((response) => response.json())
+          .then((data) => setSomeState(data))
+          .catch((err) => {
+            throw err;
+          });
+      }, [id]);
+
+      return <p className="mockClassName">{`${someState.firstName} ${someState.lastName}`}</p>;
+    };
+
+    // mock re-fetch on re-mount
+    fetch
+      .mockOnce(
+        JSON.stringify({
+          firstName: 'Anon',
+          lastName: 'Ops',
+        })
+      )
+      .mockOnce(
+        JSON.stringify({
+          firstName: 'Anon',
+          lastName: 'Central',
+        })
+      );
+
+    const props = {
+      exact: true,
+      redirectPath: '/login',
+      disableLoginProtection: false,
+      path: '/admin/users/:id',
+      authenticated: true,
+    };
+
+    // start with user with id 1
+    history.push('/admin/users/1');
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <PrivateComponent
+            {...props}
+            component={MockComponent}
+            activeRoles={['ROLE_VIEW_KEYCLOAK_USERS']}
+          />
+        </Router>
+      </Provider>
+    );
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    expect(wrapper.find('.mockClassName').text()).toMatchInlineSnapshot(`"Anon Ops"`);
+
+    // simulate navigation - similar url but different pathname (id)
+    history.push('/admin/users/2');
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    // expect change to result of second fetch
+    expect(wrapper.find('.mockClassName').text()).toMatchInlineSnapshot(`"Anon Central"`);
+
+    // expect both fetch calls
+    expect(fetch.mock.calls).toMatchObject([
+      ['http://example.com/users/1'],
+      ['http://example.com/users/2'],
+    ]);
   });
 });
