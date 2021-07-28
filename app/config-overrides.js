f5de0f32e9f54893034940fe710ff96d0c3370ec
Macharia Muguku <machariamuguku@gmail.com>
2021-07-29 01:15:34 +0300

    setup webpack to compile files outside the default src folder

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -8,5 +8,11 @@ module.exports = (config) => {
   // Resolve the path aliases.
   config.resolve.plugins.push(new TsconfigPathsPlugin());
 
+  // Let Babel compile outside of src/.
+  const oneOfRule = config.module.rules.find((rule) => rule.oneOf);
+  const tsRule = oneOfRule.oneOf.find((rule) => rule.test.toString().includes('ts|tsx'));
+  tsRule.include = undefined;
+  tsRule.exclude = /node_modules/;
+
   return config;
 };
