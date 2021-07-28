895e894387576c5c0047ee48505ab98a3b9ca5ea
Macharia Muguku <machariamuguku@gmail.com>
2021-07-29 01:14:19 +0300

    remove ModuleScopePlugin which prevents import of files from outside src folder

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,6 +1,10 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = (config) => {
+  // Remove the ModuleScopePlugin which throws when we try
+  // to import something outside of src/.
+  config.resolve.plugins.pop();
+
   // Resolve the path aliases.
   config.resolve.plugins.push(new TsconfigPathsPlugin());
 
