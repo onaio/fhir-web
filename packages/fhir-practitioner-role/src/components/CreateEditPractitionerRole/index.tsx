ce2df32cbba0d9661d82b6f3098e8f285bff35cd
Kipchirchir Sigei <arapgodsmack@gmail.com>
2021-08-04 15:52:04 +0300

    Add functionality to edit/create practitioner roles
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

---
+++ packages/fhir-practitioner-role/src/components/CreateEditPractitionerRole/index.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Col, Row } from 'antd';
+import { useQuery } from 'react-query';
+import FHIR from 'fhirclient';
+import { Spin } from 'antd';
+import { sendErrorNotification } from '@opensrp/notifications';
+import { RouteComponentProps } from 'react-router-dom';
+import { Dictionary } from '@onaio/utils';
+import lang from '../../lang';
+import {
+  FHIR_PRACTITIONER_ROLE,
+  FHIR_GROUPS,
+  FHIR_PRACTITIONERS,
+  ROUTE_PARAM_CARE_TEAM_ID,
+} from '../../constants';
+import { IfhirR4 } from '@smile-cdr/fhirts';
+import { PractitionerRoleForm, FormFields } from './Form';
+import { getPatientName } from './utils';
+
+// Interface for route params
+interface RouteParams {
+  careTeamId: string;
+}
+
+/** props for editing a user view */
+export interface EditCareTeamProps {
+  fhirBaseURL: string;
+}
+
+/** type intersection for all types that pertain to the props */
+export type CreateEditCareTeamProps = EditCareTeamProps & RouteComponentProps<RouteParams>;
+
+/** default props for editing user component */
+export const defaultEditCareTeamsProps: EditCareTeamProps = {
+  fhirBaseURL: '',
+};
+
+export const defaultInitialValues: FormFields = {
+  uuid: '',
+  id: '',
+  name: '',
+  status: '',
+  active: true,
+  practitionersId: [],
+  groupsId: '',
+};
+/**
+ *
+ * @param props - CreateEditUser component props
+ */
+
+const CreateEditCareTeam: React.FC<CreateEditCareTeamProps> = (props: CreateEditCareTeamProps) => {
+  const { fhirBaseURL } = props;
+  const careTeamId = props.match.params[ROUTE_PARAM_CARE_TEAM_ID];
+  const singleCareTeam = useQuery(
+    `${FHIR_PRACTITIONER_ROLE}/${careTeamId}`,
+    async () =>
+      careTeamId
+        ? FHIR.client(fhirBaseURL).request(`${FHIR_PRACTITIONER_ROLE}/${careTeamId}`)
+        : undefined,
+    {
+      onError: () => sendErrorNotification(lang.ERROR_OCCURED),
+      select: (res: IfhirR4.ICareTeam) => res,
+    }
+  );
+
+  const fhirGroups = useQuery(
+    FHIR_GROUPS,
+    async () => FHIR.client(fhirBaseURL).request(FHIR_GROUPS),
+    {
+      onError: () => sendErrorNotification(lang.ERROR_OCCURED),
+      select: (res: IfhirR4.IBundle) => res,
+    }
+  );
+
+  const fhirPractitioners = useQuery(
+    FHIR_PRACTITIONERS,
+    async () => FHIR.client(fhirBaseURL).request(FHIR_PRACTITIONERS),
+    {
+      onError: () => sendErrorNotification(lang.ERROR_OCCURED),
+      select: (res: IfhirR4.IBundle) => res,
+    }
+  );
+
+  if (singleCareTeam.isLoading || fhirGroups.isLoading || fhirPractitioners.isLoading) {
+    return <Spin size="large" />;
+  }
+
+  const buildInitialValues = singleCareTeam.data
+    ? {
+        uuid: (singleCareTeam.data?.identifier as Dictionary[])[0].value as string,
+        id: singleCareTeam.data.id,
+        name: singleCareTeam.data.name,
+        status: singleCareTeam.data.status ?? 'active',
+        practitionersId: singleCareTeam.data.participant?.map(
+          (p: Dictionary) => p.member.reference.split('/')[1]
+        ),
+        groupsId: singleCareTeam.data.subject?.reference?.split('/')[1] ?? '',
+      }
+    : defaultInitialValues;
+
+  const careTeamFormProps = {
+    fhirBaseURL,
+    initialValues: buildInitialValues,
+    practitioners:
+      fhirPractitioners.data?.entry?.map((e: Dictionary) => {
+        return {
+          id: e.resource.id,
+          name: getPatientName(e.resource),
+        };
+      }) ?? [],
+    groups:
+      fhirGroups.data?.entry?.map((e: Dictionary) => ({
+        id: e.resource?.id,
+        name: e.resource?.name as string,
+      })) ?? [],
+  };
+
+  return (
+    <Row>
+      <Col span={24}>
+        <PractitionerRoleForm {...careTeamFormProps} />
+      </Col>
+    </Row>
+  );
+};
+
+CreateEditCareTeam.defaultProps = defaultEditCareTeamsProps;
+
+export { CreateEditCareTeam };
---
+++ packages/fhir-practitioner-role/src/components/CreateEditPractitionerRole/utils.tsx
@@ -0,0 +1,86 @@
+import { history } from '@onaio/connected-reducer-registry';
+import { v4 } from 'uuid';
+import FHIR from 'fhirclient';
+import { sendErrorNotification, sendSuccessNotification } from '@opensrp/notifications';
+import lang from '../../lang';
+import { URL_CARE_TEAM } from '../../constants';
+import { Dictionary } from '@onaio/utils';
+import { IfhirR4 } from '@smile-cdr/fhirts';
+import { FormFields } from './Form';
+
+export const submitForm = async (
+  values: FormFields,
+  fhirBaseURL: string,
+  id?: string,
+  uuid?: string
+): Promise<void> => {
+  const practitionerRoleId = uuid ? uuid : v4();
+  const payload: Omit<IfhirR4.IPractitionerRole, 'meta'> = {
+    resourceType: 'PractitionerRole',
+    identifier: [
+      {
+        use: 'official',
+        value: practitionerRoleId,
+      },
+    ],
+    id: id,
+    active: values.active,
+    organization: values.groupsId
+      ? {
+          reference: `Organization/${values.groupsId}`,
+        }
+      : undefined,
+    practitioner: values.groupsId
+      ? {
+          reference: `Practitioner/${values.groupsId}`,
+        }
+      : undefined,
+  };
+  const serve = FHIR.client(fhirBaseURL);
+  if (id) {
+    await serve
+      .update(payload)
+      .then(() => sendSuccessNotification(lang.CARE_TEAMS_UPDATE_SUCCESS))
+      .catch(() => sendErrorNotification(lang.ERROR_OCCURRED));
+  } else {
+    await serve
+      .create(payload)
+      .then(() => sendSuccessNotification(lang.CARE_TEAMS_ADD_SUCCESS))
+      .catch(() => sendErrorNotification(lang.ERROR_OCCURRED));
+  }
+  history.push(URL_CARE_TEAM);
+};
+
+/** Util function to build out patient or practitioner name
+ *
+ * @param {object} patient - patient resource object
+ * @returns {string} - returns patient or practitioner name string
+ */
+export function getPatientName(patient: Dictionary | undefined) {
+  if (!patient) {
+    return '';
+  }
+
+  let name = patient.name;
+  if (!Array.isArray(name)) {
+    name = [name];
+  }
+  name = name[0];
+  if (!name) {
+    return '';
+  }
+
+  const family = Array.isArray(name.family) ? name.family : [name.family];
+  const given = Array.isArray(name.given) ? name.given : [name.given];
+  const prefix = Array.isArray(name.prefix) ? name.prefix : [name.prefix];
+  const suffix = Array.isArray(name.suffix) ? name.suffix : [name.suffix];
+
+  return [
+    prefix.map((t = '') => String(t).trim()).join(' '),
+    given.map((t = '') => String(t).trim()).join(' '),
+    family.map((t = '') => String(t).trim()).join(' '),
+    suffix.map((t = '') => String(t).trim()).join(' '),
+  ]
+    .filter(Boolean)
+    .join(' ');
+}
