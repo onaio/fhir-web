1ee16e88314a865c054d8bd360e07f3e0739b595
Ashfahan <me@ashfahan.com>
2021-10-14 13:35:18 +0500

    define HEALTH_CARE_SERVICE_RESOURCE_TYPE in constant

--- packages/fhir-health-care-service/src/types.ts
+++ packages/fhir-health-care-service/src/types.ts
@@ -1,12 +1,13 @@
 /** Organizations redux module */
 import { Require } from '@opensrp/react-utils';
 import { IfhirR4 } from '@smile-cdr/fhirts';
+import { HEALTH_CARE_SERVICE_RESOURCE_TYPE } from './constants';
 
 /** interface for Objects */
 
 export interface HealthcareService
   extends Require<IfhirR4.IHealthcareService, 'id' | 'providedBy' | 'active' | 'name'> {
-  resourceType: 'HealthcareService';
+  resourceType: typeof HEALTH_CARE_SERVICE_RESOURCE_TYPE;
 }
 
 export type HealthcareServiceDetail = HealthcareService & { organization?: Organization };
