1ebe4d09341ef2edd5945acbe48e0cd194cebc9d
Macharia Muguku <machariamuguku@gmail.com>
2021-10-17 02:04:40 +0300

    😆 nvm you can set a custom .env file in jest config, also unifying test coverages is a pain 🤦

--- app/src/configs/tests/utils.test.ts
+++ app/src/configs/tests/utils.test.ts
@@ -3,6 +3,11 @@ import { setEnv } from '../utils';
 jest.mock('../env');
 
 describe('setEnv util function', () => {
+  it('picks the test .env file', () => {
+    let getGeoLevel = Number(setEnv('REACT_APP_PLAN_ASSIGNMENT_AT_GEO_LEVEL', '0'));
+    // expect the value in the .env.test
+    expect(getGeoLevel).toEqual(1);
+  });
   it('sets an env var value to integer type', () => {
     const envModule = require('../env');
     envModule.PLAN_ASSIGNMENT_AT_GEO_LEVEL = 1;
