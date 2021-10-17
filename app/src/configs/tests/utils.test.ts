77848754ddfc110f4cf32a0a572a9143b1912d7e
Macharia Muguku <machariamuguku@gmail.com>
2021-10-17 03:20:58 +0300

    remove mocking env.ts module - use test env file instead, rearrange setEnv type test, add test for team member reassignment

--- app/src/configs/tests/utils.test.ts
+++ app/src/configs/tests/utils.test.ts
@@ -1,19 +1,39 @@
 import { setEnv } from '../utils';
-
-jest.mock('../env');
+import { DISABLE_TEAM_MEMBER_REASSIGNMENT } from '../env';
 
 describe('setEnv util function', () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules(); // clears cache
+    process.env = { ...OLD_ENV }; // Make copy
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // Restore old environment
+  });
+
   it('picks the test .env file', () => {
     let getGeoLevel = Number(setEnv('REACT_APP_PLAN_ASSIGNMENT_AT_GEO_LEVEL', '0'));
     // expect the value in the .env.test
     expect(getGeoLevel).toEqual(1);
   });
   it('sets an env var value to integer type', () => {
-    const envModule = require('../env');
-    envModule.PLAN_ASSIGNMENT_AT_GEO_LEVEL = 1;
-    let getGeoLevel = Number(setEnv('REACT_APP_PLAN_ASSIGNMENT_AT_GEO_LEVEL', '1'));
-    expect(typeof getGeoLevel).toEqual('number');
-    getGeoLevel = setEnv('REACT_APP_PLAN_ASSIGNMENT_AT_GEO_LEVEL', '1');
+    // defaults to string
+    let getGeoLevel = setEnv('REACT_APP_PLAN_ASSIGNMENT_AT_GEO_LEVEL', '0');
     expect(typeof getGeoLevel).toEqual('string');
+    // sets to number
+    getGeoLevel = Number(setEnv('REACT_APP_PLAN_ASSIGNMENT_AT_GEO_LEVEL', '0'));
+    expect(typeof getGeoLevel).toEqual('number');
+  });
+  it('disable team member reassignment defaults to true', () => {
+    // matches .env.test (REACT_APP_DISABLE_TEAM_MEMBER_REASSIGNMENT=false)
+    expect(DISABLE_TEAM_MEMBER_REASSIGNMENT).toStrictEqual(false);
+    // remove var from env (simulate not set)
+    delete process.env.REACT_APP_DISABLE_TEAM_MEMBER_REASSIGNMENT;
+    // require again - to force re-evaluation after delete
+    const actual = jest.requireActual('../env').DISABLE_TEAM_MEMBER_REASSIGNMENT;
+    // expect default true if not set
+    expect(actual).toStrictEqual(true);
   });
 });
