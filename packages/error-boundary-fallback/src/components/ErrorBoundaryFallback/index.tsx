01ee8b99ed57e72c6ea8fd44a177f6df6c98fd7b
p-netm <pmuriuki98@gmail.com>
2021-12-01 10:05:40 +0300

    Refactor tranlsations in errorBoundaryFallback

--- packages/error-boundary-fallback/src/components/ErrorBoundaryFallback/index.tsx
+++ packages/error-boundary-fallback/src/components/ErrorBoundaryFallback/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Result, Button } from 'antd';
-import lang from '../../lang';
-
+import { useTranslation } from '../../mls';
 interface Props {
   homeUrl?: string;
 }
 
 const ErrorBoundaryFallback = (props: Props) => {
   const { homeUrl } = props;
+  const { t } = useTranslation();
   return (
     <Result
       status="error"
-      title={lang.PAGE_TITLE}
-      subTitle={lang.PAGE_SUB_TITLE}
+      title={t('An Error Occurred')}
+      subTitle={t(
+        'There has been an error. It’s been reported to the site administrators via email and should be fixed shortly. Thanks for your patience.'
+      )}
       extra={
         <Button
           id="backHome"
@@ -20,7 +22,7 @@ const ErrorBoundaryFallback = (props: Props) => {
           onClick={() => (window.location.href = homeUrl ? homeUrl : '/')}
           type="primary"
         >
-          {lang.BUTTON_TITLE}
+          {t('Back Home')}
         </Button>
       }
     />
