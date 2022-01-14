47999dd1f875689770432f31a8dae26318071776
p-netm <pmuriuki98@gmail.com>
2022-01-14 11:30:06 +0300

    Transform @helsenorge modules during test runs
    Mock fhirClient entry to resolve to the browser entry point

--- jest.config.js
+++ jest.config.js
@@ -11,13 +11,20 @@ module.exports = {
     '!**/dist/**',
     '!**/{tests,__tests__}/**',
   ],
+  // this modules are not transpiled to commonjs, they also have un-transpiled scss code
+  // which is added in the moduleNameMapper config further below
+  transformIgnorePatterns: [
+    'node_modules/(?!(@helsenorge/toolkit|@helsenorge/core-utils|@helsenorge/designsystem-react)/)',
+  ],
   setupFiles: ['./setupTests'],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   roots: ['packages/', 'app'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+    // force the entry point of fhirclient to be that used on the browser as opposed to that of node
+    fhirclient: '<rootDir>/node_modules/fhirclient/lib/entry/browser',
   },
 };
 
