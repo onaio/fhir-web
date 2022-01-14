edb6f24a655cd9f5c3b9091fee62aea21659b5fb
p-netm <pmuriuki98@gmail.com>
2022-01-14 18:02:58 +0300

    Add fhir-resources package

---
+++ packages/fhir-resources/src/QuestionnaireResponse/tests/questionnaireResponse.test.tsx
@@ -0,0 +1,36 @@
+import { parseResource } from '../questionResponse';
+import { openChoiceQuestRes } from './fixtures';
+
+test('parse Resource is ok', () => {
+  expect(parseResource(openChoiceQuestRes)).toEqual({
+    author: undefined,
+    authoredDateTime: undefined,
+    basedOn: undefined,
+    id: '321',
+    partOf: undefined,
+    questionnaire: 'Questionnaire/123',
+    questionnaireVersion: 'current',
+    rootItems: [
+      {
+        answer: [
+          {
+            item: [
+              {
+                linkId: '/G1/Q4-help',
+                text: 'Favorite toppings',
+              },
+            ],
+            valueCoding: {
+              code: 'Ham',
+              display: 'Ham',
+            },
+          },
+        ],
+        linkId: '/G1/Q4',
+        text: 'What pizza toppings would you like?',
+      },
+    ],
+    source: undefined,
+    subject: undefined,
+  });
+});
