cde1a85ab465934b7d79015fe75c7b5f434cbb59
Macharia Muguku <machariamuguku@gmail.com>
2022-01-17 22:01:17 +0300

    Revert "fix card support location hierarchy"

--- packages/card-support/src/constants.ts
+++ packages/card-support/src/constants.ts
@@ -1,9 +1,7 @@
 // OPENSRP API endpoints
+export const OPENSRP_URL_USER_ASSIGNMENT = 'organization/user-assignment';
 export const OPENSRP_URL_CLIENT_SEARCH = 'client/search';
 export const OPENSRP_URL_LOCATION_HIERARCHY = 'location/hierarchy';
-export const PRACTITIONER_FROM_USER = `practitioner/user`;
-export const ORGANIZATION_BY_PRACTITIONER = 'organization/by-practitioner';
-export const GET_ASSIGNMENTS_ENDPOINT = `organization/assignedLocationsAndPlans`;
 
 /** CSV File MIME Types */
 export const TEXT_CSV = 'text/csv';
