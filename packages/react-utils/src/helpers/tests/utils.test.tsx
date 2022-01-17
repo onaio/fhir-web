912ba950fffac21ff416a0ae06f853c3e19bfc44
p-netm <pmuriuki98@gmail.com>
2022-01-17 21:37:36 +0300

    Cleanup fhir-resource package

--- packages/react-utils/src/helpers/tests/utils.test.tsx
+++ packages/react-utils/src/helpers/tests/utils.test.tsx
@@ -1,23 +1,7 @@
 import { intlFormatDateStrings } from '../utils';
-import * as configs from '@opensrp/pkg-config';
-
-jest.mock('@opensrp/pkg-config', () => ({
-  __esModule: true,
-  ...Object.assign({}, jest.requireActual('@opensrp/pkg-config')),
-}));
 
 test('date formatting works correctly ', () => {
-  jest.spyOn(configs, 'getConfig').mockImplementation(() => {
-    return { language: 'en_core' };
-  });
   expect(intlFormatDateStrings('01 Jan 1970 00:00:00 GMT')).toEqual('1/1/1970');
-  expect(intlFormatDateStrings('2011-10-10')).toEqual('10/10/2011');
-  expect(intlFormatDateStrings('2011-10-10T14:48:00.000+09:00')).toEqual('10/10/2011');
-
-  jest.spyOn(configs, 'getConfig').mockImplementation(() => {
-    return { language: 'fr-FR' };
-  });
-  expect(intlFormatDateStrings('01 Jan 1970 00:00:00 GMT')).toEqual('01/01/1970');
   expect(intlFormatDateStrings('2011-10-10')).toEqual('10/10/2011');
   expect(intlFormatDateStrings('2011-10-10T14:48:00.000+09:00')).toEqual('10/10/2011');
 });
