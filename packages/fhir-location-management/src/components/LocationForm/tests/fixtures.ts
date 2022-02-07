2690b2a45a2e5a4dac25f541151b7ef81ecd333d
p-netm <pmuriuki98@gmail.com>
2022-02-08 02:26:47 +0300

    Test regression fixes

--- packages/fhir-location-management/src/components/LocationForm/tests/fixtures.ts
+++ packages/fhir-location-management/src/components/LocationForm/tests/fixtures.ts
@@ -1,13 +1,13 @@
 import { ILocation } from '@smile-cdr/fhirts/dist/FHIR-R4/interfaces/ILocation';
 
-export const createdLocation1 = {
+export const createdLocation1 = ({
   resourceType: 'Location',
   status: 'suspended',
   name: 'area51',
   alias: 'creepTown',
   description:
     'The secret Nevada base, known as Area 51, is often the subject of alien conspiracy theories.',
-  partOf: { reference: 'Location/Location/303' },
+  partOf: { reference: 'Location/303', display: 'Ona Office Sub Location' },
   identifier: [{ use: 'official', value: '9b782015-8392-4847-b48c-50c11638656b' }],
   physicalType: {
     coding: [
@@ -18,7 +18,7 @@ export const createdLocation1 = {
       },
     ],
   },
-} as ILocation;
+} as unknown) as ILocation;
 
 export const createdLocation2 = {
   ...createdLocation1,
