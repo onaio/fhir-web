0408c1d040cdcf3d012da5820e474e3fad2c3a6c
Macharia Muguku <machariamuguku@gmail.com>
2022-02-21 01:38:03 +0300

    add eslint explicitly, @typescript-eslint was merged into prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
     'plugin:prettier/recommended',
     'plugin:cypress/recommended',
     'typestrict',
