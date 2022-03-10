f29f6f0621fabffa66608cae9825e0ede5c10f61
Macharia Muguku <machariamuguku@gmail.com>
2022-03-10 12:41:33 +0300

    only render responsibility for view details

--- packages/keycloak-user-management/src/components/UserGroupDetailView/index.tsx
+++ packages/keycloak-user-management/src/components/UserGroupDetailView/index.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { Col, Space, Typography, Spin } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 import { Resource404 } from '@opensrp/react-utils';
-import { sendErrorNotification } from '@opensrp/notifications';
 import { Button } from 'antd';
 import { URL_USER_EDIT } from '../../constants';
 import { UserGroupMembers } from '../UserGroupsList';
-import { KeycloakUserGroup } from 'keycloak-user-management/src/ducks/userGroups';
 import { Link } from 'react-router-dom';
-import { loadGroupDetails, loadGroupMembers } from '../UserGroupsList/utils';
 import lang from '../../lang';
+import { KeycloakUserGroup } from '../../ducks/userGroups';
 const { Text } = Typography;
 
 /** typings for the view details component */
 export interface ViewDetailsProps {
-  groupId: string;
-  keycloakBaseURL: string;
+  isUserGroupMembersLoading: boolean;
+  isUserGroupMembersError: boolean;
+  userGroupMembers: UserGroupMembers[] | undefined;
+  isGroupDetailsLoading: boolean;
+  isGroupDetailsError: boolean;
+  GroupDetails: KeycloakUserGroup | undefined;
   onClose: () => void;
 }
 
@@ -26,34 +28,24 @@ export interface ViewDetailsProps {
  * @param props - detail view component props
  */
 const ViewDetails = (props: ViewDetailsProps) => {
-  const { groupId, keycloakBaseURL, onClose } = props;
-  const [loading, setLoading] = React.useState<boolean>(true);
-  const [userGroupMembers, setUserGroupMembers] = React.useState<UserGroupMembers[] | null>(null);
-  const [singleUserGroup, setSingleUserGroup] = React.useState<KeycloakUserGroup | null>(null);
-
-  React.useEffect(() => {
-    if (groupId) {
-      setLoading(true);
-      const membersPromise = loadGroupMembers(groupId, keycloakBaseURL, setUserGroupMembers);
-      const userGroupPromise = loadGroupDetails(groupId, keycloakBaseURL, setSingleUserGroup);
-      Promise.allSettled([membersPromise, userGroupPromise])
-        .catch(() => {
-          sendErrorNotification(lang.ERROR_OCCURED);
-        })
-        .finally(() => setLoading(false));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [groupId]);
+  const {
+    isUserGroupMembersLoading,
+    isUserGroupMembersError,
+    userGroupMembers,
+    isGroupDetailsLoading,
+    isGroupDetailsError,
+    GroupDetails,
+    onClose,
+  } = props;
 
   return (
     <Col className="view-details-content">
       <div className="flex-right">
         <Button icon={<CloseOutlined />} shape="circle" type="text" onClick={() => onClose()} />
       </div>
-      {loading ? (
+      {isUserGroupMembersLoading || isGroupDetailsLoading ? (
         <Spin size="large" className="custom-ant-spin" />
-      ) : // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      !loading && groupId && (!singleUserGroup || !userGroupMembers) ? (
+      ) : isUserGroupMembersError || isGroupDetailsError || !GroupDetails || !userGroupMembers ? (
         <Resource404 />
       ) : (
         <Space direction="vertical">
@@ -61,18 +53,18 @@ const ViewDetails = (props: ViewDetailsProps) => {
             {lang.NAME}
           </Text>
           <Text type="secondary" className="display-block">
-            {singleUserGroup?.name}
+            {GroupDetails.name}
           </Text>
           <Text strong={true} className="display-block">
             {lang.GROUP_UUID}
           </Text>
           <Text type="secondary" className="display-block">
-            {singleUserGroup?.id}
+            {GroupDetails.id}
           </Text>
           <Text strong={true} className="display-block">
             {lang.ROLES}
           </Text>
-          {singleUserGroup?.realmRoles?.map((role: string) => (
+          {GroupDetails.realmRoles?.map((role: string) => (
             <Text key={role} type="secondary" className="display-block">
               {role}
             </Text>
@@ -80,7 +72,7 @@ const ViewDetails = (props: ViewDetailsProps) => {
           <Text strong={true} className="display-block">
             {lang.MEMBERS}
           </Text>
-          {userGroupMembers?.map((object: UserGroupMembers) => (
+          {userGroupMembers.map((object: UserGroupMembers) => (
             <Link key={object.id} to={`${URL_USER_EDIT}/${object.id}`}>
               {object.username}
             </Link>
