7828bb4a68b1d0aba17e1b505b1f6aedac705f26
Macharia Muguku <machariamuguku@gmail.com>
2022-03-30 15:28:35 +0300

    change download reports endpoint

--- packages/reports/src/components/DistrictReport/utils.tsx
+++ packages/reports/src/components/DistrictReport/utils.tsx
@@ -1,5 +1,5 @@
 import { OpenSRPService } from '@opensrp/server-service';
-import { OPENSRP_URL_GENERATE_REPORT } from '../../constants';
+import { OPENSRP_URL_DOWNLOAD_REPORT } from '../../constants';
 import { downloadFile } from '@opensrp/react-utils';
 
 export const submitForm = async (
@@ -8,9 +8,9 @@ export const submitForm = async (
   accessToken: string,
   openSRPBaseURL: string
 ) => {
-  const COMPOSED_GENERATE_REPORT_URL = `${OPENSRP_URL_GENERATE_REPORT}/${districtId}/${period}`;
+  const COMPOSED_DOWNLOAD_REPORT_URL = `${OPENSRP_URL_DOWNLOAD_REPORT}/${districtId}/${period}`;
 
-  const serve = new OpenSRPService(accessToken, openSRPBaseURL, COMPOSED_GENERATE_REPORT_URL);
+  const serve = new OpenSRPService(accessToken, openSRPBaseURL, COMPOSED_DOWNLOAD_REPORT_URL);
   const blob = await serve.download();
 
   downloadFile(blob, `district_report_${period}.csv`);
