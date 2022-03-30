a1fca1f1a28cd79635e8d6fbc939be03e98f0804
Macharia Muguku <machariamuguku@gmail.com>
2022-03-30 15:28:35 +0300

    change download reports endpoint

--- packages/reports/src/constants.ts
+++ packages/reports/src/constants.ts
@@ -1,4 +1,4 @@
 // OPENSRP API endpoints
 export const SECURITY_AUTHENTICATE = 'security/authenticate';
 export const OPENSRP_URL_LOCATION_HIERARCHY = 'location/hierarchy';
-export const OPENSRP_URL_GENERATE_REPORT = 'report/generate';
+export const OPENSRP_URL_DOWNLOAD_REPORT = 'report/download';
