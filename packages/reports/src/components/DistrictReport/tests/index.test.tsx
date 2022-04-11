af4554c2f828c8265c0b6e0c3dc094789f5c9d70
Macharia Muguku <machariamuguku@gmail.com>
2022-04-11 22:24:16 +0300

    bump coverage - cover submit and fetch failure paths

--- packages/reports/src/components/DistrictReport/tests/index.test.tsx
+++ packages/reports/src/components/DistrictReport/tests/index.test.tsx
@@ -14,6 +14,7 @@ import MockDate from 'mockdate';
 import * as notifications from '@opensrp/notifications';
 import { sampleTeamAssignment, locationsHierarchy } from './fixtures';
 import lang from '../../../lang';
+import * as submitMocks from '../utils';
 
 const history = createBrowserHistory();
 
@@ -42,6 +43,11 @@ describe('DistrictReport', () => {
         }
       )
     );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    fetch.resetMocks();
   });
 
   it('renders without crashing', () => {
@@ -89,6 +95,9 @@ describe('DistrictReport', () => {
 
   it('submit is disabled until location and date range are selected', async () => {
     MockDate.set('2022-01-13T19:31:00.000Z');
+
+    jest.mock('../utils.tsx');
+    const submitFormMock = jest.spyOn(submitMocks, 'submitForm');
 
     fetch.mockOnce(JSON.stringify(sampleTeamAssignment));
     fetch.mockOnce(JSON.stringify(locationsHierarchy));
@@ -144,13 +153,25 @@ describe('DistrictReport', () => {
       wrapper.update();
     });
 
-    wrapper.update();
-
     expect(wrapper.find('Button[htmlType="submit"]').prop('disabled')).toEqual(false);
+
+    await act(async () => {
+      wrapper.find('Button[htmlType="submit"]').simulate('submit');
+      await flushPromises();
+      wrapper.update();
+    });
+
+    expect(submitFormMock).toHaveBeenLastCalledWith(
+      'some-location-uuid',
+      '2022-01',
+      'hunter2',
+      'https://some.open.opensrp.url/opensrp/rest/'
+    );
   });
 
   it('handles fetch error when fetching user data - team assignments', async () => {
     fetch.mockRejectOnce(new Error('API is down'));
+    fetch.mockOnce(JSON.stringify(locationsHierarchy));
 
     const notificationErrorMock = jest.spyOn(notifications, 'sendErrorNotification');
 
@@ -181,5 +202,37 @@ describe('DistrictReport', () => {
     expect(notificationErrorMock).toHaveBeenCalledWith(
       lang.USER_NOT_ASSIGNED_AND_USERS_TEAM_NOT_ASSIGNED
     );
+  });
+  it('handles fetch error when fetching user data - locations hierarchy', async () => {
+    fetch.mockOnce(JSON.stringify(sampleTeamAssignment));
+    fetch.mockRejectOnce(new Error('API is down'));
+
+    const notificationErrorMock = jest.spyOn(notifications, 'sendErrorNotification');
+
+    // turn retries off - makes fetch fail on first try
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
+    const wrapper = mount(
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Router history={history}>
+            <DistrictReport {...downloadProps} />
+          </Router>
+        </Provider>
+      </QueryClientProvider>
+    );
+
+    await act(async () => {
+      await flushPromises();
+      wrapper.update();
+    });
+
+    expect(notificationErrorMock).toHaveBeenCalledWith(lang.ERROR_OCCURRED);
   });
 });
