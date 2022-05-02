74fa6a800cbcd238ccdacc7924665ea9c7444eba
Peter Muriuki <pmuriuki98@gmail.com>
2022-05-02 23:39:20 +0300

    Add localization support for fhir-views

--- packages/fhir-views/src/QuestionnaireList/index.tsx
+++ packages/fhir-views/src/QuestionnaireList/index.tsx
@@ -13,6 +13,8 @@ import { PageHeader, Row, Col, Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { Helmet } from 'react-helmet';
 import { ParsedQuestionnaire, parseQuestionnaire } from '@opensrp/fhir-resources';
+import { useTranslation } from '../mls';
+import type { TFunction } from '@opensrp/i18n';
 import './index.css';
 
 /** props for the PlansList view */
@@ -28,9 +30,12 @@ interface QuestionnaireListProps {
 export const ActionsColumnCustomRender: Column<ParsedQuestionnaire>['render'] = (
   record: IQuestionnaire
 ) => {
+  const { t } = useTranslation();
   return (
     <>
-      <Link to={`${QUEST_RESPONSE_VIEW_URL}/${record.id}`}>View Questionnaire Responses</Link>
+      <Link to={`${QUEST_RESPONSE_VIEW_URL}/${record.id}`}>
+        {t('View Questionnaire Responses')}
+      </Link>
     </>
   );
 };
@@ -50,33 +55,34 @@ export const NamesColumnCustomRender: Column<ParsedQuestionnaire>['render'] = (
 /**
  * generates columns for questionnaire rendering component
  *
+ * @param t
  */
-export const getColumns = (): Column<ParsedQuestionnaire>[] => {
+export const getColumns = (t: TFunction): Column<ParsedQuestionnaire>[] => {
   const columns: Column<ParsedQuestionnaire>[] = [
     {
-      title: 'Name/Id',
+      title: t('Name/Id'),
       render: NamesColumnCustomRender,
       width: '20%',
     },
     {
-      title: 'Status',
+      title: t('Status'),
       dataIndex: 'status' as const,
     },
     {
-      title: 'Publisher',
+      title: t('Publisher'),
       dataIndex: 'publisher' as const,
     },
     {
-      title: 'Version',
+      title: t('Version'),
       dataIndex: 'version' as const,
     },
     {
-      title: 'date',
+      title: t('date'),
       dataIndex: 'date' as const,
       render: (value) => intlFormatDateStrings(value),
     },
     {
-      title: 'Actions',
+      title: t('Actions'),
       render: ActionsColumnCustomRender,
       width: '20%',
     },
@@ -91,6 +97,7 @@ export const getColumns = (): Column<ParsedQuestionnaire>[] => {
  */
 const QuestionnaireList = (props: QuestionnaireListProps) => {
   const { fhirBaseURL } = props;
+  const { t } = useTranslation();
 
   const { searchFormProps, tablePaginationProps, queryValues } = useSimpleTabularView(
     fhirBaseURL,
@@ -98,7 +105,7 @@ const QuestionnaireList = (props: QuestionnaireListProps) => {
   );
   const { data, isFetching, isLoading, error } = queryValues;
 
-  const columns = getColumns();
+  const columns = getColumns(t);
   const dataSource = ((data?.records ?? []) as IQuestionnaire[]).map(parseQuestionnaire);
   const tableProps = {
     datasource: dataSource,
@@ -110,7 +117,7 @@ const QuestionnaireList = (props: QuestionnaireListProps) => {
   if (error && !data) {
     return <BrokenPage errorMessage={(error as Error).message} />;
   }
-  const pageTitle = 'Questionnaire list view';
+  const pageTitle = t('Questionnaire list view');
 
   return (
     <div className="content-section fhir-resource-container">
@@ -126,7 +133,7 @@ const QuestionnaireList = (props: QuestionnaireListProps) => {
             <Link to={'#'}>
               <Button type="primary" disabled={true}>
                 <PlusOutlined />
-                Create questionnaire
+                {t('Create questionnaire')}
               </Button>
             </Link>
           </div>
