a4f8363d9c52d83a392cc90249e4b71c455501d4
p-netm <pmuriuki98@gmail.com>
2022-05-09 12:52:17 +0300

    Sanitize assignments fetched in team detail view (#984)
    * Sanitize assignments fetched in team detail view
    
    * Refactor docstrin in sanitize assignments

--- packages/team-management/src/ducks/tests/fixtures.ts
+++ packages/team-management/src/ducks/tests/fixtures.ts
@@ -60,3 +60,53 @@ export const teamMember = {
 };
 
 export const organizations: Organization[] = [org1, org2];
+
+const assignment1 = {
+  organizationId: null,
+  jurisdictionId: 'noOrganization',
+  planId: '335ef7a3-7f35-58aa-8263-4419464946d8',
+  fromDate: 1600128000000,
+  toDate: 1600128000000,
+};
+
+const assignment2 = {
+  organizationId: 'validAsignment',
+  jurisdictionId: 'null',
+  planId: '335ef7a3-7f35-58aa-8263-4419464946d8',
+  fromDate: 1600128000000,
+  toDate: 1600128000000,
+};
+
+export const assignments = [assignment1, assignment2];
+export const practitioners = [
+  {
+    identifier: '390d8961-65d6-41e7-9c2b-c20fdd71b923',
+    active: true,
+    name: 'Test11 Eleven',
+    userId: 'ff2e15be-b4d8-4065-9a92-f6829b9e1cfd',
+    username: 'test11',
+    dateCreated: 1644219812336,
+    dateEdited: 1644219812336,
+    serverVersion: 5,
+  },
+  {
+    identifier: '39c178b3-8cd4-434c-a597-74f17200ccda',
+    active: true,
+    name: 'Gareth Graham',
+    userId: '0e9e8304-2c1b-42c5-a673-a91eafc67970',
+    username: 'ggraham',
+    dateCreated: 1644219812336,
+    dateEdited: 1644219812336,
+    serverVersion: 12,
+  },
+  {
+    identifier: '296a9687-c7fe-4571-8b70-f6aa14e7990c',
+    active: true,
+    name: 'Demo dev User',
+    userId: '821f587d-734f-4263-8d50-ec37f2e84ef4',
+    username: 'demo',
+    dateCreated: 1644219812336,
+    dateEdited: 1644219812336,
+    serverVersion: 14,
+  },
+];
