d175082c8a2797ceb032f004becb90b15cb2a937
Peter Muriuki <pmuriuki98@gmail.com>
2022-09-29 13:04:58 +0300

    Fix action column styling in location-list

--- packages/fhir-location-management/src/components/LocationUnitList/Table.tsx
+++ packages/fhir-location-management/src/components/LocationUnitList/Table.tsx
@@ -54,7 +54,7 @@ const Table: React.FC<Props> = (props: Props) => {
         width: '10%',
         // eslint-disable-next-line react/display-name
         render: (_: boolean, record) => (
-          <span className="d-flex justify-content-end align-items-center Actions">
+          <span>
             <Link to={`${URL_LOCATION_UNIT_EDIT}/${record.id}`}>
               <Button type="link" className="m-0 p-1">
                 {t('Edit')}
