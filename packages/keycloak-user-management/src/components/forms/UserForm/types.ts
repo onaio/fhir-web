cf9f82e485144174aad57985c065968d7ba5c1c6
Macharia Muguku <machariamuguku@gmail.com>
2022-11-11 01:46:29 +0300

    add usertype and practitioner role types to form fields, initialize them in initial form values

--- packages/keycloak-user-management/src/components/forms/UserForm/types.ts
+++ packages/keycloak-user-management/src/components/forms/UserForm/types.ts
@@ -2,14 +2,17 @@ import { KeycloakUser, Practitioner, UserAttributes, UserGroup } from '../../../
 import { Dictionary } from '@onaio/utils';
 import { IPractitioner } from '@smile-cdr/fhirts/dist/FHIR-R4/interfaces/IPractitioner';
 import { TFunction } from '@opensrp/i18n';
+import { IPractitionerRole } from '@smile-cdr/fhirts/dist/FHIR-R4/interfaces/IPractitionerRole';
 
 export interface FormFields
   extends Pick<KeycloakUser, 'id' | 'username' | 'firstName' | 'lastName' | 'email' | 'enabled'> {
   active?: boolean;
+  userType?: 'practitioner' | 'supervisor';
   userGroups?: string[];
   practitioner?: Practitioner | IPractitioner;
   keycloakUser?: KeycloakUser;
   contact?: string;
+  practitionerRole?: IPractitionerRole;
 }
 
 export type FormFieldsKey = keyof FormFields | keyof UserAttributes;
