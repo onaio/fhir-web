3176d140ac133aaffcbfcec6fd7de98fd6ada444
Macharia Muguku <machariamuguku@gmail.com>
2022-11-11 01:15:38 +0300

    add a practitioner role resource type constant

--- packages/fhir-keycloak-user-management/src/constants.ts
+++ packages/fhir-keycloak-user-management/src/constants.ts
@@ -2,6 +2,7 @@ export const practitionerResourceType = 'Practitioner';
 export const careTeamResourceType = 'CareTeam';
 export const organizationResourceType = 'Organization';
 export const groupResourceType = 'Group';
+export const practitionerRoleResourceType = 'PractitionerRole';
 
 // keycloak endpoints strings
 export const keycloakCountEndpoint = 'count';
