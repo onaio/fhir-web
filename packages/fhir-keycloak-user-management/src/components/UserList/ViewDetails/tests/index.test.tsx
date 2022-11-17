d0fed98e024cbdec5c22368d199133c4e32642ea
Macharia Muguku <machariamuguku@gmail.com>
2022-11-18 02:53:47 +0300

    add user type field in user details

--- packages/fhir-keycloak-user-management/src/components/UserList/ViewDetails/tests/index.test.tsx
+++ packages/fhir-keycloak-user-management/src/components/UserList/ViewDetails/tests/index.test.tsx
@@ -6,8 +6,10 @@ import { authenticateUser } from '@onaio/session-reducer';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import nock from 'nock';
 import { waitForElementToBeRemoved } from '@testing-library/dom';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
 import fetch from 'jest-fetch-mock';
+import { KeycloakUser } from '@opensrp/user-management';
+import { practitionerRoleResourceType } from '../../../../constants';
 
 jest.mock('fhirclient', () => {
   return jest.requireActual('fhirclient/lib/entry/browser');
@@ -27,6 +29,34 @@ const props = {
   fhirBaseUrl: 'http://test.server.org',
   keycloakBaseUrl: 'http://test-keycloak.server.org',
   resourceId: 405,
+};
+
+const keycloakUser = {
+  id: 'c1d36d9a-b771-410b-959e-af2c04d132a2',
+  username: 'allay_allan',
+} as KeycloakUser;
+
+const practitionerRoleBundle = {
+  resourceType: 'Bundle',
+  entry: [
+    {
+      resource: {
+        resourceType: 'PractitionerRole',
+        id: '38f12000-a066-45ef-8668-65d10e295279',
+        code: [
+          {
+            coding: [
+              {
+                system: 'http://snomed.info/sct',
+                code: '405623001',
+                display: 'Assigned practitioner',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
 
 beforeAll(() => {
@@ -79,4 +109,29 @@ test('responds as expected to errors', async () => {
   expect(screen.getByText(/coughid/)).toBeInTheDocument();
 
   expect(nock.isDone()).toBeTruthy();
+});
+
+test('shows user type in details', async () => {
+  fetch.mockOnce(JSON.stringify(keycloakUser));
+  fetch.mockResponseOnce(JSON.stringify([]));
+
+  nock(props.fhirBaseUrl)
+    .get(`/${practitionerRoleResourceType}/_search`)
+    .query({
+      identifier: props.resourceId,
+    })
+    .reply(200, practitionerRoleBundle);
+
+  const { getByText, getByTestId } = render(<AppWrapper {...props}></AppWrapper>);
+
+  expect(getByTestId('custom-spinner')).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(getByText(/Fetching linked practitionerRole/)).toBeInTheDocument();
+  });
+
+  await waitFor(() => {
+    expect(getByText(/User Type/)).toBeInTheDocument();
+    expect(getByTestId('user-type')).toBeInTheDocument();
+  });
 });
