06bdc2e42f0c37fd6c2618b2e0f2a88f15b34492
Peter Muriuki <pmuriuki98@gmail.com>
2022-11-23 00:27:12 +0300

    Creating commodity adds it to list resource

--- packages/fhir-group-management/src/components/CommodityAddEdit/tests/index.test.tsx
+++ packages/fhir-group-management/src/components/CommodityAddEdit/tests/index.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/naming-convention */
 import React from 'react';
 import { Route, Router, Switch } from 'react-router';
@@ -6,15 +7,31 @@ import { CommodityAddEdit } from '..';
 import { Provider } from 'react-redux';
 import { store } from '@opensrp/store';
 import nock from 'nock';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { waitForElementToBeRemoved } from '@testing-library/dom';
 import { createMemoryHistory } from 'history';
 import { authenticateUser } from '@onaio/session-reducer';
-import { commodity1 } from './fixtures';
-import { groupResourceType } from '../../../constants';
+import { commodity1, createdCommodity, newList } from './fixtures';
+import { groupResourceType, listResourceType } from '../../../constants';
+import userEvent from '@testing-library/user-event';
+import * as notifications from '@opensrp/notifications';
+
+jest.mock('@opensrp/notifications', () => ({
+  __esModule: true,
+  ...Object.assign({}, jest.requireActual('@opensrp/notifications')),
+}));
 
 jest.mock('fhirclient', () => {
   return jest.requireActual('fhirclient/lib/entry/browser');
+});
+
+const mockv4 = '9b782015-8392-4847-b48c-50c11638656b';
+jest.mock('uuid', () => {
+  const actual = jest.requireActual('uuid');
+  return {
+    ...actual,
+    v4: () => mockv4,
+  };
 });
 
 const queryClient = new QueryClient({
@@ -26,8 +43,10 @@ const queryClient = new QueryClient({
   },
 });
 
+const listResId = 'list-resource-id';
 const props = {
   fhirBaseURL: 'http://test.server.org',
+  listId: listResId,
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -124,4 +143,127 @@ test('data loading problem', async () => {
 
   // errors out
   expect(screen.getByText(/something aweful happened/)).toBeInTheDocument();
+});
+
+test('#1116 adds new resources to list', async () => {
+  const history = createMemoryHistory();
+  history.push('/add');
+
+  nock(props.fhirBaseURL)
+    .put(`/${groupResourceType}/${mockv4}`, createdCommodity)
+    .reply(200, { ...createdCommodity, id: '123' })
+    .persist();
+
+  nock(props.fhirBaseURL).get(`/${listResourceType}/${listResId}`).reply(200, newList).persist();
+
+  render(
+    <Router history={history}>
+      <AppWrapper {...props}></AppWrapper>
+    </Router>
+  );
+
+  const successNoticeMock = jest
+    .spyOn(notifications, 'sendSuccessNotification')
+    .mockImplementation(() => undefined);
+
+  // simulate name change
+  const nameInput = document.querySelector('input#name')!;
+  userEvent.type(nameInput, 'Dettol');
+
+  // simulate value selection for type
+  const typeInput = document.querySelector('input#type')!;
+  userEvent.click(typeInput);
+  const deviceTitle = document.querySelector('[title="Device"]')!;
+  fireEvent.click(deviceTitle);
+
+  // simulate unit measure value
+  const unitOfMeasureInput = document.querySelector('input#unitOfMeasure')!;
+  userEvent.click(unitOfMeasureInput);
+  const bottlesOption = document.querySelector('[title="Bottles"]')!;
+  fireEvent.click(bottlesOption);
+
+  const submitButton = document.querySelector('#submit-button')!;
+  userEvent.click(submitButton);
+
+  await waitFor(() => {
+    expect(successNoticeMock.mock.calls).toEqual([['Commodity updated successfully']]);
+  });
+  expect(nock.isDone()).toBeTruthy();
+});
+
+test('#1116 adding new group but list does not exist', async () => {
+  const history = createMemoryHistory();
+  history.push('/add');
+
+  nock(props.fhirBaseURL)
+    .put(`/${groupResourceType}/${mockv4}`, createdCommodity)
+    .reply(200, { ...createdCommodity })
+    .persist();
+
+  nock(props.fhirBaseURL)
+    .get(`/${listResourceType}/${listResId}`)
+    .reply(404, {
+      resourceType: 'OperationOutcome',
+      text: {
+        status: 'generated',
+        div: '<div xmlns="http://www.w3.org/1999/xhtml"><h1>Operation Outcome</h1><table border="0"><tr><td style="font-weight: bold;">ERROR</td><td>[]</td><td><pre>HAPI-2001: Resource List/ea15c35a-8e8c-47ce-8122-c347cefa1b4d is not known</pre></td>\n\t\t\t</tr>\n\t\t</table>\n\t</div>',
+      },
+      issue: [
+        {
+          severity: 'error',
+          code: 'processing',
+          diagnostics: `HAPI-2001: Resource List/${listResId} is not known`,
+        },
+      ],
+    });
+
+  const updatedList = {
+    ...newList,
+    entry: [{ item: { reference: `${groupResourceType}/${mockv4}` } }],
+  };
+
+  nock(props.fhirBaseURL)
+    .put(`/${listResourceType}/${listResId}`, newList)
+    .reply(200, newList)
+    .persist();
+
+  nock(props.fhirBaseURL)
+    .put(`/${listResourceType}/${listResId}`, updatedList)
+    .reply(200, updatedList)
+    .persist();
+
+  render(
+    <Router history={history}>
+      <AppWrapper {...props}></AppWrapper>
+    </Router>
+  );
+
+  const successNoticeMock = jest
+    .spyOn(notifications, 'sendSuccessNotification')
+    .mockImplementation(() => undefined);
+
+  // simulate name change
+  const nameInput = document.querySelector('input#name')!;
+  userEvent.type(nameInput, 'Dettol');
+
+  // simulate value selection for type
+  const typeInput = document.querySelector('input#type')!;
+  userEvent.click(typeInput);
+  const deviceTitle = document.querySelector('[title="Device"]')!;
+  fireEvent.click(deviceTitle);
+
+  // simulate unit measure value
+  const unitOfMeasureInput = document.querySelector('input#unitOfMeasure')!;
+  userEvent.click(unitOfMeasureInput);
+  const bottlesOption = document.querySelector('[title="Bottles"]')!;
+  fireEvent.click(bottlesOption);
+
+  const submitButton = document.querySelector('#submit-button')!;
+  userEvent.click(submitButton);
+
+  await waitFor(() => {
+    expect(successNoticeMock.mock.calls).toEqual([['Commodity updated successfully']]);
+  });
+
+  expect(nock.isDone()).toBeTruthy();
 });
