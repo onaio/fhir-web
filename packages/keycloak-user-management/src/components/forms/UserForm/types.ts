dbafd154513e4e50e532cc26a1eb2274ad07f2f1
Macharia Muguku <machariamuguku@gmail.com>
2022-11-24 15:07:37 +0300

    add usertypes and usertype codes as string literals

--- packages/keycloak-user-management/src/components/forms/UserForm/types.ts
+++ packages/keycloak-user-management/src/components/forms/UserForm/types.ts
@@ -3,11 +3,12 @@ import { Dictionary } from '@onaio/utils';
 import { IPractitioner } from '@smile-cdr/fhirts/dist/FHIR-R4/interfaces/IPractitioner';
 import { TFunction } from '@opensrp/i18n';
 import { IPractitionerRole } from '@smile-cdr/fhirts/dist/FHIR-R4/interfaces/IPractitionerRole';
+import { PRACTITIONER, SUPERVISOR } from '../../../constants';
 
 export interface FormFields
   extends Pick<KeycloakUser, 'id' | 'username' | 'firstName' | 'lastName' | 'email' | 'enabled'> {
   active?: boolean;
-  userType?: 'practitioner' | 'supervisor';
+  userType?: typeof PRACTITIONER | typeof SUPERVISOR;
   userGroups?: string[];
   practitioner?: Practitioner | IPractitioner;
   keycloakUser?: KeycloakUser;
