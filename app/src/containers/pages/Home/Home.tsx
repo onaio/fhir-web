c5c9fb6ae9e25f683fbab3f2c2fd5424daebee8e
Peter Muriuki <pmuriuki98@gmail.com>
2023-03-29 12:02:49 +0300

    Refactor how call to action buttons on homepage are generated

--- app/src/containers/pages/Home/Home.tsx
+++ app/src/containers/pages/Home/Home.tsx
@@ -3,17 +3,29 @@ import { Col, Row, Button } from 'antd';
 import { Link } from 'react-router-dom';
 import './Home.css';
 import { useTranslation } from '../../../mls';
-import { URL_USER, URL_LOCATION_UNIT, URL_TEAM_ASSIGNMENT, URL_TEAMS } from '../../../constants';
-import { ENABLE_TEAMS_ASSIGNMENT_MODULE } from '../../../configs/env';
 import React from 'react';
-import {
-  COMPOSITE_ENABLE_LOCATIONS_MANAGEMENT,
-  COMPOSITE_ENABLE_TEAM_MANAGEMENT,
-  COMPOSITE_ENABLE_USER_MANAGEMENT,
-} from '../../../configs/settings';
+import { useSelector } from 'react-redux';
+import { getExtraData } from '@onaio/session-reducer';
+import { getRoutesForHomepage } from '../../../routes';
 
 export const Home = () => {
   const { t } = useTranslation();
+  const extraData = useSelector((state) => {
+    return getExtraData(state);
+  });
+  const { roles } = extraData;
+  const routes = React.useMemo(() => getRoutesForHomepage(roles as string[], t), [roles, t]);
+
+  let columnNum = 2;
+  if (routes.length < 5) {
+    columnNum = 2;
+  } else if (routes.length < 7) {
+    columnNum = 3;
+  } else {
+    columnNum = 4;
+  }
+
+  const normalSpanLength = 24 / columnNum;
 
   return (
     <div className="text-center home">
@@ -25,49 +37,24 @@ export const Home = () => {
           <h3>{t('Welcome to OpenSRP')}</h3>
         </Col>
       </Row>
-      <Row gutter={16} className="links-box">
-        {COMPOSITE_ENABLE_USER_MANAGEMENT && (
-          <Col className="gutter-row" span={12}>
-            <Link to={URL_USER} className="admin-link">
-              <Button color="outline" className="btn-links">
-                {t('User Management')}
-              </Button>
-            </Link>
-          </Col>
-        )}
-        {COMPOSITE_ENABLE_TEAM_MANAGEMENT && (
-          <Col className="gutter-row" span={12}>
-            <Link to={URL_TEAMS} className="admin-link">
-              <Button color="outline" className="btn-links">
-                {t('Team Management')}
-              </Button>
-            </Link>
-          </Col>
-        )}
-      </Row>
-      <Row gutter={16} className="links-box">
-        {COMPOSITE_ENABLE_LOCATIONS_MANAGEMENT && (
-          <>
-            <Col className="gutter-row" span={12}>
-              <Link to={URL_LOCATION_UNIT} className="admin-link">
+      <Row
+        gutter={[
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+        ]}
+        className="links-box"
+      >
+        {routes.map((route) => {
+          return (
+            <Col className="gutter-row" xl={normalSpanLength} lg={8} md={8} sm={12}>
+              <Link to={route.url as string} className="admin-link">
                 <Button color="outline" className="btn-links">
-                  {t('Location Units')}
+                  {route.title}
                 </Button>
               </Link>
             </Col>
-          </>
-        )}
-      </Row>
-      <Row gutter={16} className="links-box">
-        {ENABLE_TEAMS_ASSIGNMENT_MODULE && (
-          <Col className="gutter-row" span={12}>
-            <Link to={URL_TEAM_ASSIGNMENT} className="admin-link">
-              <Button color="outline" className="btn-links">
-                {t('Team Assignment')}
-              </Button>
-            </Link>
-          </Col>
-        )}
+          );
+        })}
       </Row>
     </div>
   );
