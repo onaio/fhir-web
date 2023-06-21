9d1f8a6ca5c1f5764004970f4750dc8f63b18ddf
Peter Muriuki <pmuriuki98@gmail.com>
2023-06-21 15:53:52 +0300

    Update team assignment to support new practitioner assignment

--- packages/fhir-team-management/src/components/AddEditOrganization/index.tsx
+++ packages/fhir-team-management/src/components/AddEditOrganization/index.tsx
@@ -21,6 +21,7 @@ import type { IPractitionerRole } from '@smile-cdr/fhirts/dist/FHIR-R4/interface
 import { IOrganization } from '@smile-cdr/fhirts/dist/FHIR-R4/interfaces/IOrganization';
 import { getOrgFormFields } from './utils';
 import { useTranslation } from '../../mls';
+import { getConfig } from '@opensrp/pkg-config';
 
 export interface AddEditOrganizationProps {
   fhirBaseURL: string;
@@ -35,6 +36,7 @@ export const AddEditOrganization = (props: AddEditOrganizationProps) => {
 
   const { id: orgId } = useParams<RouteParams>();
   const { t } = useTranslation();
+  const configuredPractAssignmentStrategy = getConfig('practToOrgAssignmentStrategy');
 
   const organization = useQuery(
     [organizationResourceType, orgId],
@@ -57,12 +59,9 @@ export const AddEditOrganization = (props: AddEditOrganizationProps) => {
   );
 
   // practitioners already assigned to this organization
-  const assignedPractitioners = useQuery(
+  const allPractitionerRoles = useQuery(
     [practitionerResourceType, organizationResourceType, orgId],
-    () =>
-      loadAllResources(fhirBaseUrl, practitionerRoleResourceType, {
-        organization: orgId as string,
-      }),
+    () => loadAllResources(fhirBaseUrl, practitionerRoleResourceType),
     {
       onError: () => sendErrorNotification(t('An Error occurred')),
       select: (res) => {
@@ -75,7 +74,7 @@ export const AddEditOrganization = (props: AddEditOrganizationProps) => {
   if (
     (!organization.isIdle && organization.isLoading) ||
     (!practitioners.isIdle && practitioners.isLoading) ||
-    (!assignedPractitioners.isIdle && assignedPractitioners.isLoading)
+    (!allPractitionerRoles.isIdle && allPractitionerRoles.isLoading)
   ) {
     return <Spin size="large" className="custom-spinner"></Spin>;
   }
@@ -84,7 +83,13 @@ export const AddEditOrganization = (props: AddEditOrganizationProps) => {
     return <BrokenPage errorMessage={(organization.error as Error).message} />;
   }
 
-  const initialValues = getOrgFormFields(organization.data, assignedPractitioners.data);
+  const assignedPractitionerRoles = (allPractitionerRoles.data ?? []).filter(
+    (practitionerRole) =>
+      practitionerRole.organization?.reference ===
+      `${organizationResourceType}/${(organization.data as IOrganization).id}`
+  );
+
+  const initialValues = getOrgFormFields(organization.data, assignedPractitionerRoles);
 
   const pageTitle = organization.data
     ? t('Edit team | {{teamName}}', { teamName: organization.data.name ?? '' })
@@ -101,9 +106,11 @@ export const AddEditOrganization = (props: AddEditOrganizationProps) => {
           fhirBaseUrl={fhirBaseUrl}
           initialValues={initialValues}
           practitioners={practitioners.data ?? []}
-          existingPractitionerRoles={assignedPractitioners.data ?? []}
+          existingPractitionerRoles={assignedPractitionerRoles}
+          allPractitionerRoles={allPractitionerRoles.data ?? []}
           cancelUrl={ORGANIZATION_LIST_URL}
           successUrl={ORGANIZATION_LIST_URL}
+          configuredPractAssignmentStrategy={configuredPractAssignmentStrategy}
         />
       </div>
     </section>
--- packages/fhir-team-management/src/components/AddEditOrganization/tests/index.test.tsx
+++ packages/fhir-team-management/src/components/AddEditOrganization/tests/index.test.tsx
@@ -15,7 +15,7 @@ import {
   practitionerResourceType,
   practitionerRoleResourceType,
 } from '../../../constants';
-import { allPractitioners, org105, org105Practitioners } from '../tests/fixtures';
+import { allPractitioners, org105 } from '../tests/fixtures';
 
 jest.mock('fhirclient', () => {
   return jest.requireActual('fhirclient/lib/entry/browser');
@@ -111,11 +111,11 @@ test('renders correctly for edit locations', async () => {
 
   nock(props.fhirBaseURL)
     .get(`/${practitionerRoleResourceType}/_search`)
-    .query({ _summary: 'count', organization: '105' })
+    .query({ _summary: 'count' })
     .reply(200, { total: 1000 })
     .get(`/${practitionerRoleResourceType}/_search`)
-    .query({ _count: 1000, organization: '105' })
-    .reply(200, org105Practitioners);
+    .query({ _count: 1000 })
+    .reply(200, allPractitioners);
 
   render(
     <Router history={history}>
