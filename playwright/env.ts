5514cc46109701f63e7df67b51abf007de0cbca1
Peter Muriuki <pmuriuki98@gmail.com>
2023-08-02 12:34:13 +0300

    Add env file

---
+++ playwright/env.ts
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+export const PLAYWRIGHT_BASE_URL = process.env.PLAYWRIGHT_BASE_URL ?? "http://localhost:3000/"
+export const PLAYWRIGHT_USERNAME = process.env.PLAYWRIGHT_USERNAME ?? ""
+export const PLAYWRIGHT_PASSWORD = process.env.PLAYWRIGHT_PASSWORD ?? ""
+export const PLAYWRIGHT_HEADLESS = process.env.PLAYWRIGHT_HEADLESS === "true"
