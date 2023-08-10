863f824a30817bd376c547d342662890c75afee6
Peter Muriuki <pmuriuki98@gmail.com>
2023-08-10 13:05:45 +0300

    Add faker js package

--- playwright/env.ts
+++ playwright/env.ts
@@ -4,3 +4,4 @@ export const PLAYWRIGHT_BASE_URL = process.env.PLAYWRIGHT_BASE_URL ?? "http://lo
 export const PLAYWRIGHT_USERNAME = process.env.PLAYWRIGHT_USERNAME ?? ""
 export const PLAYWRIGHT_PASSWORD = process.env.PLAYWRIGHT_PASSWORD ?? ""
 export const PLAYWRIGHT_HEADLESS = process.env.PLAYWRIGHT_HEADLESS === "true"
+export const PLAYWRIGHT_PREFIX = process.env.PLAYWRIGHT_PREFIX ?? "e2e"
