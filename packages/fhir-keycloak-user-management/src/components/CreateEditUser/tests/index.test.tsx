51a9a8e5ae18d11efa87f457dd4e7aaa3288fab8
Peter Muriuki <pmuriuki98@gmail.com>
2023-09-08 15:48:50 +0300

    Add username to credentials url for fhir user management

--- packages/fhir-keycloak-user-management/src/components/CreateEditUser/tests/index.test.tsx
+++ packages/fhir-keycloak-user-management/src/components/CreateEditUser/tests/index.test.tsx
@@ -28,6 +28,9 @@ import {
   practitionerRoleBundle,
   updatedPractitionerRole,
   compositionResource,
+  newPractitioner,
+  newPractitionerRole,
+  newGroup,
 } from './fixtures';
 import userEvent from '@testing-library/user-event';
 import * as notifications from '@opensrp/notifications';
@@ -45,11 +48,13 @@ jest.mock('@opensrp/notifications', () => ({
   ...Object.assign({}, jest.requireActual('@opensrp/notifications')),
 }));
 
+const mockId = 'acb9d47e-7247-448f-be93-7a193a5312da';
+
 jest.mock('uuid', () => {
   const actual = jest.requireActual('uuid');
   return {
     ...actual,
-    v4: () => 'acb9d47e-7247-448f-be93-7a193a5312da',
+    v4: () => mockId,
   };
 });
 
@@ -288,6 +293,172 @@ test('renders correctly for edit user', async () => {
       method: 'PUT',
     },
   ]);
+
+  expect(history.location.pathname).toEqual('');
+});
+
+test('renders correctly for new user', async () => {
+  history.push('/add');
+
+  const thisProps = {
+    ...props,
+    history,
+    location: {
+      hash: '',
+      pathname: '/add',
+      search: '',
+      state: '',
+    },
+    match: {
+      isExact: true,
+      params: {},
+      path: `/add`,
+      url: `/add`,
+    },
+    keycloakUser: null,
+  };
+
+  fetch.once(JSON.stringify(userGroup));
+  fetch.once(JSON.stringify(keycloakUser), { headers: { location: `/${keycloakUser.id}` } });
+
+  nock(props.baseUrl)
+    .get(`/${practitionerResourceType}/_search`)
+    .query({
+      identifier: keycloakUser.id,
+    })
+    .reply(200, practitioner);
+
+  nock(props.baseUrl)
+    .get('/PractitionerRole/_search')
+    .query({
+      identifier: keycloakUser.id,
+    })
+    .reply(200, practitionerRoleBundle);
+
+  nock(props.baseUrl)
+    .put(`/${practitionerResourceType}/${mockId}`, newPractitioner)
+    .reply(200, updatedPractitioner);
+
+  nock(props.baseUrl)
+    .put(`/${practitionerRoleResourceType}/${mockId}`, newPractitionerRole)
+    .reply(200, {});
+
+  nock(props.baseUrl)
+    .get(`/Group/_search`)
+    .query({
+      identifier: keycloakUser.id,
+    })
+    .reply(200, group);
+
+  nock(props.baseUrl).put(`/Group/${mockId}`, newGroup).reply(200, {});
+
+  nock(props.baseUrl)
+    .get(`/Composition/_search`)
+    .query({
+      _getpagesoffset: '0',
+      _count: '20',
+      type: `http://snomed.info/sct|1156600005`,
+      _elements: 'identifier,title',
+    })
+    .reply(200, compositionResource)
+    .persist();
+
+  const successStub = jest
+    .spyOn(notifications, 'sendSuccessNotification')
+    .mockImplementation(jest.fn);
+
+  const errorStub = jest.spyOn(notifications, 'sendErrorNotification').mockImplementation(jest.fn);
+
+  const { getByTestId, getByText, queryByTitle } = render(
+    <Router history={history}>
+      <AppWrapper {...thisProps}></AppWrapper>
+    </Router>
+  );
+
+  expect(getByTestId('custom-create-user-spinner')).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(getByText(/User Type/)).toBeInTheDocument();
+  });
+
+  expect(fetch.mock.calls.map((req) => req[0])).toEqual(['http://test-keycloak.server.org/groups']);
+
+  // simulate first Name change
+  const firstNameInput = document.querySelector('input#firstName');
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  userEvent.type(firstNameInput!, 'flotus');
+
+  const lastNameInput = document.querySelector('input#lastName');
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  userEvent.type(lastNameInput!, 'plotus');
+
+  const emailInput = document.querySelector('input#email');
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  userEvent.type(emailInput!, 'flotus@plotus.duck');
+
+  const usernameInput = document.querySelector('input#username');
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  userEvent.type(usernameInput!, 'flopo');
+
+  const markSupervisor = document.querySelectorAll('input[name="userType"]')[1];
+  userEvent.click(markSupervisor);
+
+  const submitButton = document.querySelector('button[type="submit"]');
+
+  // find antd Select with id 'fhirCoreAppId' in the 'Form' component
+  const appIdSection = document.querySelector('[data-testid="fhirCoreAppId"]') as Element;
+
+  // click on input. - should see the first 5 records by default
+  const appIdInput = appIdSection.querySelector('.ant-select-selector') as Element;
+
+  // simulate click on select - to show dropdown items
+  fireEvent.mouseDown(appIdInput);
+
+  // await waitForElementToBeRemoved(appIdSection.querySelector('.anticon-spin'));
+  await waitFor(() => {
+    const spin = appIdSection.querySelector('.anticon-spin');
+    expect(spin).toBeNull();
+  });
+
+  fireEvent.click(queryByTitle('Device configurations(cha)') as Element);
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  fireEvent.click(submitButton!);
+
+  // need not concern ourselves with groups, should be tested in user-management package
+
+  await waitFor(() => {
+    expect(errorStub).toHaveBeenCalledTimes(0);
+    expect(successStub.mock.calls).toEqual([
+      ['User created successfully'],
+      ['Practitioner created successfully'],
+      ['User Group edited successfully'],
+      ['Group resource created successfully'],
+      ['PractitionerRole created successfully'],
+    ]);
+  });
+
+  expect(fetch.mock.calls.map((req) => req[0])).toEqual([
+    'http://test-keycloak.server.org/groups',
+    'http://test-keycloak.server.org/users',
+  ]);
+
+  expect(
+    fetch.mock.calls.map((call) => ({
+      endpoint: call[0],
+      method: call[1]?.method,
+    }))
+  ).toEqual([
+    { endpoint: 'http://test-keycloak.server.org/groups', method: 'GET' },
+    {
+      endpoint: 'http://test-keycloak.server.org/users',
+      method: 'POST',
+    },
+  ]);
+
+  expect(history.location.pathname).toEqual(
+    '/admin/users/credentials/cab07278-c77b-4bc7-b154-bcbf01b7d35b/flopo'
+  );
 });
 
 test('it fetches groups', async () => {
