0e0e4f5e8b522a625b9ffde7015b99d9ca2c3888
Peter Muriuki <pmuriuki98@gmail.com>
2023-10-03 18:49:18 +0300

    Add initial scenarios for group module

---
+++ playwright/tests/scenarios/group_management.test.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import { PLAYWRIGHT_BASE_URL } from '../../env';
+import { HomePage } from '../poms/app/home';
+import { waitForSpinner } from '../helpers/utils';
+
+test.describe("Group Management", () => {
+    /**
+    * User can see present groups
+    */
+    test('Can see existing groups', async ({ page }) => {
+        const homePage = new HomePage(page)
+        await homePage.goto()
+
+        // go to group list view
+        await homePage.dashboard.groupsLink.click()
+        await waitForSpinner(page)
+        await expect(page).toHaveURL(`${PLAYWRIGHT_BASE_URL}/groups/list`)
+
+        // confirm list view is loaded.
+        await expect(homePage.dashboard.section.getByRole("heading", { name: /Groups List/i })).toBeVisible()
+
+        // we do not know what groups would be here
+
+        const tableHeader = homePage.page.locator('table thead')
+        await expect(tableHeader.allTextContents()).toEqual("")
+    })
+})
