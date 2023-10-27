a3f2f156b74015a3b88e90c983222f90a0c99b49
Peter Muriuki <pmuriuki98@gmail.com>
2023-10-27 15:04:51 +0300

    Second iteration ideating data flow

--- packages/fhir-keycloak-user-management/src/index.tsx
+++ packages/fhir-keycloak-user-management/src/index.tsx
@@ -1,3 +1,5 @@
 export * from './components/CreateEditUser';
 export * from './components/UserList/ListView';
 export * from './constants';
+import {UserDetails as UserDetailsV2} from './components/UserList/UserDetails-v2';
+export {UserDetailsV2};
