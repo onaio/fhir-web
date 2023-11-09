12c02fa7b096808df81ba18033071ae79b98984f
Peter Muriuki <pmuriuki98@gmail.com>
2023-11-09 15:26:25 +0300

    Integrate rbac into user detail view

--- packages/fhir-keycloak-user-management/src/components/UserList/Viewdetails/ViewDetailResources/tests/practitionerDetails.test.tsx
+++ packages/fhir-keycloak-user-management/src/components/UserList/Viewdetails/ViewDetailResources/tests/practitionerDetails.test.tsx
@@ -17,5 +17,8 @@ test('renders without crashing', () => {
 
   const tableRows = document.querySelectorAll('table tr');
   const text = [...tableRows].map((tr) => tr.textContent);
-  expect(text).toEqual(['NameActiveCoding', 'test1147 1147Active']);
+  expect(text).toEqual([
+    'IdNameActiveUser TypePractitioner Role Coding',
+    '3a801d6e-7bd3-4a5f-bc9c-64758fbb3dadtest1147 1147ActivepractitionerAssigned practitioner(http://snomed.info/sct|405623001), ',
+  ]);
 });
