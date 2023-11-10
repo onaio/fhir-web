09f44136001c270da7df17ad4ff00313d5db7100
Peter Muriuki <pmuriuki98@gmail.com>
2023-11-10 11:26:24 +0300

    Update tests

--- packages/fhir-location-management/src/components/AddEditLocationUnit/tests/index.test.tsx
+++ packages/fhir-location-management/src/components/AddEditLocationUnit/tests/index.test.tsx
@@ -12,6 +12,21 @@ import { fhirHierarchy } from '../../../ducks/tests/fixtures';
 import { waitForElementToBeRemoved } from '@testing-library/dom';
 import { createdLocation1 } from '../../LocationForm/tests/fixtures';
 import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as notifications from '@opensrp/notifications';
+
+jest.mock('@opensrp/notifications', () => ({
+  __esModule: true,
+  ...Object.assign({}, jest.requireActual('@opensrp/notifications')),
+}));
+
+jest.mock('uuid', () => {
+  const actual = jest.requireActual('uuid');
+  return {
+    ...actual,
+    v4: () => '13cafa46-7251-429a-8d19-8da0583c0c5a',
+  };
+});
 
 jest.mock('fhirclient', () => {
   return jest.requireActual('fhirclient/lib/entry/browser');
@@ -127,6 +142,68 @@ test('renders correctly for edit locations', async () => {
   // some small but inconclusive proof that the form rendered and has some initial values
   expect(screen.getByLabelText(/name/i)).toMatchSnapshot('name field');
   expect(screen.getByLabelText(/alias/i)).toMatchSnapshot('alias field');
+});
+
+test('#1277 - works ok for new locations', async () => {
+  const history = createMemoryHistory();
+  history.push(`/add`);
+
+  const notificationSuccessMock = jest.spyOn(notifications, 'sendSuccessNotification');
+  const notificationErrorMock = jest.spyOn(notifications, 'sendErrorNotification');
+
+  nock(props.fhirBaseURL)
+    .get(`/${locationResourceType}/_search`)
+    .query({ _summary: 'count' })
+    .reply(200, { total: 1000 });
+
+  nock(props.fhirBaseURL)
+    .get(`/${locationResourceType}/_search`)
+    .query({ _count: 1000 })
+    .reply(200, locationSData);
+
+  const createdLoc = {
+    resourceType: 'Location',
+    status: 'active',
+    name: 'area51',
+    partOf: { reference: 'Location/2252', display: 'Root FHIR Location' },
+    identifier: [{ use: 'official', value: '13cafa46-7251-429a-8d19-8da0583c0c5a' }],
+    physicalType: {
+      coding: [
+        {
+          system: 'http://terminology.hl7.org/CodeSystem/location-physical-type',
+          code: 'jdn',
+          display: 'Jurisdiction',
+        },
+      ],
+    },
+    id: '13cafa46-7251-429a-8d19-8da0583c0c5a',
+  };
+
+  nock(props.fhirBaseURL).put(`/Location/${createdLoc.id}`, createdLoc).reply(201, {}).persist();
+
+  render(
+    <Router history={history}>
+      <AppWrapper {...props}></AppWrapper>
+    </Router>
+  );
+
+  await waitForElementToBeRemoved(document.querySelector('.ant-spin'));
+
+  // simulate name change
+  const nameInput = screen.getByLabelText('Name');
+  userEvent.type(nameInput, 'area51');
+
+  const save = screen.getByRole('button', { name: 'Save' });
+  userEvent.click(save);
+
+  await waitFor(() => {
+    expect(notificationSuccessMock).toHaveBeenCalledWith('Location was successfully created');
+  });
+
+  // successful submission after action should not result in a disabled
+  // query immediately fetching data and thus running into an error.
+  expect(notificationErrorMock).not.toHaveBeenCalled();
+  expect(nock.isDone()).toBeTruthy();
 });
 
 test('data loading problem', async () => {
