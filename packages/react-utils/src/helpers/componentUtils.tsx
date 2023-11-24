fcd9e9956535058aac423dee16b488fbcf0f5e4b
Peter Muriuki <pmuriuki98@gmail.com>
2023-11-24 09:59:54 +0300

    First working iteration

--- packages/react-utils/src/helpers/componentUtils.tsx
+++ packages/react-utils/src/helpers/componentUtils.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
-import { Route, RouteProps, RouteComponentProps, useLocation } from 'react-router';
+import { Route, RouteProps, RouteComponentProps, useLocation, Redirect } from 'react-router';
 import { getAllConfigs } from '@opensrp/pkg-config';
 import ConnectedPrivateRoute from '@onaio/connected-private-route';
 import { UnauthorizedPage } from '../components/UnauthorizedPage';
@@ -34,23 +34,24 @@ export const PrivateComponent = (props: ComponentProps) => {
 
   const { t } = useTranslation();
 
-  const RbacWrappedComponent = (props: Record<string, unknown>) => (
-    <RbacCheck
-      permissions={permissions}
-      fallback={
-        <UnauthorizedPage
-          title={t('403')}
-          errorMessage={t('Sorry, you are not authorized to access this page')}
-        />
-      }
-    >
-      <WrappedComponent {...props} />
-    </RbacCheck>
-  );
+  // const RbacWrappedComponent = (props: Record<string, unknown>) => (
+  //   <RbacCheck
+  //     permissions={permissions}
+  //     fallback={
+  //       <UnauthorizedPage
+  //         title={t('403')}
+  //         errorMessage={t('Sorry, you are not authorized to access this page')}
+  //       />
+  //     }
+  //   >
+  //     <WrappedComponent {...props} />
+  //   </RbacCheck>
+  // );
 
   const CPRProps = {
     ...otherProps,
-    component: RbacWrappedComponent,
+    permissions,
+    component: WrappedComponent,
     keycloakBaseURL: configs.keycloakBaseURL,
     opensrpBaseURL: configs.opensrpBaseURL,
     fhirBaseURL: configs.fhirBaseURL,
@@ -129,3 +130,88 @@ export const tailLayout = {
     lg: { offset: 6, span: 14 },
   },
 };
+
+
+
+/** interface for PrivateRoute props */
+interface PrivateRouteProps extends RouteProps {
+  authenticated: boolean /** is the current user authenticated */;
+  disableLoginProtection: boolean /** should we disable login protection */;
+  redirectPath: string /** redirect to this path is use if not authenticated */;
+  routerDisabledRedirectPath: string /** redirect to this path if router is not enabled */;
+  routerEnabled: boolean /** is this route enabled */;
+  permissions: any;
+}
+
+/** declare default props for PrivateRoute */
+const defaultPrivateRouteProps: Partial<PrivateRouteProps> = {
+  authenticated: false,
+  disableLoginProtection: false,
+  redirectPath: '/login',
+  routerDisabledRedirectPath: '/',
+  routerEnabled: true
+};
+
+
+
+/** The PrivateRoute component
+ * This component is a simple wrapper around Route and takes all its props in
+ * addition to:
+ *  1. {bool} authenticated
+ *  2. {string} redirectPath
+ *
+ * If authenticated === true then render the component supplied
+ * Otherwise redirect to the redirectPath
+ */
+const PrivateRoute = (props: PrivateRouteProps) => {
+  const {
+    component: Component,
+    authenticated,
+    disableLoginProtection,
+    redirectPath,
+    routerEnabled,
+    routerDisabledRedirectPath,
+    location,
+    permissions,
+    ...theOtherProps
+  } = props;
+
+  /** recreates the url : the path; query string if any; a hash tag if any */
+  const currentPath = `${(location && location.pathname) || ''}${(location && location.search) ||
+    ''}${(location && location.hash) || ''}`;
+  // we can now create the full redirect path, append next searchParma
+  const fullRedirectPath = `${redirectPath}?next=${currentPath}`;
+
+  return (
+    /* tslint:disable jsx-no-lambda */
+    <Route
+      {...theOtherProps}
+      render={routeProps => {
+        if (routerEnabled) {
+          return (authenticated === true || disableLoginProtection === true) && Component ? (
+            <RbacCheck
+              permissions={permissions}
+              fallback={
+                <UnauthorizedPage
+                  title={('403')}
+                  errorMessage={('Sorry, you are not authorized to access this page')}
+                />
+              }
+            >
+              <Component {...routeProps} {...theOtherProps} />
+            </RbacCheck>
+          ) : (
+            <Redirect to={fullRedirectPath} />
+          );
+        } else {
+          return <Redirect to={routerDisabledRedirectPath} />;
+        }
+      }}
+    />
+    /* tslint:enable jsx-no-lambda */
+  );
+};
+
+PrivateRoute.defaultProps = defaultPrivateRouteProps;
+
+export { PrivateRoute }; // export the un-connected component
