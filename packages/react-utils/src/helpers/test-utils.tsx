264857fefab6aa2f9add33d8b99cecc29f23c9d3
Peter Muriuki <pmuriuki98@gmail.com>
2024-01-26 10:38:24 +0300

    Add util function to fill out select fields

--- packages/react-utils/src/helpers/test-utils.tsx
+++ packages/react-utils/src/helpers/test-utils.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { RoleContext, UserRole, FhirResources, Permit, IamResources } from '@opensrp/rbac';
 import { history } from '@onaio/connected-reducer-registry';
 import { Router } from 'react-router';
+import {fireEvent} from '@testing-library/react'
+import userEvents from '@testing-library/user-event';
 
 export const superUserRole = new UserRole([...FhirResources, ...IamResources], Permit.MANAGE);
 
@@ -21,3 +23,50 @@ export const ContextProvider = (props: ContextProviderProps) => {
     </Provider>
   );
 };
+
+interface SearchableSelectValues{
+  selectId: string;
+  searchOptionText: string;
+  fullOptionText: string
+  beforeFilterOptions: string[];
+  afterFilterOptions: string[];
+
+}
+
+export function fillSearchableSelect(tddValues: SearchableSelectValues){
+  const {selectId, fullOptionText, searchOptionText, beforeFilterOptions, afterFilterOptions} = tddValues;
+  
+  // simulate value selection for type
+  const selectComponent = document.querySelector(`input#${selectId}`)!
+  fireEvent.mouseDown(selectComponent);
+
+  let optionTexts = [
+    ...document.querySelectorAll(
+      `#${selectId}_list+div.rc-virtual-list .ant-select-item-option-content`
+    ),
+  ].map((option) => {
+    return option.textContent;
+  });
+
+  expect(optionTexts).toHaveLength(beforeFilterOptions.length);
+  expect(optionTexts).toEqual(beforeFilterOptions);
+
+  // filter searching through members works
+  userEvents.type(selectComponent, searchOptionText);
+
+  // options after search
+  let afterFilterOptionTexts = [
+    ...document.querySelectorAll(
+      `#${selectId}_list+div.rc-virtual-list .ant-select-item-option-content`
+    ),
+  ].map((option) => {
+    return option.textContent;
+  });
+
+  expect(afterFilterOptionTexts).toEqual(afterFilterOptions);
+
+  fireEvent.click(document.querySelector(`[title="${fullOptionText}"]`)!);
+}
+
+
+// TODO - extract this functionality to its own module.
