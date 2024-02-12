dc2ab4b46c7deff72fd65f6849a495f71431a8a2
Peter Muriuki <pmuriuki98@gmail.com>
2024-02-12 09:50:56 +0300

    Add Eusm centric commodity list

---
+++ packages/fhir-group-management/src/components/CommodityList/Eusm/List.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import { Button, Divider, Dropdown, MenuProps } from 'antd';
+import { MoreOutlined } from '@ant-design/icons';
+import { ADD_EDIT_COMMODITY_URL } from '../../../constants';
+import { Link } from 'react-router-dom';
+import { useTranslation } from '../../../mls';
+import { BaseListView, BaseListViewProps, TableData } from '../../BaseComponents/BaseGroupsListView';
+import { TFunction } from '@opensrp/i18n';
+import {
+  useSearchParams,
+  viewDetailsQuery,
+} from '@opensrp/react-utils';
+import {
+  supplyMgSnomedCode,
+  snomedCodeSystem,
+} from '../../../helpers/utils';
+import { useUserRole } from '@opensrp/rbac';
+import { ViewDetailsWrapper } from './ViewDetails';
+
+interface GroupListProps {
+  fhirBaseURL: string;
+  listId: string; // commodities are added to list resource with this id
+}
+
+/**
+ * Shows the list of all group and there details
+ *
+ * @param  props - GroupList component props
+ * @returns returns healthcare display
+ */
+export const EusmCommodityList = (props: GroupListProps) => {
+  const { fhirBaseURL, listId } = props;
+
+  const { t } = useTranslation();
+  const { addParam } = useSearchParams();
+  const userRole = useUserRole();
+
+  const getItems = (record: TableData): MenuProps['items'] => {
+    return [
+      {
+        key: '1',
+        permissions: [],
+        label: (
+          <Button
+            data-testid="view-details"
+            onClick={() => addParam(viewDetailsQuery, record.id)}
+            type="link"
+          >
+            {t('View Details')}
+          </Button>
+        ),
+      },
+    ]
+      .filter((item) => userRole.hasPermissions(item.permissions))
+      .map((item) => {
+        const { permissions, ...rest } = item;
+        return rest;
+      });
+  };
+
+  const getColumns = (t: TFunction) => [
+    {
+      title: t("Material Number"),
+      dataIndex: "materialNumber" as const,
+      key: "materialNumber" as const,
+    },
+    {
+      title: t('Name'),
+      dataIndex: 'name' as const,
+      key: 'name' as const,
+    },
+    {
+      title: t("Attractive Item"),
+      dataIndex: "attractiveItem" as const,
+      key: "attractiveItem" as const,
+      render: (value: boolean) => <div>{value ? t('Yes') : t('No')}</div>,
+    },
+    {
+      title: t('type'),
+      dataIndex: 'type' as const,
+      key: 'type' as const,
+    }, {
+      title: t('Active'),
+      dataIndex: 'active' as const,
+      key: 'active' as const,
+      render: (value: boolean) => <div>{value ? t('Active') : t('Disabled')}</div>,
+    },
+    {
+      title: t('Actions'),
+      width: '10%',
+      // eslint-disable-next-line react/display-name
+      render: (_: unknown, record: TableData) => (
+        <span className="d-flex align-items-center">
+          <Link to={`${ADD_EDIT_COMMODITY_URL}/${record.id}`} className="m-0 p-1">
+            {t('Edit')}
+          </Link>
+          <Divider type="vertical" />
+          <Dropdown
+            menu={{ items: getItems(record) }}
+            placement="bottomRight"
+            arrow
+            trigger={['click']}
+          >
+            <MoreOutlined data-testid="action-dropdown" className="more-options" />
+          </Dropdown>
+        </span>
+      ),
+    },
+  ];
+
+  const baseListViewProps: BaseListViewProps = {
+    getColumns: getColumns,
+    createButtonLabel: t('Add Commodity'),
+    createButtonUrl: ADD_EDIT_COMMODITY_URL,
+    fhirBaseURL,
+    pageTitle: t('Commodity List'),
+    extraQueryFilters: {
+      code: `${snomedCodeSystem}|${supplyMgSnomedCode}`,
+      '_has:List:item:_id': listId,
+    },
+    viewDetailsRender: (fhirBaseURL, resourceId) => <ViewDetailsWrapper fhirBaseURL={fhirBaseURL} resourceId={resourceId!} />
+  };
+
+  return <BaseListView {...baseListViewProps} />;
+};
---
+++ packages/fhir-group-management/src/components/CommodityList/Eusm/tests/fixtures.ts
@@ -0,0 +1,196 @@
+export const firstTwentyEusmCommodities = {
+  resourceType: 'Bundle',
+  id: '9cdfbf16-0b4f-4b2d-9e39-10bb78c950f4',
+  meta: {
+    lastUpdated: '2023-03-09T13:03:23.084+00:00',
+  },
+  type: 'searchset',
+  total: 1,
+  link: [
+    {
+      relation: 'self',
+      url: 'https://fhir.labs.smartregister.org/fhir/Group/_search?_count=50&_elements=name%2Cid&code=http%3A%2F%2Fsnomed.info%2Fsct%7C386452003',
+    },
+  ],
+  entry: [
+    {
+      "fullUrl": "https://fhir.labs.smartregister.org/fhir/Group/52cffa51-fa81-49aa-9944-5b45d9e4c117",
+      "resource": {
+        "resourceType": "Group",
+        "id": "52cffa51-fa81-49aa-9944-5b45d9e4c117",
+        "meta": {
+          "versionId": "1",
+          "lastUpdated": "2024-02-09T09:03:27.930+00:00",
+          "source": "#d38d9951b109b72f"
+        },
+        "identifier": [
+          {
+            "use": "official",
+            "value": "52cffa51-fa81-49aa-9944-5b45d9e4c117"
+          },
+          {
+            "use": "secondary",
+            "value": "606109db-5632-48c5-8710-b726e1b3addf"
+          }
+        ],
+        "active": true,
+        "type": "substance",
+        "actual": false,
+        "code": {
+          "coding": [
+            {
+              "system": "http://snomed.info/sct",
+              "code": "386452003",
+              "display": "Supply management"
+            }
+          ]
+        },
+        "name": "Bed nets",
+        "characteristic": [
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "23435363",
+                  "display": "Attractive Item code"
+                }
+              ]
+            },
+            "valueBoolean": false
+          },
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "34536373",
+                  "display": "Is it there code"
+                }
+              ]
+            },
+            "valueCodeableConcept": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "34536373-1",
+                  "display": "Value entered on the It is there code"
+                }
+              ],
+              "text": "yes"
+            }
+          },
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "45647484",
+                  "display": "Is it in good condition? (optional)"
+                }
+              ]
+            },
+            "valueCodeableConcept": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "45647484-1",
+                  "display": "Value entered on the Is it in good condition? (optional)"
+                }
+              ],
+              "text": "Yes, no tears, and inocuated"
+            }
+          },
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "56758595",
+                  "display": "Is it being used appropriately? (optional)"
+                }
+              ]
+            },
+            "valueCodeableConcept": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "56758595-1",
+                  "display": "Value entered on the Is it being used appropriately? (optional)"
+                }
+              ],
+              "text": "Hanged at correct height and covers averagely sized beds"
+            }
+          },
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "67869606",
+                  "display": "Accountability period (in months)"
+                }
+              ]
+            },
+            "valueQuantity": {
+              "value": 12
+            }
+          },
+          {
+            "code": {
+              "coding": [
+                {
+                  "system": "http://snomed.info/sct",
+                  "code": "1231415",
+                  "display": "Product Image code"
+                }
+              ]
+            },
+            "valueReference": {
+              "reference": "Binary/24d55827-fbd8-4b86-a47a-2f5b4598c515"
+            }
+          }
+        ]
+      },
+      "search": {
+        "mode": "match"
+      }
+    }
+  ],
+};
+
+export const listResource = {
+  resourceType: 'List',
+  id: 'ea15c35a-8e8c-47ce-8122-c347cefa1b4a',
+  identifier: [
+    {
+      use: 'official',
+      value: 'ea15c35a-8e8c-47ce-8122-c347cefa1b4a',
+    },
+  ],
+  status: 'current',
+  mode: 'working',
+  title: 'Supply Chain commodities',
+  code: {
+    coding: [
+      {
+        system: 'http://ona.io',
+        code: 'supply-chain',
+        display: 'Supply Chain Commodity',
+      },
+    ],
+    text: 'Supply Chain Commodity',
+  },
+  entry: [
+    {
+      "item": {
+        "reference": "Group/52cffa51-fa81-49aa-9944-5b45d9e4c117"
+      }
+    },
+    {
+      "item": {
+        "reference": "Binary/24d55827-fbd8-4b86-a47a-2f5b4598c515"
+      }
+    }
+  ],
+};
