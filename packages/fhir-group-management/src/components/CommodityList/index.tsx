d2406c652d8962e4485ab7d356acbc23dd67d10a
Peter Muriuki <pmuriuki98@gmail.com>
2024-02-16 10:38:31 +0300

    Show error banner if list id is not configured

--- packages/fhir-group-management/src/components/CommodityList/index.tsx
+++ packages/fhir-group-management/src/components/CommodityList/index.tsx
@@ -2,9 +2,27 @@ import { getConfig } from '@opensrp/pkg-config';
 import { EusmCommodityList } from './Eusm/List';
 import { DefaultCommodityList, GroupListProps } from './Default/List';
 import React from 'react';
+import { useTranslation } from '../../mls';
+import { Alert } from 'antd';
 
 export const CommodityList = (props: GroupListProps) => {
+  const { t } = useTranslation();
+  const { listId } = props;
   const projectCode = getConfig('projectCode');
+
+  if (!listId) {
+    return (
+      <Alert
+        type="error"
+        message={t('Incorrect/Missing Configuration')}
+        description={t(
+          'List id is either incorrectly configured or missing. Kindly contact support to fix this.'
+        )}
+        banner
+        showIcon
+      />
+    );
+  }
 
   if (projectCode === 'eusm') {
     return <EusmCommodityList {...props} />;
