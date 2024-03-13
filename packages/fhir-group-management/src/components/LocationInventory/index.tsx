3d9ee9fbecc533846a1847d8213b872ee7531360
Eric Musyoka <wambua.eric01@gmail.com>
2024-03-13 13:33:09 +0300

    Add initial form for adding location inventory

---
+++ packages/fhir-group-management/src/components/LocationInventory/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTranslation } from '../../mls';
+import { Helmet } from 'react-helmet';
+import { PageHeader } from '@opensrp/react-utils';
+import { AddLocationInventoryForm } from './form';
+
+interface AddLocationInventoryProps {
+  fhirBaseURL: string;
+}
+
+/**
+ * component to add location inventory
+ *
+ * @param  props - AddLocationInventoryProps component props
+ * @returns returns form to add location inventories
+ */
+export const AddLocationInventory = (props: AddLocationInventoryProps) => {
+  const { fhirBaseURL } = props;
+  const { t } = useTranslation();
+  const pageTitle = t('Add locations Inventory');
+
+  return (
+    <section className="content-section">
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+      <PageHeader title={pageTitle} />
+      <div className="bg-white p-5">
+        <AddLocationInventoryForm fhirBaseUrl={fhirBaseURL} />
+      </div>
+    </section>
+  );
+};
