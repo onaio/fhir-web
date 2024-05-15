8140be0206918f8246254e493dc48ef98ade5603
Peter Muriuki <pmuriuki98@gmail.com>
2024-05-15 11:50:46 +0300

    Deprecate and remove location management opensrp-1

--- packages/location-management/src/cycle.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'cycle' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface Dictionary<T = any> {
-    [key: string]: T;
-  }
-
-  export declare function decycle(obj: Dictionary): object;
-}
