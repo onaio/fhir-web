fdb247043792ca5197d07aabb0caa9170693fe96
kahummer <pkahama@ona.io>
2024-06-27 10:54:14 +0300

    Add resource types to constants

--- packages/fhir-flag/src/constants.tsx
+++ packages/fhir-flag/src/constants.tsx
@@ -3,3 +3,5 @@ export const organizationAffiliationResourceType = 'OrganizationAffiliation';
 export const FlagResourceType = 'Flag';
 export const GroupResourceType = 'Group';
 export const ListResourceType = 'List';
+export const EncounterResourceType = 'Encounter';
+export const ObservationREsourceType = 'Observation';
