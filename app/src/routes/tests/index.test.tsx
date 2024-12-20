f73a398b30592bfbd49506cdc943d2f42e5793a6
Peter Muriuki <pmuriuki98@gmail.com>
2024-12-20 18:58:44 +0300

    Fix test regressions

--- app/src/routes/tests/index.test.tsx
+++ app/src/routes/tests/index.test.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { store } from '@opensrp/store';
 import { authenticateUser } from '@onaio/session-reducer';
 import { filterFalsyRoutes, getRoutes } from '..';
 import { DashboardOutlined } from '@ant-design/icons';
-import React from 'react';
 import { superUserRole } from '@opensrp/react-utils';
+import UploadIcon from '@2fd/ant-design-icons/lib/Upload';
 
 jest.mock('../../configs/env');
 jest.mock('../../configs/settings');
@@ -124,66 +125,110 @@ describe('routes', () => {
               {
                 key: 'users',
                 title: 'Users',
+                permissions: ['iam_user.read'],
+
                 url: '/admin/users',
               },
               {
                 key: 'user-groups',
+                permissions: ['iam_group.read'],
+
                 title: 'User Groups',
                 url: '/admin/users/groups',
               },
               {
                 key: 'user-roles',
+                permissions: ['iam_role.read'],
                 title: 'User Roles',
                 url: '/admin/users/roles',
               },
             ],
             enabled: true,
+            isHomePageLink: true,
             key: 'user-management',
+            permissions: ['iam_user.read'],
             title: 'User Management',
+            url: '/admin/users',
           },
           {
             children: [
               {
                 key: 'location-unit',
+                permissions: ['Location.read'],
+
                 title: 'Hierarchy',
                 url: '/admin/location/unit',
               },
               {
                 key: 'all-locations',
+                permissions: ['Location.read'],
+
                 title: 'All Locations',
                 url: '/admin/location/all',
               },
             ],
             enabled: true,
+            isHomePageLink: true,
             key: 'location-management',
+            permissions: ['Location.read'],
+
             title: 'Location Management',
+            url: '/admin/location/unit',
           },
           {
             children: [
               {
                 key: 'ORGS',
+                permissions: ['Organization.read'],
+
                 title: 'Organizations',
                 url: '/admin/teams',
               },
               {
                 enabled: true,
                 key: 'org-assignment',
+                permissions: ['OrganizationAffiliation.read', 'Location.read'],
+
                 title: 'Organization Assignment',
                 url: '/admin/teams/team-assignment',
               },
             ],
             enabled: true,
+            isHomePageLink: true,
             key: 'org-management',
+            permissions: ['Organization.read'],
+
             title: 'Organization Management',
+            url: '/admin/teams',
           },
-          { enabled: true, key: 'fhir-quest', title: 'Questionnaire Management', url: '/quest' },
+          {
+            enabled: true,
+            isHomePageLink: true,
+            key: 'fhir-quest',
+            permissions: ['Questionnaire.read'],
+
+            title: 'Questionnaire Management',
+            url: '/quest',
+          },
         ],
         enabled: true,
         key: 'admin',
         otherProps: {
           icon: <DashboardOutlined />,
         },
+        permissions: [],
         title: 'Administration',
+      },
+      {
+        enabled: true,
+        isHomePageLink: true,
+        key: 'data-import',
+        otherProps: {
+          icon: <UploadIcon />,
+        },
+        permissions: ['DataImport.read'],
+        title: 'Data Imports',
+        url: '/import',
       },
     ]);
   });
@@ -201,7 +246,7 @@ describe('routes', () => {
       (t: string) => t,
       superUserRole
     );
-    const parentKeys = routes.flatMap((x) => x.children).map((x) => x.key);
+    const parentKeys = routes.flatMap((x) => x.children).map((x) => (x ? x.key ?? '' : ''));
     expect(parentKeys).toContain('org-management');
     expect(parentKeys).toContain('user-management');
     expect(parentKeys).toContain('location-management');
