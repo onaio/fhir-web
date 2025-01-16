2c0bd455a81fdaf32f085951641c06d115f9d5cf
Peter Muriuki <pmuriuki98@gmail.com>
2025-01-16 11:56:27 +0300

    Reduce branch coverage threshhold

--- jest.config.js
+++ jest.config.js
@@ -28,7 +28,7 @@ module.exports = {
   testTimeout: 10000,
   coverageThreshold: {
     global: {
-      branches: 80,
+      branches: 75,
       functions: 80,
       lines: 80,
       statements: 80,
